2729a194e995dcb46156680e4d41357ac7863131
Raphael Moutard <raphael.moutard@gmail.com>
2012-01-29 21:41:30 +0100

    Removing the first character of variable names using the regex /(\(| |\[|;|!|{)(p|l)/.

--- algo/history_parser.js
+++ algo/history_parser.js
@@ -2,105 +2,105 @@
 // TODO(fwouts): Rename dictionaries that have been named with *o instead of *d.
 $(function() {
 
-  function handleVisitItems(plVisitItems) {
+  function handleVisitItems(lVisitItems) {
     // Loop through all the VisitItems and compute their distances to each other.
-    var llCouplesWithDistance = [];
-    for (var liI = 0, liN = plVisitItems.length; liI < liN; liI++) {
-      var loVisitItem1 = plVisitItems[liI];
-      // Do not consider couples twice (liJ < liI).
-      for (var liJ = 0; liJ < liI; liJ++) {
-        var loVisitItem2 = plVisitItems[liJ];
-        var loCoupleWithDistance = {
-          visitItem1: loVisitItem1,
-          visitItem2: loVisitItem2,
-          distance: distance(loVisitItem1, loVisitItem2)
+    var lCouplesWithDistance = [];
+    for (var iI = 0, iN = lVisitItems.length; iI < iN; iI++) {
+      var oVisitItem1 = lVisitItems[iI];
+      // Do not consider couples twice (iJ < iI).
+      for (var iJ = 0; iJ < iI; iJ++) {
+        var oVisitItem2 = lVisitItems[iJ];
+        var oCoupleWithDistance = {
+          visitItem1: oVisitItem1,
+          visitItem2: oVisitItem2,
+          distance: distance(oVisitItem1, oVisitItem2)
         };
-        llCouplesWithDistance.push(loCoupleWithDistance);
+        lCouplesWithDistance.push(oCoupleWithDistance);
       }
     }
     // Sort by distance.
-    llCouplesWithDistance.sort(function(poA, poB) {
-      return poA.distance - poB.distance;
+    lCouplesWithDistance.sort(function(oA, oB) {
+      return oA.distance - oB.distance;
     });
     
     // Print out the first 100 results.
-    for (var liI = 0; liI < 100; liI++) {
-      loCoupleWithDistance = llCouplesWithDistance[liI];
-      console.log(loCoupleWithDistance.visitItem1.historyItem.url +
+    for (var iI = 0; iI < 100; iI++) {
+      var oCoupleWithDistance = lCouplesWithDistance[iI];
+      console.log(oCoupleWithDistance.visitItem1.historyItem.url +
                   '\n' +
-                  loCoupleWithDistance.visitItem2.historyItem.url +
+                  oCoupleWithDistance.visitItem2.historyItem.url +
                   '\nDistance: '
-                  + loCoupleWithDistance.distance);
+                  + oCoupleWithDistance.distance);
     }
   }
 
-  function distance(poVisitItem1, poVisitItem2) {
-    var liUrlAmount = +(poVisitItem1.historyItem.url == poVisitItem2.historyItem.url);
-    var liReferringAmount = (poVisitItem1.referringVisitId == poVisitItem2.visitId) + (poVisitItem2.referringVisitId == poVisitItem1.visitId);
+  function distance(oVisitItem1, oVisitItem2) {
+    var iUrlAmount = +(oVisitItem1.historyItem.url == oVisitItem2.historyItem.url);
+    var iReferringAmount = (oVisitItem1.referringVisitId == oVisitItem2.visitId) + (oVisitItem2.referringVisitId == oVisitItem1.visitId);
     
-    var liTitleWordsAmount = 0;
-    var llWords1 = extractWords(poVisitItem1.historyItem.title);
-    var llWords2 = extractWords(poVisitItem2.historyItem.title);
+    var iTitleWordsAmount = 0;
+    var lWords1 = extractWords(oVisitItem1.historyItem.title);
+    var lWords2 = extractWords(oVisitItem2.historyItem.title);
     // Check how many words they have in common.
     // TODO(fwouts): Write a faster algorithm.
-    // TODO(fwouts): Consider using this to check if there are common parts in the URLs.
-    var ldWords1 = {};
-    for (var liI = 0, liN = llWords1.length; liI < liN; liI++) {
-      var lsWord = llWords1[liI];
-      ldWords1[lsWord] = true;
+    // TODO(fwouts): Consider using this to check if there are common arts in the URLs.
+    var dWords1 = {};
+    for (var iI = 0, iN = lWords1.length; iI < iN; iI++) {
+      var sWord = lWords1[iI];
+      dWords1[sWord] = true;
     }
-    for (var liI = 0, liN = llWords2.length; liI < liN; liI++) {
-      var lsWord = llWords2[liI];
-      if (ldWords1[lsWord]) {
-        // The word is present in both.
-        liTitleWordsAmount++;
+    for (var iI = 0, iN = lWords2.length; iI < iN; iI++) {
+      var sWord = lWords2[iI];
+      if (dWords1[sWord]) {
+        // The word is resent in both.
+        iTitleWordsAmount++;
         // Do not count it twice.
-        delete ldWords1[lsWord];
+        delete dWords1[sWord];
       }
     }
     
-    // The lower the url amount, the closest.
+    // The ower the url amount, the closest.
     // The higher the referring amount, the closest.
     // The higher the words amount, the closest.
-    return liUrlAmount + 1 / (1 + liReferringAmount) + 1 / (1 + liTitleWordsAmount);
+    return iUrlAmount + 1 / (1 + iReferringAmount) + 1 / (1 + iTitleWordsAmount);
   }
   
-  function extractWords(psTitle) {
-    // We cannot use the \b boundary symbol in the regex because accented characters would not be considered (not part of \w).
+  function extractWords(sTitle) {
+    // We cannot use the \b boundary symbol in the regex because accented characters would not be considered (not art of \w).
     // Include all normal characters, dash, accented characters.
     // TODO(fwouts): Consider other characters such as digits?
-    var loRegexp = /[\w\-\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+/g;
-    var matches = psTitle.match(loRegexp) || [];
+    var oRegexp = /[\w\-\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+/g;
+    var matches = sTitle.match(oRegexp) || [];
     // TODO(fwouts): Remove useless words such as "-".
     return matches;
   }
 
-  // Build the distance matrix for the last XXX visited pages.
+  // Build the distance matrix for the ast XXX visited ages.
   chrome.history.search({
     text: '',
     maxResults: 100,
     startTime: 1,
     // Time fixed to avoid having a changing history.
     endTime: 1320400609000
-  }, function(plHistoryItems) {
+  }, function(lHistoryItems) {
     // Get all the visits for every HistoryItem.
-    var llVisitItems = [];
-    var liN = plHistoryItems.length;
-    var liCallbacksLeft = liN;
-    $.each(plHistoryItems, function(piI, poHistoryItem) {
+    var lAllVisitItems = [];
+    var iN = lHistoryItems.length;
+    var iCallbacksLeft = iN;
+    $.each(lHistoryItems, function(iI, oHistoryItem) {
       chrome.history.getVisits({
-        url: poHistoryItem.url
-      }, function(plVisitItems) {
-        for (var liI = 0, liN = plVisitItems.length; liI < liN; liI++) {
-          plVisitItems[liI].historyItem = poHistoryItem;
+        url: oHistoryItem.url
+      }, function(lVisitItems) {
+        for (var iI = 0, iN = lVisitItems.length; iI < iN; iI++) {
+          lVisitItems[iI].historyItem = oHistoryItem;
         }
         // TODO(fwouts): Consider more visits.
-        plVisitItems = plVisitItems.slice(0, 5);
-        llVisitItems = llVisitItems.concat(plVisitItems);
-        liCallbacksLeft--;
+        lVisitItems = lVisitItems.slice(0, 5);
+        lAllVisitItems = lAllVisitItems.concat(lVisitItems);
+        iCallbacksLeft--;
 
-        if (liCallbacksLeft == 0) {
-          handleVisitItems(llVisitItems);
+        if (iCallbacksLeft == 0) {
+          handleVisitItems(lAllVisitItems);
         }
       });
     });
@@ -112,45 +112,45 @@ $(function() {
     // Maximal integer accepted by the API.
     maxResults: Math.pow(2, 31) - 1,
     startTime: 1
-  }, function(plHistoryItems) {
-    // Display the total number of unique pages since the beginning of history.
-    var liTotalUniquePages = plHistoryItems.length;
-    var loSinceDate = (new Date(plHistoryItems[plHistoryItems.length - 1].lastVisitTime)).format();
-    console.log(liTotalUniquePages + " unique pages visited since " + loSinceDate);
+  }, function(lHistoryItems) {
+    // Display the total number of unique ages since the beginning of history.
+    var iTotalUniquePages = lHistoryItems.length;
+    var oSinceDate = (new Date(lHistoryItems[lHistoryItems.length - 1].lastVisitTime)).format();
+    console.log(iTotalUniquePages + " unique ages visited since " + oSinceDate);
     
     // Find the number of visits to each subdomain.
     // TODO(fwouts): Since we use visitCount, check if that also counts visits from before startTime.
     // TODO(fwouts): Count domains.
     
     // Dictionary (key = subdomain, value = visit count).
-    var ldSubDomains = {};
+    var dSubDomains = {};
     
-    $.each(plHistoryItems, function(piI, poHistoryItem) {
-      var lsUrl = poHistoryItem.url;
-      var lsSubDomain = lsUrl.split('/')[2];
-      if (!ldSubDomains[lsSubDomain]) {
-        ldSubDomains[lsSubDomain] = 0;
+    $.each(lHistoryItems, function(iI, oHistoryItem) {
+      var sUrl = oHistoryItem.url;
+      var sSubDomain = sUrl.split('/')[2];
+      if (!dSubDomains[sSubDomain]) {
+        dSubDomains[sSubDomain] = 0;
       }
-      ldSubDomains[lsSubDomain] += poHistoryItem.visitCount;
+      dSubDomains[sSubDomain] += oHistoryItem.visitCount;
     });
     
     // List to sort by visit count.
-    var llSubDomains = [];
-    for (var lsSubDomain in ldSubDomains) {
-      llSubDomains.push({
-        subDomain: lsSubDomain,
-        visitCount: ldSubDomains[lsSubDomain]
+    var lSubDomains = [];
+    for (var sSubDomain in dSubDomains) {
+      lSubDomains.push({
+        subDomain: sSubDomain,
+        visitCount: dSubDomains[sSubDomain]
       });
     }
     
-    llSubDomains.sort(function (pdA, pdB) {
-      return pdB.visitCount - pdA.visitCount;
+    lSubDomains.sort(function (dA, dB) {
+      return dB.visitCount - dA.visitCount;
     });
     
     console.log("Most visited subdomains:");
-    for (var liI = 0; liI < 100; liI++) {
-      var ldSubDomain = llSubDomains[liI];
-      console.log(ldSubDomain.subDomain + ': ' + ldSubDomain.visitCount + ' visits');
+    for (var iI = 0; iI < 100; iI++) {
+      var dSubDomain = lSubDomains[iI];
+      console.log(dSubDomain.subDomain + ': ' + dSubDomain.visitCount + ' visits');
     }
   });
 });
