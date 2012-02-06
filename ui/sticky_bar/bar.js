9f87e96024790ea0e4f7a38c76442651e1982a5f
Raphael Moutard <raphael.moutard@gmail.com>
2012-02-06 01:24:09 +0100

    Adding basic animations. Disabling tests in history_parser by default.

---
+++ ui/sticky_bar/bar.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Represents the bar displayed in the middle of the screen, containing stickers for each story.
+UI.StickyBar.Bar = function() {
+  this._lStickers = [];
+  this._$stickyBar = $('#stickyBar_bar');
+  
+  this._$stickyBar.animate({
+    top: '50%'
+  }, 'slow');
+};
+
+$.extend(UI.StickyBar.Bar.prototype, {
+  
+  // Returns the jQuery element.
+  $: function() {
+    return this._$stickyBar;
+  },
+  
+  // Creates a new sticker and adds it to the bar.
+  buildSticker: function() {
+    // Each sticker will have a different position.
+    var iPosition = this._lStickers.length;
+    var oSticker = new UI.StickyBar.Sticker(this, iPosition);
+    oSticker.$().appendTo(this._$stickyBar);
+    this._lStickers.push(oSticker);
+  }
+  
+});
