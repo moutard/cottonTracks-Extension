00afaa49d636c151ee1ebe7df26fcd52f59388ba
Francois Wouts <francois.wouts@gmail.com>
2012-02-08 09:05:44 +0100

    Adding hover on line.

--- ui/path/line.js
+++ ui/path/line.js
@@ -3,21 +3,24 @@
 // A curved vertical line representing a story.
 // oCenter should be a UI.Point instance.
 UI.Path.Line = function(oFrameCenter) {
-  var iWidth = 1000;
+  // TODO(fwouts): Set the width and height in a more elegant manner.
+  var iWidth = 100;
   var iHeight = 1000;
   var iCenterX = iWidth / 2;
   var iCenterY = iHeight / 2;
   var oPaper = this._oPaper = new Raphael(oFrameCenter.fX - iCenterX, oFrameCenter.fY - iCenterY, iWidth, iHeight);
 
-  var oGraphPath = this._oGraphPath = oPaper.path('M ' + iCenterX + ' ' + iCenterY);
-  oGraphPath.attr(
-    {
-      'stroke': '#333333',
-      'stroke-width': 4,
-      'stroke-linejoin': 'round',
-      'stroke-linecap': 'round'
-    }
-  );
+  var dDefaultStyle = {
+    'stroke': '#333',
+    'stroke-width': 4,
+    'stroke-linejoin': 'round',
+    'stroke-linecap': 'round',
+    'cursor': 'pointer'
+  };
+  var dHoverStyle = {
+    'stroke': '#000',
+    'stroke-width': 6,
+  };
 
   // For testing.
   // We need to use the same number of points in the initial and final paths so that each point gets animated
@@ -27,17 +30,27 @@ UI.Path.Line = function(oFrameCenter) {
   var lInitialPoints = [];
   var lFinalPoints = [];
   for (var iI = 0; iI < iNumberOfPoints; iI++) {
+    var iFinalY = (iI / iNumberOfPoints) * iHeight;
     var oInitialPoint = new UI.Point(iCenterX, iCenterY);
     var oFinalPoint;
-    var iFinalY = (iI / iNumberOfPoints) * iHeight;
     if (iI % 2) {
-      oFinalPoint = oInitialPoint.translate(iHorizontalShift, iFinalY - oInitialPoint.fY);
+      oFinalPoint = oInitialPoint.translate(iHorizontalShift, iFinalY - iCenterY);
     } else {
-      oFinalPoint = oInitialPoint.translate(-iHorizontalShift, iFinalY - oInitialPoint.fY);
+      oFinalPoint = oInitialPoint.translate(-iHorizontalShift, iFinalY - iCenterY);
     }
     lInitialPoints.push(oInitialPoint);
     lFinalPoints.push(oFinalPoint);
   }
+
+  var oGraphPath = this._oGraphPath = oPaper.path(this.pathFromPoints(lInitialPoints));
+  oGraphPath.attr(dDefaultStyle);
+  oGraphPath.hover(function() {
+    // Hover in.
+    this.animate(dHoverStyle, 100);
+  }, function() {
+    // Hover out.
+    this.animate(dDefaultStyle, 100);
+  });
   
   this.animate(this.pathFromPoints(lFinalPoints), 500);
 };
