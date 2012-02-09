4b8e72db1bdeb38dfdbe4de343ae33c9534f22c9
rmoutard <raphael.moutard@gmail.com>
2012-02-09 09:16:20 +0100

    Merge conflicts

---
+++ algo/kmeans.js
@@ -0,0 +1,14 @@
+function Cluster(){
+  this.loCenter = nil; //historyItem
+  this.llClusterSet = []; //list of historyItem
+  
+}
+
+Cluster.prototype.computeAverage = function(){
+  // initialize each cluster with a random page
+};
+
+
+Cluster.prototype.assignment = function(plHistoryItems){
+  // Assignment step: Assign each observation to the cluster with the closest mean
+};
