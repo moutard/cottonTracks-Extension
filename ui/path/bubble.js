12fc1322e22060234b35b6fd81f17b499b628928
Francois Wouts <francois.wouts@gmail.com>
2012-02-25 07:41:35 +0100

    UI: Adding bubbles.

---
+++ ui/path/bubble.js
@@ -0,0 +1,59 @@
+'use strict';
+
+// A bubble showing on a line (represents one element of a story).
+UI.Path.Bubble = function() {
+  var self = this;
+  
+  var $container = this._$container = $('<div class="path_bubble">');
+  $container.text('Bubble');
+  
+  var $handle = this._$handle = $('<div class="path_bubble_handle">');
+  $handle.click(function() {
+    self.toggle();
+  });
+  
+  this._visible = false;
+};
+
+$.extend(UI.Path.Bubble.prototype, {
+  
+  // oBubbleOrigin should be a UI.Point instance.
+  setOrigin: function(oBubbleOrigin) {
+    this._oBubbleOrigin = oBubbleOrigin;
+    var $handle = this._$handle;
+    $handle.css({
+      left: this._oBubbleOrigin.fX,
+      top: this._oBubbleOrigin.fY
+    });
+    $handle.css('opacity', 0);
+    $handle.appendTo('body');
+    $handle.animate({
+      opacity: 1
+    }, 'slow');
+  },
+  
+  show: function() {
+    this._visible = true;
+    this._$container.css({
+      display: 'none',
+      left: this._oBubbleOrigin.fX,
+      top: this._oBubbleOrigin.fY
+    }).appendTo('body');
+    
+    this._$container.show('fast');
+  },
+  
+  hide: function() {
+    this._visible = false;
+    this._$container.hide('fast');
+  },
+  
+  toggle: function() {
+    if (this._visible) {
+      this.hide();
+    } else {
+      this.show();
+    }
+  }
+  
+});
