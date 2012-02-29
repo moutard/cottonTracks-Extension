55171c5209feb9d3185b65b06657120ef516d95b
Raphael Moutard <raphael.moutard@gmail.com>
2012-02-29 18:38:53 +0100

    algo : storySELECT advancement

--- algo/storySELECT.js
+++ algo/storySELECT.js
@@ -8,16 +8,20 @@ Cotton.Algo.clusterStory = function(lHistoryItems, iNbCluster) {
   for ( var i = 0; i <= iNbCluster; i++) {
     lStories[i] = new Story();
   }
-
+  
+  var bStoryUnderConstruction = true;
   // 
   for ( var j = 0; j < lHistoryItems.length; j++) {
     if (lHistoryItems[j].clusterId !== "UNCLASSIFIED"
         && lHistoryItems[j].clusterId !== "NOISE") {
+      bStoryUnderConstruction = false;
       lStories[lHistoryItems[j].clusterId].addHistoryItem(lHistoryItems[j]);
+    } else if(bStoryUnderConstruction){
+      lStories[iNbCluster].addHistoryItem(lHistoryItems[j]);
     }
   }
 
-  console.log(lStories);
+  //console.log(lStories);
   return lStories;
 };
 
@@ -26,6 +30,9 @@ Cotton.Algo.storySELECT = function(lStories) {
 
   var iMaxNumberOfStories = 5;
   var lDisplayStories = new Array();
+  
+  // return story under construction 
+  lDisplayStories.push(lStories[lStories.length - 1]);
 
   // return the last one
   lDisplayStories.push(_.min(lStories, function(oItem) {
@@ -33,11 +40,11 @@ Cotton.Algo.storySELECT = function(lStories) {
   }));
 
   lStories.sort(function(oStory1, oStory2) {
-    return oStory1.length() - oStory2.length();
+    return oStory2.length() - oStory1.length();
   });
-
-  lDisplayStories.concat(lStories.slice(0, 5));
+  
+  lDisplayStories = lDisplayStories.concat(lStories.slice(0, iMaxNumberOfStories));
   // return the lastOne
-  console.log(lDisplayStories);
+  //console.log(lDisplayStories);
   return lDisplayStories;
 };
