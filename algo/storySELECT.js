1c46e38fc1e3b71a115f4821cd4a65be103f72fa
moutard <raphael.moutard@gmail.com>
2012-03-02 11:59:31 +0100

    Algo : remove trailing whitespace

--- algo/storySELECT.js
+++ algo/storySELECT.js
@@ -1,16 +1,16 @@
 'use strict';
 
 Cotton.Algo.clusterStory = function(lHistoryItems, iNbCluster) {
-  // 
+  //
   var lStories = new Array();
 
   // initialized
   for ( var i = 0; i <= iNbCluster; i++) {
     lStories[i] = new Cotton.Model.Story();
   }
-  
+
   var bStoryUnderConstruction = true;
-  // 
+  //
   for ( var j = 0; j < lHistoryItems.length; j++) {
     if (lHistoryItems[j].clusterId !== "UNCLASSIFIED"
         && lHistoryItems[j].clusterId !== "NOISE") {
@@ -30,19 +30,19 @@ Cotton.Algo.storySELECT = function(lStories) {
 
   var iMaxNumberOfStories = 5;
   var lDisplayStories = new Array();
-  
-  // return story under construction 
+
+  // return story under construction
   lDisplayStories.push(lStories[lStories.length - 1]);
 
   // return the last one
   lDisplayStories.push(_.max(lStories, function(oItem) {
-    return oItem.fLastVisitTime();
+    return oItem.lastVisitTime();
   }));
 
   lStories.sort(function(oStory1, oStory2) {
     return oStory2.length() - oStory1.length();
   });
-  
+
   lDisplayStories = lDisplayStories.concat(lStories.slice(0, iMaxNumberOfStories));
   // return the lastOne
   //console.log(lDisplayStories);
