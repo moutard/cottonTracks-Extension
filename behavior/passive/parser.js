78d4981c352c49db57c4aa6d963b65f5a11e56e6
Francois Wouts <francois.wouts@gmail.com>
2012-03-03 16:47:29 +0100

    Behavior: Preparing the structure, adding Class by John Resig.

---
+++ behavior/passive/parser.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Cotton.Behavior.Passive.Parser = Class.extend({
+  init: function() {
+    // alert('a');
+  }
+});
+
+// For testing.
+$(function() {
+  new Cotton.Behavior.Passive.Parser();
+});
