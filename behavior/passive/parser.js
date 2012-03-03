0273b48959ba5857b4ebcf30de3792dcca5bad74
Raphael Moutard <raphael.moutard@gmail.com>
2012-03-03 19:07:47 +0100

    Adding first version of passive parser.

--- behavior/passive/parser.js
+++ behavior/passive/parser.js
@@ -2,7 +2,36 @@
 
 Cotton.Behavior.Passive.Parser = Class.extend({
   init: function() {
-    // alert('a');
+    
+    function couldBeContentBlock($block) {
+      return ($block.is(':visible') && $block.width() > 400 && $block.height() > 400);
+    }
+    
+    function findContentBlock($block) {
+      
+      var bCouldBeContentBlock = couldBeContentBlock($block);
+      
+      if (bCouldBeContentBlock) {
+        // Check if there is one child (and only one) that could also be a content block. Then it
+        // would be a better content block.
+        var lMatchingChildren = $block.children().filter(function() {
+          var $childBlock = $(this);
+          var bCouldBeContentBlock = couldBeContentBlock($childBlock);
+          return bCouldBeContentBlock;
+        });
+        if (lMatchingChildren.length == 1) {
+          return findContentBlock($(lMatchingChildren[0]));
+        } else {
+          console.log(lMatchingChildren);
+          return $block;
+        }
+      } else {
+        return $([]);
+      }
+    }
+    
+    console.log("Found the content block:");
+    console.log(findContentBlock($('body')));
   }
 });
 
