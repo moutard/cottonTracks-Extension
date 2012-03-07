752bdf175f78fa9dcd358f3778fa43b8084254da
rmoutard <raphael.moutard@gmail.com>
2012-03-07 18:49:49 +0100

    Algo : toTestDBSCAN

--- algo/to_test_dbscan.js
+++ algo/to_test_dbscan.js
@@ -14,18 +14,16 @@ function displayHistoryItem(oHistoryItem, sColor) {
 
   var sLine = '<div class="url" style="color:' + sColor + '">';
   sLine += '<h5 style="font-size:14px; margin-bottom:0px; margin-top:8px;">'
-      + oHistoryItem["title"] +  ' || cluster : ' +  oHistoryItem["clusterId"]
- + '</h5>';
+      + oHistoryItem["title"] + ' || cluster : ' + oHistoryItem["clusterId"]
+      + '</h5>';
   sLine += '<h6 style="font-size:11px; margin:2px">' + oHistoryItem["url"]
       + '</h6>';
   sLine += '</div>';
   $("#liste").append(sLine);
-  //console.log(oHistoryItem);
+  // console.log(oHistoryItem);
 };
 
 function displayStory(oStory) {
-  //oStory.removeHistoryItem(a[a.length - 1].id);
-  //oStory.moveHistoryItem(a[a.length - 2].id);
 
   var lHistoryItems = oStory.iter();
   var sColor = randomColor();
@@ -73,7 +71,7 @@ function displayStorySELECTResult(iNbCluster, lHistoryItems) {
   var bUseRelevance = Cotton.Config.Parameters.bUseRelevance;
 
   var lStories = Cotton.Algo.clusterStory(lHistoryItems, iNbCluster);
-  //var lDStories = Cotton.Algo.storySELECT(lStories, bUseRelevance);
+  // var lDStories = Cotton.Algo.storySELECT(lStories, bUseRelevance);
 
   $('#loader-animation').remove();
   for ( var i = 0; i < lStories.length; i++) {
@@ -81,63 +79,66 @@ function displayStorySELECTResult(iNbCluster, lHistoryItems) {
   }
 
   openStore(lStories);
-
 }
 
 // MODEL
 
 // clear the dataBase
 
-function openStore(lStories){
-  var oStore = new Cotton.DB.Store('ct',
-        { 'stories': Cotton.Translators.STORY_TRANSLATORS },
-        function() {
-          console.log("store ready");
-          storeData(this, lStories);
-        }
-  );
+function storeStories(lStories) {
+  // open a Store
+
+  var oStore = new Cotton.DB.Store('ct', {
+    'stories' : Cotton.Translators.STORY_TRANSLATORS
+  }, function() {
+    console.log("store ready");
+    // console.log(this);
+    storeData(this, lStories);
+  });
 }
 
-function storeData(oStore, lStories){
-    // store the most recent in last
-    for(var i = lStories.length - 1, oStory; oStory = lStories[i]; i--){
-       oStore.put('stories', oStory, function() {
-        console.log("Story added");
-        oStore.list('stories', function(oStory) {
-          //console.log(oStory._lHistoryItems);
-        });
+function storeData(oStore, lStories) {
+  // store the most recent in last
+  for ( var i = lStories.length - 1, oStory; oStory = lStories[i]; i--) {
+    oStore.put('stories', oStory, function() {
+      console.log("Story added");
+      oStore.list('stories', function(oStory) {
+        // console.log(oStory._lHistoryItems);
       });
-    }
-}
 
+    });
+  }
+}
 
 // CONTROLLER
-//
-// Cotton.DB.ManagementTools.listDB();
+Cotton.DB.ManagementTools.clearDB();
+Cotton.DB.ManagementTools.listDB();
 
 // WORKER
 var worker = new Worker('algo/worker.js');
 
 worker.addEventListener('message', function(e) {
   console.log('Worker ends: ', e.data.iNbCluster);
-   //displayDBSCANResult(e.data.iNbCluster, e.data.lHistoryItems);
+
+  // displayDBSCANResult(e.data.iNbCluster, e.data.lHistoryItems);
+
   displayStorySELECTResult(e.data.iNbCluster, e.data.lHistoryItems);
 
 }, false);
 
-if(localStorage){
+if (localStorage) {
   // check if broswer support localStorage
 
-  if(localStorage['CottonFirstOpening'] === undefined ||
-     localStorage['CottonFirstOpening'] === "true"){
+  if (localStorage['CottonFirstOpening'] === undefined
+      || localStorage['CottonFirstOpening'] === "true") {
 
     Cotton.DB.ManagementTools.clearDB();
     window.UI.firstVisit();
 
     chrome.history.search({
-    text : '',
-    startTime : 0,
-    maxResults : Cotton.Config.Parameters.iMaxResult,
+      text : '',
+      startTime : 0,
+      maxResults : Cotton.Config.Parameters.iMaxResult,
     }, function(lHistoryItems) {
       console.log(lHistoryItems);
       // Get all the visits for every HistoryItem.
@@ -145,13 +146,12 @@ if(localStorage){
     });
     localStorage['CottonFirstOpening'] = "false";
   } else {
-    //localStorage['CottonFirstOpening'] = true;
+    // localStorage['CottonFirstOpening'] = true;
     // use stories store in the database
     Cotton.DBSCAN2.startDbscanUser();
     Cotton.DB.ManagementTools.printDB(displayStory);
- }
+  }
 
 } else {
- console.log("Browser doesn't support the local storage");
+  console.log("Browser doesn't support the local storage");
 }
-
