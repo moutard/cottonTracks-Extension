4bac3c25247180944290163064fa622e40c64644
moutard <raphael.moutard@gmail.com>
2012-03-13 17:48:18 +0100

    DBSCAN2 : first version

--- db/management_tools.js
+++ db/management_tools.js
@@ -2,7 +2,7 @@
 
 Cotton.DB.ManagementTools = {};
 
-Cotton.DB.ManagementTools.clearDB = function(){
+Cotton.DB.ManagementTools.clearDB = function () {
    new Cotton.DB.Store('ct',
        { 'stories': Cotton.Translators.STORY_TRANSLATORS },
        function() {
@@ -15,7 +15,7 @@ Cotton.DB.ManagementTools.clearDB = function(){
       });
 };
 
-Cotton.DB.ManagementTools.listDB = function(){
+Cotton.DB.ManagementTools.listDB = function () {
   console.log('LIST');
    new Cotton.DB.Store('ct',
        { 'stories': Cotton.Translators.STORY_TRANSLATORS },
@@ -27,7 +27,7 @@ Cotton.DB.ManagementTools.listDB = function(){
 
 };
 
-Cotton.DB.ManagementTools.printDB = function(mActionWithStory){
+Cotton.DB.ManagementTools.printDB = function (mActionWithStory) {
   var self = this;
   console.log('PRINT');
    new Cotton.DB.Store('ct',
@@ -38,3 +38,17 @@ Cotton.DB.ManagementTools.printDB = function(mActionWithStory){
         });
        });
 };
+
+Cotton.DB.ManagementTools.addStories = function (lStories) {
+  var oStore = new Cotton.DB.Store('ct',
+        { 'stories': Cotton.Translators.STORY_TRANSLATORS },
+        function() {
+          console.log("store ready");
+          for(var i = 0, oStory; oStory = lStories[i]; i++){
+            oStore.put('stories', oStory, function() {
+              console.log("Story added");
+            });
+          }
+        }
+      );
+}
---
+++ dbscan2/dbscanUser.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// this algo use dbscan on the last story and all the visited pages that have
+// not been scanned yet.
+
+// WORKER
+// Use the worker of DBSCAN. It's exaclty the same. Just change the callback.
+Cotton.DBSCAN2.dbscanWorker = new Worker('algo/worker.js');
+
+dbscanWorker.addEventListener('message', function(e) {
+  console.log('Worker ends: ', e.data.iNbCluster);
+  // When DBSCAN is over, store new computed stories in the IndexedDB database.
+  var lStories = Cotton.Algo.clusterStory(e.data.lHistoryItems, e.data.iNbCluster);
+  Cotton.DB.ManagementTools.addStories(lStories);
+}, false);
+
+Cotton.DBSCAN2.dbscanUser = function () {
+  // 
+  // dbscanUser should be called every time a new tab is open.
+  // or maybe just new window for the beginning.
+
+  var lastStory;
+  var oStore = new Cotton.DB.Store('ct',
+        { 'stories': Cotton.Translators.STORY_TRANSLATORS },
+        function() {
+          // Get the last Story in the database to know when we finished.
+          this.getLastEntry('stories', function(oStory){
+            lastStory = oStory;
+            
+            chrome.history.search({
+              // Get all the historyItem we haven't scanned yet.
+              text : '',
+              startTime : lastStory.lastVisitTime(),
+              maxResults : Cotton.Config.Parameters.iMaxResult,
+            }, function(lHistoryItems) {
+
+              // include current story's historyItems
+              lHistoryItems = lHistoryItems.concat(lastStory.iter());
+              worker.postMessage(lHistoryItems);
+            });
+          });
+        } 
+  );
+};
