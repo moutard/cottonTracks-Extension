769e3f3678b65471010f876b3e4753d50b3035d6
Francois Wouts <francois.wouts@gmail.com>
2012-03-18 22:44:08 +0100

    Adding support for Google Analytics.

---
+++ config/ga.js
@@ -0,0 +1,11 @@
+// Snippet taken from http://code.google.com/chrome/extensions/trunk/tut_analytics.html.
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-30134257-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+})();
