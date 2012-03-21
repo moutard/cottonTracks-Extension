1fe13cd9ad0f300bc5536245605755fbcd464a50
Raphael Moutard <raphael.moutard@gmail.com>
2012-03-21 18:27:00 +0100

    DB : make a difference between Cotton and Chrome historyItem

--- translators/history_item_translator.js
+++ translators/history_item_translator.js
@@ -6,28 +6,53 @@ Cotton.Translators.HISTORY_ITEM_TRANSLATORS = [];
 (function() {
 
   var mObjectToDbRecordConverter = function(oHistoryItem) {
-
-    var dDbRecord = {
-      sId : oHistoryItem.id,
-      sChromeApiId : oHistoryItem.id, // to keep a link with chrome history
-      sUrl : oHistoryItem.url,
-      sTitle : oHistoryITem.title,
-      fLastVisitTime : oHistoryItem.lastVisitTime,
-      iVisitCount : oHistoryItem.visitCount,
-      iTypedCount : oHistoryItem.typedCount,
-    };
-
-    // TODO(rmoutard): ask fwouts why it's important ?
-    var iId = oHistoryItem.id() || null;
-    if (iId) {
-      dDbRecord.id = iId;
+    
+    var dDbRecord = {};
+    
+    // oHistoryItem is a chromeHistoryItem.
+    if(oHistoryItem.id !=== undefined){ 
+      dDBRecord = {
+        sChromeId : oHistoryItem.id, // to keep a link with chrome history
+        sUrl : oHistoryItem.url,
+        sTitle : oHistoryITem.title,
+        fLastVisitTime : oHistoryItem.lastVisitTime,
+        iVisitCount : oHistoryItem.visitCount,
+        iTypedCount : oHistoryItem.typedCount,
+      };
+    } else {
+    // else oHistoryItem is a CottonHistoryItem.
+      dDbRecord = {
+        // sId : oHistoryItem.id, auto incremented
+        sChromeId : oHistoryItem.chromeId(), // to keep a link with chrome history
+        sUrl : oHistoryItem.url(),
+        sTitle : oHistoryITem.title(),
+        fLastVisitTime : oHistoryItem.lastVisitTime(),
+        iVisitCount : oHistoryItem.visitCount(),
+        iTypedCount : oHistoryItem.typedCount(),
+      };
     }
+    // Handle expanded historyItem
+
     return dDbRecord;
   };
 
   var mDbRecordToObjectConverter = function(oDbRecord) {
     // oDbRecord is just a dictionnary
-    var oHistoryItem = new Cotton.Model.HistoryItem(oDbRecord);
+    var oHistoryItem = new Cotton.Model.HistoryItem();
+    if(oHistoryItem !== undefined){
+      // Use private attributes because they are immutable. 
+      oHistoryItem._sId = oDbRecord.id;
+      oHistoryItem._sChromeId = oDbRecord.sChromeId;
+      oHistoryItem._sUrl = oHistoryItem.sUrl;
+      oHistoryItem._sTitle(oHistoryItem.sTitle);
+      
+      //
+      oHistoryItem.setLastVisitTime(oHistoryItem.iLastVisitTime);
+      oHistoryItem.setVisitCount(oHistoryItem.iVisitCount);
+      oHistoryItem.setTypedCount(oHistoryItem.itypedCount);
+
+    }
+
     return oHistoryItem;
   };
 
