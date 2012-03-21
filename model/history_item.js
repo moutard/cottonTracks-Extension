9b90308d3d937b5f7fd5c44200e715e04310d373
moutard <raphael.moutard@gmail.com>
2012-03-21 18:27:00 +0100

    DB : make a difference between Cotton and Chrome historyItem

--- model/history_item.js
+++ model/history_item.js
@@ -4,7 +4,8 @@
 Cotton.Model.HistoryItem = function (oHistoryItem) {
   // Current chrome API model
   this._sId;
-  this._sUrl;
+  this._sChromeId; // Should be the same that Google chrome history.
+  this._sUrl; // Can be use as unique identifier.
   this._sTitle;
   this._iLastVisitTime;
   this._iVisitCount;
@@ -19,31 +20,53 @@ Cotton.Model.HistoryItem = function (oHistoryItem) {
 
   // Improved model - only available for DBSCAN2
   // TODO(rmoutard) : see with fwouts the model of textHighLigter
-  this._sTextHighlighter;
-  this._fReaderRatio;
-  this._iNewTab;
-
-  if(oHistoryItem !== undefined){
-    this._sId = oHistoryItem.sId;
-    this._sUrl = oHistoryItem.sUrl;
-    this._sTitle = oHistoryItem.sTitle;
-    this._iLastVisitTime = oHistoryItem.iLastVisitTime;
-    this._iVisitCount = oHistoryItem.iVisitCount;
-    this._iTypedCount = oHistoryItem.itypedCount;
-  }
-
+  this._lTextHighlighter = [];
+  this._iScrollCount = 0;
+  this._lCopyPaste = [];
+ 
 };
 
 $.extend(Cotton.Model.HistoryItem.prototype, {
   //GETTER
-  id  :   function() { return this._sID; },
+  // can't be set
+  id  :   function() { return this._sId; },
+  chromeId : function() { return this._sChromeId; },
   url :   function() { return this._sUrl; },
   title : function() { return this._sTitle; },
+  
+  // can be set
   lastVisitTime : function() { return this._iLastVisitTime; },
+  setLastVisitTime : function(lastVisitTime){ 
+    this._iLastVisitTime = lastVisitTime; 
+  },
   visitCount :    function() { return this._iVisitCount; },
+  setVisitCount : function(visitCount) {this._iVisitCount = visitCount;},
   typedCount :    function() { return this._iTypedCount; },
-
+  setTypedCount : function(typedCount) { this._iTypedCount = typedCount;},
+  
+  // expanded
   textHighLighter : function() { return this._sTextHighlighter; },
-  readerRatio     : function() { return this._fReaderRatio; },
-  newTab          : function() { return this._iNewTab; }
+  setTextHighLighter : function(highLight) {
+    this._sTextHighlighter.push(highLight);
+  },
+  scrollCount : function() { return this._iScrollCount; },
+  setScrollCount : function(scrollCount) { this._iScrollCount = scrollCount;},
+  copyPaste : function() { return this._lCopyPaste; },
+  setCopyPaste : function(copyPaste) { this._lCopyPaste.push(copyPaste); },
+
+  // method
+  getInfoFromPage : function() {
+    this._sUrl = window.location.href;
+    this._sTitle = document.title;
+    this._iLastVisitTime = new Date().getTime();
+   
+    // maybe not work if not called by the extension.
+    /* 
+    chrome.history.getVisits({ 'url' : this._sUrl },
+        function(lVisitItems) {
+          if(lVisitItems.length > 0){this._sChromeId = lVisitItem[0].id; }
+        }
+    );
+    */
+  },
 });
