1887344ee75876b5caf81bef53bf32b6e40c196f
Raphael Moutard <raphael.moutard@gmail.com>
2012-04-15 21:58:22 +0200

    behavior : visit item in open each new tab

--- messaging/content_script_listener.js
+++ messaging/content_script_listener.js
@@ -7,16 +7,25 @@ function onRequest(request, sender, sendResponse) {
   console.log("background has received a message");
   console.log(request);
 
-  //Cotton.DB.ManagementTools.listDB();
+  // Cotton.DB.ManagementTools.listDB();
   // it seems request.historyItem is not an HistoryItem but just a dictionnary.
-  var oHistoryItem = new Cotton.Model.HistoryItem();
-  oHistoryItem.deserialize(request.historyItem);
-  Cotton.DB.Pool.push(oHistoryItem);
+  var oVisitItem = new Cotton.Model.VisitItem();
+  oVisitItem.deserialize(request.visitItem);
+  // Cotton.DB.Pool.push(oVisitItem);
+  var oStore = new Cotton.DB.Store('ct', {
+    'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS
+  }, function() {
+    oStore.put('visitItems', oVisitItem, function(iId) {
+      console.log("visitItem added");
+      console.log(iId);
+    });
 
+  });
   // Return nothing to let the connection be cleaned up.
-  sendResponse({received: "true"});
+  sendResponse({
+    received : "true"
+  });
 };
 
 // Listen for the content script to send a message to the background page.
 chrome.extension.onRequest.addListener(onRequest);
-
