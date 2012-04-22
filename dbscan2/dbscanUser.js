d7ecb1247934b5e46474a544b24d0075e9995b2b
Raphael Moutard <raphael.moutard@gmail.com>
2012-04-22 22:07:24 +0200

    dbscan2 : use dbscan

--- dbscan2/dbscanUser.js
+++ dbscan2/dbscanUser.js
@@ -3,9 +3,9 @@
 // this algo use dbscan on the last story and all the visited pages that have
 // not been scanned yet.
 
-function handleResultsOfDBSCAN2(iNbCluster, lHistoryItems){
+function handleResultsOfDBSCAN2(iNbCluster, lVisitItems) {
 
-  var dStories = Cotton.Algo.clusterStory(lHistoryItems, iNbCluster);
+  var dStories = Cotton.Algo.clusterStory(lVisitItems, iNbCluster);
   // var lDStories = Cotton.Algo.storySELECT(lStories, bUseRelevance);
   var bUseRelevance = Cotton.Config.Parameters.bUseRelevance;
 
@@ -29,10 +29,10 @@ Cotton.DBSCAN2.dbscanWorker = new Worker('algo/worker.js');
 Cotton.DBSCAN2.dbscanWorker.addEventListener('message', function(e) {
   // When DBSCAN is over, store new computed stories in the IndexedDB database.
   console.log("After dbscan");
-  console.log(e.data.lHistoryItems);
+  console.log(e.data.lVisitItems);
   console.log(e.data.iNbCluster);
 
-  handleResultsOfDBSCAN2(e.data.iNbCluster, e.data.lHistoryItems);
+  handleResultsOfDBSCAN2(e.data.iNbCluster, e.data.lVisitItems);
 }, false);
 
 Cotton.DBSCAN2.startDbscanUser = function() {
@@ -45,26 +45,31 @@ Cotton.DBSCAN2.startDbscanUser = function() {
     'stories' : Cotton.Translators.STORY_TRANSLATORS
   }, function() {
     // Get the last Story in the database to know when we finished.
-    this.getLastEntry('stories', function(oStory) {
-      console.log(oStory);
-      lastStory = oStory;
-      this.delete('stories', oStory, function(){
-        chrome.history.search({
-          // Get all the historyItem we haven't scanned yet.
-          text : '',
-          startTime : lastStory.lastVisitTime(),
-          maxResults : Cotton.Config.Parameters.iMaxResult,
-        }, function(lHistoryItems) {
-          console.log("result with last Visit Item");
-          console.log(lHistoryItems);
-          // include current story's historyItems
-
-          lHistoryItems = lHistoryItems.concat(lastStory.iter());
-          console.log("Before dbscan");
-          console.log(lHistoryItems);
-          Cotton.DBSCAN2.dbscanWorker.postMessage(lHistoryItems);
-        }); // end search
-      }); // end delete
+    this.getLastEntry('stories', function(oLastStory) {
+      // TODO(rmoutard) : instead of range by id, use fLastVisitTime.
+      var iLastId = oLastStory.getLastVisitItemId();
+      console.log(iLastId);
+      var oVisitStore = new Cotton.DB.Store('ct', {
+        'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS
+      }, function() {
+        this.getRange('visitItems', 0, iLastId, function(lPoolVisitItems) {
+          console.log(lPoolVisitItems);
+          Cotton.DBSCAN2.dbscanWorker.postMessage(lPoolVisitItems);
+        });
+      });
+      /*
+       * this.delete('stories', oStory, function(){ chrome.history.search({ //
+       * Get all the historyItem we haven't scanned yet. text : '', startTime :
+       * lastStory.lastVisitTime(), maxResults :
+       * Cotton.Config.Parameters.iMaxResult, }, function(lHistoryItems) {
+       * console.log("result with last Visit Item"); console.log(lHistoryItems); //
+       * include current story's historyItems
+       * 
+       * lHistoryItems = lHistoryItems.concat(lastStory.iter());
+       * console.log("Before dbscan"); console.log(lHistoryItems);
+       * Cotton.DBSCAN2.dbscanWorker.postMessage(lHistoryItems); }); // end
+       * search }); // end delete
+       */
     }); // end getLastEntry
   });
 };
