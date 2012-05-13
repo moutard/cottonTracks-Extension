d587958b4fd4d20be33b989e908003c6be32e94b
rmoutard <raphael.moutard@gmail.com>
2012-05-13 21:17:33 +0200

    Installation : draft - not important

--- algo/to_test_dbscan.js
+++ algo/to_test_dbscan.js
@@ -32,35 +32,67 @@ wDBSCAN.addEventListener('message', function(e) {
 
 }, false);
 
-// START
-if (localStorage) {
-  // Check if broswer support localStorage
+/*
+ * // START if (localStorage) { // Check if broswer support localStorage //
+ * ct-dev-env is used to dev purpose. Remove this line for user version.
+ * localStorage['ct-dev-env'] = true;
+ * 
+ * if (localStorage['CottonFirstOpening'] === undefined ||
+ * localStorage['CottonFirstOpening'] === "true") { // This is the first visit. //
+ * CT-DEV-ENV if (localStorage['ct-dev-env'] === true) { // In ct-dev-en delete
+ * the database. var oDeleteRequest =
+ * window.webkitIndexedDB.deleteDatabase('ct');
+ * 
+ * oDeleteRequest.onsuccess = function(event) { // After deleting database
+ * create a new one. Cotton.DB.populateDB(function() { var oStore = new
+ * Cotton.DB.Store('ct', { 'visitItems' :
+ * Cotton.Translators.VISIT_ITEM_TRANSLATORS }, function() {
+ * oStore.getList('visitItems', function(lAllVisitItems) {
+ * wDBSCAN.postMessage(lAllVisitItems); }); }); }); };
+ * 
+ * oDeleteRequest.onerror = function(event) { // TODO(rmoutard) : write error
+ * function. }; } } else { // This is not the first visit. // DBSCAN2.
+ * Cotton.DBSCAN2.startDbscanUser(); } } else { console.log("Browser doesn't
+ * support the local storage"); }
+ */
 
-  if (localStorage['CottonFirstOpening'] === undefined
-      || localStorage['CottonFirstOpening'] === "true") {
-    // This is the first visit.
+// Get all the indexedDB on this computer. Check if our ct database already
+// exists.
+var oDBRequest = window.webkitIndexedDB.getDatabaseNames();
 
-    Cotton.DB.ManagementTools.purge('visitItems', function() {
-      Cotton.DB.ManagementTools.purge('stories', function() {
-        Cotton.DB.populateDB(function() {
-          var oStore = new Cotton.DB.Store('ct', {
-            'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS
-          }, function() {
-            oStore.getList('visitItems', function(lAllVisitItems) {
-              wDBSCAN.postMessage(lAllVisitItems);
-            });
-          });
+oDBRequest.onsuccess = function(oIDBRequest) {
+
+  if (_.indexOf(oIDBRequest.result, 'ct') === -1) {
+    // The ct database doesn't exist. So it's the first installation.
+    // Launch populateDB to create the database for the first time.
+    Cotton.DB.populateDB(function() {
+      var oStore = new Cotton.DB.Store('ct', {
+        'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS
+      }, function() {
+        oStore.getList('visitItems', function(lAllVisitItems) {
+          wDBSCAN.postMessage(lAllVisitItems);
         });
       });
     });
-
   } else {
-    // This is not the first visit.
+    // There is already a ct database. That means two choices :
+    // - You open a tab after installation.
+    // - It's not the first installation.
+    // Depends on the value of CottonOpeningFirst.
 
-    // DBSCAN2.
-    Cotton.DBSCAN2.startDbscanUser();
+    if (localStorage['ct-loading-page'] === undefined
+        || localStorage['ct-loading-page'] === "true") {
+      // It's not the first installation.
+      // Cotton.DBSCAN2.startDbscanUser();
+
+    } else {
+      // You open a tab after installation.
+      Cotton.DBSCAN2.startDbscanUser();
+    }
   }
 
-} else {
-  console.log("Browser doesn't support the local storage");
-}
+};
+
+oDBRequest.onerror = function(event) {
+  // TODO(rmoutard) : Create the database ?
+};
