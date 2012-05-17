f5306b6593396f40ddbabeb8802e044886118d29
Raphael Moutard <raphael.moutard@gmail.com>
2012-05-17 17:01:01 +0200

    DB : add getXItems and integrate it to uiWorld

--- ui/world.js
+++ ui/world.js
@@ -5,15 +5,15 @@ Cotton.UI.World = function() {
   var oStickyBar = this._ostickyBar = new Cotton.UI.StickyBar.Bar();
   var object = {};
   oStickyBar.on('ready', function() {
-    
-    Cotton.DBSCAN2.getStories(function(lStories) {
+
+    Cotton.DBSCAN2.getXStories(10, function(lStories) {
       // Various initializers, mostly for testing.
       var lStickers = [];
       _.each(lStories, function(oStory) {
         var oSticker = oStickyBar.buildSticker(oStory);
         lStickers.push(oSticker);
       });
-      
+
       _.each(lStickers, function(oSticker) {
         oSticker.display();
       });
@@ -27,5 +27,5 @@ Cotton.UI.World.COMMUNICATOR = {};
 _.extend(Cotton.UI.World.COMMUNICATOR, Backbone.Events);
 
 $.extend(Cotton.UI.World.prototype, {
-  
+
 });
