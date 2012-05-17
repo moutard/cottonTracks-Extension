1b6e4b68577758a671909a1475391ba9686b75b9
Raphael Moutard <raphael.moutard@gmail.com>
2012-05-17 13:35:49 +0200

    UI : Infinite animation for landscape background. Use Adrien's version

--- ui/loading.js
+++ ui/loading.js
@@ -1,21 +1,23 @@
 'use strict';
 
-$(window).load(function() {
+$(window).load(
+    function() {
       $('body').addClass('ct-body-loaded');
       $('.ct-landscape').addClass('running');
       // If it's not the first time curtain is already open.
       if (localStorage['CottonFirstOpening'] === undefined
-      || localStorage['CottonFirstOpening'] === "true") {
+          || localStorage['CottonFirstOpening'] === "true") {
       } else {
         Cotton.UI.openCurtain();
       }
-});
+    });
 
-Cotton.UI.openCurtain = function(){
+Cotton.UI.openCurtain = function() {
   $('body').addClass('ct-body-open');
   $('.ct-landscape').removeClass('running');
+  $('.ct-landscape').css('opacity', '0');
 };
 
-Cotton.UI.closeCurtain = function(){
+Cotton.UI.closeCurtain = function() {
   $('body').removeClass('ct-body-open');
 }
