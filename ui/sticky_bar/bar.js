f0543a1a28ff479b7e7f1a1cec3f2b4a82748800
Raphael Moutard <raphael.moutard@gmail.com>
2012-05-19 18:21:56 +0200

    UI : Draft - sumUp story on stickers' hover

--- ui/sticky_bar/bar.js
+++ ui/sticky_bar/bar.js
@@ -1,77 +1,80 @@
 'use strict';
 
-// Represents the bar displayed in the middle of the screen, containing stickers for each story.
+// Represents the bar displayed in the middle of the screen, containing stickers
+// for each story.
 Cotton.UI.StickyBar.Bar = Class.extend({
-  
-  _lStickers: null,
-  _$stickyBar: null,
-  _iTranslateX: 0,
-  
-  init: function() {
+
+  _lStickers : null,
+  _$stickyBar : null,
+  _$sumUp : null,
+  _iTranslateX : 0,
+
+  init : function() {
     var self = this;
     this._lStickers = [];
     this._$stickyBar = $('#ct-stickyBar_bar');
-    
+    this._$sumUp = $('#ct-sumUp');
+
     this._$stickyBar.find('.ct-arrow_left').click(function() {
       // TODO(fwouts): Use constants.
       self.translateStickers(self._iTranslateX + 300);
     });
-    
+
     this._$stickyBar.find('.ct-arrow_right').click(function() {
       // TODO(fwouts): Use constants.
       self.translateStickers(self._iTranslateX - 300);
     });
-    
+
     this._$stickyBar.find('.ct-container').bind('mousewheel', function(oEvent) {
       // TODO(fwouts): Use constants.
       var iDelta = oEvent.originalEvent.wheelDeltaX * 0.5;
       self.translateStickers(self._iTranslateX + iDelta, true);
       oEvent.preventDefault();
     });
-    
+
     this._$stickyBar.animate({
-      top: '0px'
+      top : '0px'
     }, 'slow', function() {
       self.trigger('ready');
     });
-    
+
     Cotton.UI.World.COMMUNICATOR.on('story_element', function(oStoryElement) {
-      //console.log(oStoryElement.url);
+      // console.log(oStoryElement.url);
     });
   },
-  
+
   // Returns the jQuery element.
-  $: function() {
+  $ : function() {
     return this._$stickyBar;
   },
-  
+
   // Creates a new sticker and adds it to the bar.
-  buildSticker: function(oStory) {
+  buildSticker : function(oStory) {
     // Each sticker will have a different position.
     var iPosition = this._lStickers.length;
     var oSticker = new Cotton.UI.StickyBar.Sticker(this, iPosition, oStory);
     this._lStickers.push(oSticker);
     return oSticker;
   },
-  
-  stickerCount: function() {
+
+  stickerCount : function() {
     return this._lStickers.length;
   },
-  
+
   // TODO(fwouts): Find a way to avoid having to manipulate DOM elements.
-  append: function($sticker) {
+  append : function($sticker) {
     // Note that we append the element to the .container, not directly to the
     // sticky bar.
     this._$stickyBar.find('> .ct-container').append($sticker);
   },
-  
-  translateStickers: function(iTranslateX, bDoNotAnimate) {
+
+  translateStickers : function(iTranslateX, bDoNotAnimate) {
     bDoNotAnimate = bDoNotAnimate || false;
     this._iTranslateX = iTranslateX;
     _.each(this._lStickers, function(oSticker) {
       oSticker.translate(iTranslateX, bDoNotAnimate);
     });
-  }
+  },
 });
 
 _.extend(Cotton.UI.StickyBar.Bar.prototype, Backbone.Events);
--- ui/sticky_bar/sticker.js
+++ ui/sticky_bar/sticker.js
@@ -3,77 +3,100 @@
 Cotton.UI.StickyBar.HORIZONTAL_SPACING = 250;
 
 // Represents a sticker on the sticky bar.
-Cotton.UI.StickyBar.Sticker = Class.extend({
+Cotton.UI.StickyBar.Sticker = Class
+    .extend({
 
-  _oBar : null,
-  _iPosition : null,
-  _oStory : null,
-  _$sticker : null,
+      _oBar : null,
+      _iPosition : null,
+      _oStory : null,
+      _$sticker : null,
 
-  init : function(oBar, iPosition, oStory) {
-    this._oBar = oBar;
-    this._iPosition = iPosition;
-    this._oStory = oStory;
-  },
+      init : function(oBar, iPosition, oStory) {
+        this._oBar = oBar;
+        this._iPosition = iPosition;
+        this._oStory = oStory;
+      },
 
-  display : function() {
-    var self = this;
+      display : function() {
+        var self = this;
 
-    var $sticker = this._$sticker = $('<div class="ct-stickyBar_sticker">');
+        var $sticker = this._$sticker = $('<div class="ct-stickyBar_sticker">');
 
-    var lVisitItems = this._oStory.visitItems();
-    var oLastVisitItem = _.last(lVisitItems);
-    this._oStory.computeTitle();
-    var $title = $('<h3>').text(this._oStory.title());
+        var lVisitItems = this._oStory.visitItems();
+        var oLastVisitItem = _.last(lVisitItems);
+        this._oStory.computeTitle();
+        var $title = $('<h3>').text(this._oStory.title());
 
-    var oExtractedDna = oLastVisitItem.extractedDNA();
-    var sImgSrc = null;
-    // TODO(fwouts): Implement the mainImage method.
-    if (oExtractedDna && oExtractedDna.mainImage) {
-      sImgSrc = oExtractedDna.mainImage();
-    }
-    var $image = $('<img>');
-    if (sImgSrc) {
-      $image.attr('src', sImgSrc);
-    } else {
-      // TODO(fwouts): Add a default image. Or pick another visit item with
-      // an image.
-    }
+        var oExtractedDna = oLastVisitItem.extractedDNA();
+        var sImgSrc = null;
+        // TODO(fwouts): Implement the mainImage method.
+        if (oExtractedDna && oExtractedDna.mainImage) {
+          sImgSrc = oExtractedDna.mainImage();
+        }
+        var $image = $('<img>');
+        if (sImgSrc) {
+          $image.attr('src', sImgSrc);
+        } else {
+          // TODO(fwouts): Add a default image. Or pick another visit item with
+          // an image.
+        }
 
-    $sticker.append($title, $image);
+        $sticker.append($title, $image);
 
-    var iStickerCount = this._oBar.stickerCount();
-    var iFinalPosition = (this._iPosition - iStickerCount / 2)
-        * Cotton.UI.StickyBar.HORIZONTAL_SPACING + this._oBar.$().width() / 2;
-    var iDistanceToCenter = this._oBar.$().width() / 2 - iFinalPosition;
-    var iInitialPosition = iFinalPosition - iDistanceToCenter * 0.2;
+        var iStickerCount = this._oBar.stickerCount();
+        var iFinalPosition = (this._iPosition - iStickerCount / 2)
+            * Cotton.UI.StickyBar.HORIZONTAL_SPACING + this._oBar.$().width()
+            / 2;
+        var iDistanceToCenter = this._oBar.$().width() / 2 - iFinalPosition;
+        var iInitialPosition = iFinalPosition - iDistanceToCenter * 0.2;
 
-    $sticker.css({
-      left : iInitialPosition
-    })
+        $sticker.css({
+          left : iInitialPosition
+        })
 
-    // TODO(fwouts): Use CSS animations.
-    $sticker.animate({
-      left : iFinalPosition
-    }, 'slow', function() {
-      self.trigger('ready');
+        // TODO(fwouts): Use CSS animations.
+        $sticker.animate({
+          left : iFinalPosition
+        }, 'slow', function() {
+          self.trigger('ready');
+        });
+
+        $sticker.hover(function() {
+          // In
+          self.openSumUp();
+        }, function() {
+          // Out
+          self.closeSumUp();
+        });
+        this._oBar.append($sticker);
+      },
+
+      translate : function(iTranslateX, bDoNotAnimate) {
+        bDoNotAnimate = bDoNotAnimate || false;
+        if (bDoNotAnimate) {
+          this._$sticker.stop().css({
+            marginLeft : iTranslateX
+          });
+        } else {
+          this._$sticker.stop().animate({
+            marginLeft : iTranslateX
+          });
+        }
+      },
+
+      openSumUp : function() {
+        var $sumUp = $('.ct-sumUp');
+        $sumUp.append('<ul></ul>');
+        var $sumUpUl = $('.ct-sumUp ul');
+        for ( var i = 0, oVisitItem; oVisitItem = this._oStory.visitItems()[i]; i++) {
+          $sumUpUl.append('<li>' + oVisitItem.title() + '</li>');
+        }
+      },
+
+      closeSumUp : function() {
+        var $sumUpUl = $('.ct-sumUp ul');
+        $sumUpUl.remove();
+      },
     });
-
-    this._oBar.append($sticker);
-  },
-
-  translate : function(iTranslateX, bDoNotAnimate) {
-    bDoNotAnimate = bDoNotAnimate || false;
-    if (bDoNotAnimate) {
-      this._$sticker.stop().css({
-        marginLeft : iTranslateX
-      });
-    } else {
-      this._$sticker.stop().animate({
-        marginLeft : iTranslateX
-      });
-    }
-  }
-});
 
 _.extend(Cotton.UI.StickyBar.Sticker.prototype, Backbone.Events);
