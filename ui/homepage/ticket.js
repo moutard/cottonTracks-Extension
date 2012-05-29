200fd279d672288ff87f24c7e97a5452a7502ddd
Raphael Moutard <raphael.moutard@gmail.com>
2012-05-29 17:56:12 +0200

    UI: add real image for favorites

--- ui/homepage/ticket.js
+++ ui/homepage/ticket.js
@@ -1,26 +1,29 @@
 'use strict';
 
 Cotton.UI.Homepage.Ticket = Class.extend({
-  
+
   _$ticket: null,
-  
+
   init: function(oGrid, sImageUrl, iStoryCount, sTitle) {
     var $ticket = this._$ticket = $('<div class="ct-ticket"></div>');
+    var $ticketImage = $('<img class="ct-ticketImage"/>');
     var $ticketHead = $('<div class="ct-ticketHead"></div>');
     var $ticketCount = $('<div class="ct-ticketCount"><b class="ct-ticketCountNumber">99</b> stories</div>');
     var $ticketTitle = $('<h3></h3>');
-    
+
     $ticket.append(
+        $ticketImage,
         $ticketHead.append(
             $ticketCount,
             $ticketTitle
         )
     );
-    
-    $ticket.css('background-image', 'url(' + sImageUrl + ')');
+
+    $ticketImage.attr('src', sImageUrl);
+    //$ticket.css('background-image', 'url(' + sImageUrl + ')');
     $ticketCount.find('.ct-ticketCountNumber').html(iStoryCount);
     $ticketTitle.text(sTitle);
-    
+
     oGrid.append($ticket);
   }
 });
