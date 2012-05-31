fce1e0bd253122f55110abc380c58c2056e4a50f
Hadri <hadrien.raffalli@gmail.com>
2012-05-31 23:07:19 +0300

    Adding Ticket domain URL and Ticket behavior on hover.

--- ui/homepage/ticket.js
+++ ui/homepage/ticket.js
@@ -4,23 +4,25 @@ Cotton.UI.Homepage.Ticket = Class.extend({
 
   _$ticket: null,
 
-  init: function(oGrid, sImageUrl, iStoryCount, sTitle) {
+    init: function(oGrid, sImageUrl, iStoryCount, sTitle, sSiteUrl) {
     var $ticket = this._$ticket = $('<div class="ct-ticket"></div>');
     var $ticketImage = $('<img class="ct-ticketImage"/>');
     var $ticketHead = $('<div class="ct-ticketHead"></div>');
     var $ticketCount = $('<div class="ct-ticketCount"><b class="ct-ticketCountNumber">99</b> stories</div>');
     var $ticketTitle = $('<h3></h3>');
+    var $ticketLink = $('<a class="ct-ticketLink"></div>'); 
 
-    $ticket.hover(function() {
+    $ticketImage.hover(function() {
           // In
-         $(this).css('-webkit-box-shadow', '0px 0px 4px #666');
+         $(this).css('-webkit-transform', 'translate(0px, -15px)'); 
           console.log('ok');
         }, function() {
           // Out
-          $(this).css('-webkit-box-shadow', '');
+          $(this).css('-webkit-transform', 'translate(0px, 0px)');
     });
-
+    
     $ticket.append(
+      $ticketLink,
         $ticketImage,
         $ticketHead.append(
             $ticketCount,
@@ -28,10 +30,13 @@ Cotton.UI.Homepage.Ticket = Class.extend({
         )
     );
 
+    $ticketLink.append($ticketImage);
     $ticketImage.attr('src', sImageUrl);
     //$ticket.css('background-image', 'url(' + sImageUrl + ')');
     $ticketCount.find('.ct-ticketCountNumber').html(iStoryCount);
     $ticketTitle.text(sTitle);
+    $ticketLink.attr('href', sSiteUrl);
+
 
     oGrid.append($ticket);
   }
