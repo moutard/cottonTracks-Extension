785aca1d1d779222b8228cf715f7820163a57aa5
Hadrien Raffalli <hadrien.raffalli@gmail.com>
2012-06-02 01:09:29 +0200

    Added custom images for tickets on local folder + Integration to the grid

--- ui/homepage/grid.js
+++ ui/homepage/grid.js
@@ -7,14 +7,14 @@ Cotton.UI.Homepage.Grid = Class.extend({
   init : function() {
     this._$homepage = $('<div class="ct-homepage">').appendTo('#ct');
 for (var iI = 0; iI < 1; iI++) {
-      new Cotton.UI.Homepage.Ticket(this, 'http://tctechcrunch.files.wordpress.com/2011/07/logo-3d.png?w=0', Math.floor(Math.random()*80+10), 'Techcrunch','http://techcrunch.com');
-      new Cotton.UI.Homepage.Ticket(this, 'http://blog.agent-influence.com/wp-content/uploads/2010/06/fubiz.png', Math.floor(Math.random()*80+10), 'Fubiz','http://fubiz.net');
-      new Cotton.UI.Homepage.Ticket(this, 'http://www.destination-webmarketing.fr/wp-content/uploads/2011/02/facebook-logo1.jpg', Math.floor(Math.random()*80+10), 'Facebook','http://facebook.com');
-      new Cotton.UI.Homepage.Ticket(this, 'http://drew-hoffman.com/wp-content/uploads/2010/12/dribbble_logo.jpg', Math.floor(Math.random()*80+10), 'Dribbble','http://dribbble.com');
-      new Cotton.UI.Homepage.Ticket(this, 'http://imjustcreative.com/wp-content/uploads/ImJustCreative-2010-01-05-at-15.58.04.jpg', Math.floor(Math.random()*80+10), 'Daring Fireball','http://daringfireball.com');
-      new Cotton.UI.Homepage.Ticket(this, 'https://studies2.hec.fr/jahia/webdav/site/hec/shared/sites/compta-gestion/acces_anonyme/Photos%20HEC/Logo-porte-web.jpg', Math.floor(Math.random()*80+10), 'HEC Paris','http://www.hec.edu');
-      new Cotton.UI.Homepage.Ticket(this, 'http://appleheadlines.com/wp-content/uploads/2011/01/netflix_logo.gif', Math.floor(Math.random()*80+10), 'Netflix','http://netflix.com');
-      new Cotton.UI.Homepage.Ticket(this, 'http://pampelmoose.com/mimg/wired_logo.jpg', Math.floor(Math.random()*80+10), 'Wired','http://wired.com');
+      new Cotton.UI.Homepage.Ticket(this, 'images/home/tickets/TC.jpg', Math.floor(Math.random()*80+10), 'Techcrunch','http://techcrunch.com');
+      new Cotton.UI.Homepage.Ticket(this, 'images/home/tickets/Fubiz.jpg', Math.floor(Math.random()*80+10), 'Fubiz','http://fubiz.net');
+      new Cotton.UI.Homepage.Ticket(this, 'images/home/tickets/FB.jpg', Math.floor(Math.random()*80+10), 'Facebook','http://facebook.com');
+      new Cotton.UI.Homepage.Ticket(this, 'images/home/tickets/Dribbble.jpg', Math.floor(Math.random()*80+10), 'Dribbble','http://dribbble.com');
+      new Cotton.UI.Homepage.Ticket(this, 'images/home/tickets/PandoDaily.jpg', Math.floor(Math.random()*80+10), 'PandoDaily','http://pandodaily.com');
+      new Cotton.UI.Homepage.Ticket(this, 'images/home/tickets/MTV.jpg', Math.floor(Math.random()*80+10), 'MTV','http://www.mtv.com');
+      new Cotton.UI.Homepage.Ticket(this, 'images/home/tickets/Twitter.jpg', Math.floor(Math.random()*80+10), 'Twitter','http://twitter.com');
+      new Cotton.UI.Homepage.Ticket(this, 'images/home/tickets/Pinterest.jpg', Math.floor(Math.random()*80+10), 'Pinterest','http://pinterest.com');  
     }
   },
 
