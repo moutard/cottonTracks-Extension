d27829fd17baed74c329cf297a0c15a4e1115af3
rmoutard <raphael.moutard@gmail.com>
2012-06-01 22:55:13 +0200

    UI : fix grid bugs

--- ui/homepage/ticket.js
+++ ui/homepage/ticket.js
@@ -1,43 +1,37 @@
 'use strict';
 
-Cotton.UI.Homepage.Ticket = Class.extend({
+Cotton.UI.Homepage.Ticket = Class
+    .extend({
 
-  _$ticket: null,
+      _$ticket : null,
 
-    init: function(oGrid, sImageUrl, iStoryCount, sTitle, sSiteUrl) {
-    var $ticket = this._$ticket = $('<div class="ct-ticket"></div>');
-    var $ticketImage = $('<img class="ct-ticketImage"/>');
-    var $ticketHead = $('<div class="ct-ticketHead"></div>');
-    var $ticketCount = $('<div class="ct-ticketCount"><b class="ct-ticketCountNumber">99</b> stories</div>');
-    var $ticketTitle = $('<h3></h3>');
-    var $ticketLink = $('<a class="ct-ticketLink"></div>'); 
+      init : function(oGrid, sImageUrl, iStoryCount, sTitle, sSiteUrl) {
+        var $ticket = this._$ticket = $('<div class="ct-ticket"></div>');
+        var $ticketImage = $('<img class="ct-ticketImage"/>');
+        var $ticketHead = $('<div class="ct-ticketHead"></div>');
+        var $ticketCount = $('<div class="ct-ticketCount"><b class="ct-ticketCountNumber">99</b> stories</div>');
+        var $ticketTitle = $('<h3></h3>');
+        var $ticketLink = $('<a class="ct-ticketLink"></div>');
 
-    $ticketImage.hover(function() {
+        $ticket.hover(function() {
           // In
-         $(this).css('-webkit-transform', 'translate(0px, -15px)'); 
+          $ticketImage.css('-webkit-transform', 'translate(0px, -15px)');
           console.log('ok');
         }, function() {
           // Out
-          $(this).css('-webkit-transform', 'translate(0px, 0px)');
+          $ticketImage.css('-webkit-transform', 'translate(0px, 0px)');
+        });
+
+        $ticket.append($ticketLink, $ticketImage, $ticketHead.append(
+            $ticketCount, $ticketTitle));
+
+        $ticketLink.append($ticketImage);
+        $ticketImage.attr('src', sImageUrl);
+        // $ticket.css('background-image', 'url(' + sImageUrl + ')');
+        $ticketCount.find('.ct-ticketCountNumber').html(iStoryCount);
+        $ticketTitle.text(sTitle);
+        $ticketLink.attr('href', sSiteUrl);
+
+        oGrid.append($ticket);
+      }
     });
-    
-    $ticket.append(
-      $ticketLink,
-        $ticketImage,
-        $ticketHead.append(
-            $ticketCount,
-            $ticketTitle
-        )
-    );
-
-    $ticketLink.append($ticketImage);
-    $ticketImage.attr('src', sImageUrl);
-    //$ticket.css('background-image', 'url(' + sImageUrl + ')');
-    $ticketCount.find('.ct-ticketCountNumber').html(iStoryCount);
-    $ticketTitle.text(sTitle);
-    $ticketLink.attr('href', sSiteUrl);
-
-
-    oGrid.append($ticket);
-  }
-});
