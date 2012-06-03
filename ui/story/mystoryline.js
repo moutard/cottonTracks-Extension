ac815ae5071c733972f3f40a233e340fb8a87ed9
rmoutard <raphael.moutard@gmail.com>
2012-06-03 10:38:57 +0200

    UI : fix closing bug and put animation.

--- ui/story/mystoryline.js
+++ ui/story/mystoryline.js
@@ -8,68 +8,75 @@
    */
   var _oCurrentlyOpenStoryline = null;
 
-  Cotton.UI.Story.Storyline = Class.extend({
-    _$storyHomepage : null,
-    _$storyLine : null,
-    _$storyColLeft : null,
-    _$storyColRight : null,
+  Cotton.UI.Story.Storyline = Class
+      .extend({
+        _$storyHomepage : null,
+        _$storyLine : null,
+        _$storyColLeft : null,
+        _$storyColRight : null,
 
-    /**
-     * A jQuery DOM object representing the vertical line joining all items.
-     */
-    init : function() {
+        /**
+         * A jQuery DOM object representing the vertical line joining all items.
+         */
+        init : function() {
 
-      Cotton.UI.Story.Storyline.removeAnyOpenStoryline();
+          Cotton.UI.Story.Storyline.removeAnyOpenStoryline();
 
-      this._$storyHomepage = $('<div class="ct-story-homepage"></div>');
-      this._$storyLine = $('<div class="ct-mystoryLine"></div>');
-      this._$storyColLeft = $('<div class="ct-storyColLeft"></div>');
-      this._$storyColRight = $('<div class="ct-storyColRight"></div>');
+          this._$storyHomepage = $('<div id="ct-story-homepage" class="clearfix"></div>');
+          this._$storyLine = $('<div class="ct-mystoryLine"></div>');
+          this._$storyColLeft = $('<div class="ct-storyColLeft"></div>');
+          this._$storyColRight = $('<div class="ct-storyColRight"></div>');
 
-      // $('#ct-story-homepage').append(this._$storyLine);
-      // this._$storyLine.append(this._$storyColLeft);
-      // this._$storyLine.append(this._$storyColRight);
+          $('#ct').append(this._$storyHomepage);
+          // this._$storyLine.append(this._$storyColLeft);
+          // this._$storyLine.append(this._$storyColRight);
 
-      $('#ct-story-homepage').append(this._$storyColLeft);
-      $('#ct-story-homepage').append(this._$storyLine);
-      $('#ct-story-homepage').append(this._$storyColRight);
-      $('#ct-story-homepage').css('display', '');
-      $('#ct-story-homepage').addClass('clearfix');
-      // TODO(fwouts): Improve/cleanup.
-      this._$storyLine.css({
-      // height : window.innerHeight
+          this._$storyHomepage.append(this._$storyColLeft);
+          this._$storyHomepage.append(this._$storyLine);
+          this._$storyHomepage.append(this._$storyColRight);
+          this._$storyHomepage.css('display', '');
+          this._$storyHomepage.addClass('clearfix');
+          // TODO(fwouts): Improve/cleanup.
+          this._$storyLine.css({
+          // height : window.innerHeight
+          });
+
+          _oCurrentlyOpenStoryline = this;
+        },
+
+        addVisitItem : function(oVisitItem, sParam) {
+          var oItem = new Cotton.UI.Story.Item(oVisitItem);
+          oItem.$().css("opacity", "0");
+          if (sParam === "left") {
+            oItem.setSide('left');
+            this._$storyColLeft.append(oItem.$());
+
+          } else if (sParam === "right") {
+            oItem.setSide('right');
+            this._$storyColRight.append(oItem.$());
+
+          }
+
+          return oItem;
+        },
+
+        buildStory : function(oVisitItem) {
+          var oItem = new Cotton.UI.Story.Item(oVisitItem);
+          oItem.appendTo(this);
+          return oItem;
+        },
+
+        remove : function() {
+          this._$storyHomepage.remove();
+          // this._$storyLine.remove();
+          this._$storyHomepage = null;
+          _oCurrentlyOpenStoryline = null;
+        },
+
+        $ : function() {
+          return this._$storyHomepage;
+        }
       });
-
-      _oCurrentlyOpenStoryline = this;
-    },
-
-    addVisitItem : function(oVisitItem, sParam) {
-      var oItem = new Cotton.UI.Story.Item(oVisitItem);
-      if (sParam === "left") {
-        oItem.setSide('left');
-        this._$storyColLeft.append(oItem.$());
-      } else if (sParam === "right") {
-        oItem.setSide('right');
-        this._$storyColRight.append(oItem.$());
-      }
-    },
-
-    buildStory : function(oVisitItem) {
-      var oItem = new Cotton.UI.Story.Item(oVisitItem);
-      oItem.appendTo(this);
-      return oItem;
-    },
-
-    remove : function() {
-      this._$storyLine.remove();
-      this._$storyLine = null;
-      _oCurrentlyOpenStoryline = null;
-    },
-
-    $ : function() {
-      return this._$storyLine;
-    }
-  });
 
   // Static methods.
   _.extend(Cotton.UI.Story.Storyline, {
