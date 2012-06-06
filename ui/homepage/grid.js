5feeaa10e113d4323149503627e9255a614bc44b
Raphael Korach <raphael.korach@gmail.com>
2012-06-06 15:18:19 +0200

    adjust cursor

--- ui/homepage/grid.js
+++ ui/homepage/grid.js
@@ -25,12 +25,12 @@ for (var iI = 0; iI < 1; iI++) {
 
   hide : function() {
     this._$homepage.hide();
-    $('.ct-iconButton_home').css('background','url("/images/topbar/home.png")');
+   $('.ct-iconButton_home').css({background:'url("/images/topbar/home.png")', cursor:'pointer'});
   },
 
   show : function() {
     Cotton.UI.Story.Storyline.removeAnyOpenStoryline();
     this._$homepage.show();
-    $('.ct-iconButton_home').css('background','url("/images/topbar/home_selected.png")');
+    $('.ct-iconButton_home').css({background:'url("/images/topbar/home_selected.png")', cursor:'default'});
   }
 });
