36902f79e209b493c18fefc7c9d515c958a03682
Raphael Moutard <raphael.moutard@gmail.com>
2012-06-29 17:45:12 +0200

    Controller : first try of a controller

--- db/populate_db.js
+++ db/populate_db.js
@@ -66,3 +66,72 @@ Cotton.DB.populateDB = function(mCallBackFunction) {
     });
   });
 };
+
+
+Cotton.DB.populateVisitItems = function(oStore, mCallBackFunction) {
+  // Get all the history items from Chrome DB.
+  var self = this;
+  console.debug('PopulateVisitItems - Start');
+  var startTime1 = new Date().getTime();
+  var elapsedTime1 = 0;
+
+  chrome.history.search({
+    text : '',
+    startTime : 0,
+    maxResults : Cotton.Config.Parameters.iMaxResult,
+  }, function(lHistoryItems) {
+    console.debug('PopulateVisitItems - chrome history search has returned '
+        + lHistoryItems.length + ' items');
+    lHistoryItems = Cotton.DB.preRemoveTools(lHistoryItems);
+
+    // TODO(rmoutard): Discuss if we can improve populate using all the
+    // visitItem for each historyItem. For the moment we consider that an
+    // historyItem is the lastVisitItem. Considering more is maybe a useless
+    // complication.
+    var iCount = 0;
+    var iPopulationLength = lHistoryItems.length;
+
+    for ( var i = 0, oHistoryItem; oHistoryItem = lHistoryItems[i]; i++) {
+      var oVisitItem = new Cotton.Model.VisitItem();
+
+      oVisitItem._sUrl = oHistoryItem.url;
+      oVisitItem._sTitle = oHistoryItem.title || '';
+      oVisitItem._iVisitTime = oHistoryItem.lastVisitTime;
+
+      oStore.put('visitItems', oVisitItem, function(iId) {
+        console.debug('PopulateVisitItems - visitItem added');
+        iCount += 1;
+        if (iCount === iPopulationLength) {
+          console.debug('PopulateDB - End');
+          elapsedTime1 = (new Date().getTime() - startTime1) / 1000;
+          console.log('@@Time to PopulateStore : ' + elapsedTime1 + 's');
+          mCallBackFunction.call(self, oStore);
+        }
+      });
+    }
+  });
+};
+
+Cotton.DB.addStories = function(oStore, lStories, mCallBackFunction) {
+  var self = this;
+  console.debug("DB - add stories");
+  var i;
+  for(i = 0; i < lStories.length; i++){
+    var oStory = lStories[lStories.length - 1 - i];
+    oStore.put('stories', oStory, function(iId) {
+      console.log("Story added");
+      if(i === (lStories.length -1)){
+        console.log(i);
+        console.log(oStore);
+        console.log(this);
+        mCallBackFunction.call(self, oStore);
+      }
+    });
+  }
+
+  // TODO(rmoutard) : remove that to use putList.
+  //setTimeout(function(){
+  //  Cotton.UI.oWorld = new Cotton.UI.World();
+  //}, 1000);
+};
+
