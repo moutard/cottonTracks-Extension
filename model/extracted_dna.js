9f69fb12cc5657f25840053e6983e4d3857916f9
Raphael Moutard <raphael.moutard@gmail.com>
2012-07-02 22:24:49 +0200

    Model : remove useless line, according to google closure compiler

--- model/extracted_dna.js
+++ model/extracted_dna.js
@@ -1,11 +1,25 @@
 'use strict';
 
-// This class is used to store all the relevent elements from a page. We extract
-// the DNA of the page, like score on each block, keywords, hightlight...
-// That's why it's called PageDNA.
-// The extractedDNA depends on the visit.
+/**
+ * ExtractedDNA
+ * 
+ * This class is used to store all the relevent elements from a page. We extract
+ * the DNA of the page, like score on each block, keywords, hightlight... That's
+ * why it's called PageDNA. The extractedDNA depends on the visit.
+ */
+
 Cotton.Model.ExtractedDNA = Class.extend({
 
+  _iPercent : 0,
+  _fPageScore : 0,
+  _lScores : [],
+  _lHighlightedText : [],
+  _sImageUrl : "",
+  _sFirstParagraph : "",
+
+  /**
+   * @constructor
+   */
   init : function() {
     this._iPercent = 0;
     this._fPageScore = 0;
@@ -13,10 +27,6 @@ Cotton.Model.ExtractedDNA = Class.extend({
     this._lHighlightedText = [];
     this._sImageUrl = "";
     this._sFirstParagraph = "";
-    // TODO(rmoutard) : see with fwouts the model of textHighLigter
-    // this._lTextHighlighter = [];
-    // this._iScrollCount = 0;
-    // this._lCopyPaste = [];
   },
 
   scores : function() {
@@ -55,14 +65,5 @@ Cotton.Model.ExtractedDNA = Class.extend({
   setFirstParagraph : function(sFirstParagraph) {
     this._sFirstParagraph = sFirstParagraph;
   },
-/*
- * textHighLighter : function() { return this._sTextHighlighter; },
- * setTextHighLighter : function(highLight) {
- * this._sTextHighlighter.push(highLight); }, scrollCount : function() { return
- * this._iScrollCount; }, setScrollCount : function(scrollCount) {
- * this._iScrollCount = scrollCount; }, copyPaste : function() { return
- * this._lCopyPaste; }, setCopyPaste : function(copyPaste) {
- * this._lCopyPaste.push(copyPaste); },
- */
 
 });
