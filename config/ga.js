929564ecc6f2b12d0cc90b4be124b6b6572ed992
Raphael Korach <raphael.korach@gmail.com>
2012-07-03 20:35:19 +0200

    follow analytics guidelines

--- config/ga.js
+++ config/ga.js
@@ -1,11 +1,9 @@
-// Snippet taken from http://code.google.com/chrome/extensions/trunk/tut_analytics.html.
+// Snippet taken from google analytics account
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-30134257-1']);
 _gaq.push(['_trackPageview']);
 (function() {
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
