84fce37fd4dc8a23c4481d8dac5b541b1074771c
Raphael Moutard <raphael.moutard@gmail.com>
2012-07-03 17:05:41 +0200

    Solve parseURL bug

--- compress.sh
+++ compress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# -- GLOBAL VARIABLES --
+# -- GLOBAL VARIABLES ---------------------------------------------------------
 YUICOMPRESSORJAR="/usr/local/rmoutard/yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar"
 GOOGLE_CLOSURE_COMPILER="/usr/local/rmoutard/compiler.jar"
 SOURCE_PATH='/usr/local/rmoutard/sz/'
@@ -24,14 +24,14 @@ cd $DESTINATION_PATH$TAR_NAME$VERSION
 rm -rf test
 echo 'Useless folders have been removed'
 
-# -- COMPILE --
+# -- COMPILE ------------------------------------------------------------------
 COMPILE_COMMAND="java -jar $GOOGLE_CLOSURE_COMPILER "
 COMPILE_OPTIONS="--language_in=ECMASCRIPT5_STRICT"
 # For each folder concatenates all the file and respects the order.
 # Then compile each file.
 
 function generateMinFile {
-  # 3 Parameters
+  # 2 Parameters
   # Array of input files
   declare -a INPUT_FILES=("${!1}")
 
@@ -42,7 +42,7 @@ function generateMinFile {
   echo "'use strict'" > $OUTPUT_FILE
   for file in ${INPUT_FILES[@]}
   do
-    echo "$file"
+    #echo "$file"
     # Remove the first line that correponds to 'use strict'
     sed -i -e "1d" "$file"
     cat "$file" >> $OUTPUT_FILE
@@ -57,31 +57,123 @@ function generateMinFile {
 
 
 # CONFIG
-local config_input_files=("./config/init.js" "./config/config.js")
+config_input_files=("./config/init.js" "./config/config.js")
 config_output_file="config.js"
 
 generateMinFile config_input_files[@] $config_output_file
 
 # DB
 
-local db_input_files=("./db/init.js" "./db/engine.js" "./db/translator.js" "./db/store.js")
+db_input_files=(  "./db/init.js"
+                  "./db/engine.js"
+                  "./db/translator.js"
+                  "./db/store.js")
 db_output_file="db.js"
 
 generateMinFile db_input_files[@] $db_output_file
 
 
 # MODEL
-local model_input_files=("./model/init.js" "./model/story.js" "./model/extracted_dna.js" "./model/visit_item.js")
+model_input_files=( "./model/init.js"
+                    "./model/story.js"
+                    "./model/extracted_dna.js"
+                    "./model/visit_item.js")
 model_output_file="model.js"
 
 generateMinFile model_input_files[@] $model_output_file
 
 # TRANSLATORS
-local translators_input_files=( "./translators/init.js"
-                                "./translators/story_translator.js"
-                                "./translators/visit_item_translators.js")
+translators_input_files=( "./translators/init.js"
+                          "./translators/story_translators.js"
+                          "./translators/visit_item_translators.js")
 translators_output_file="translators.js"
 
 generateMinFile translators_input_files[@] $translators_output_file
 
+# UI
+ui_input_files=(  "./ui/init.js"
+                  "./ui/ui.js"
+                  "./ui/world.js"
+                  "./ui/homepage/init.js"
+                  "./ui/homepage/homepage.js"
+                  "./ui/homepage/favorites_grid.js"
+                  "./ui/homepage/favorites_ticket.js"
+                  "./ui/homepage/apps_grid.js"
+                  "./ui/homepage/apps_ticket.js"
+                  "./ui/homepage/grid.js"
+                  "./ui/homepage/ticket.js"
+                  "./ui/sticky_bar/init.js"
+                  "./ui/sticky_bar/commands.js"
+                  "./ui/sticky_bar/bar.js"
+                  "./ui/sticky_bar/sticker.js"
+                  "./ui/story/init.js"
+                  "./ui/story/default_item.js"
+                  "./ui/story/image_item.js"
+                  "./ui/story/video_item.js"
+                  "./ui/story/item.js"
+                  "./ui/story/mystoryline.js"
+                  "./ui/loading.js")
 
+ui_output_file="ui.js"
+
+generateMinFile ui_input_files[@] $ui_output_file
+
+# -- UPDATE PATH --------------------------------------------------------------
+
+function removePath {
+  # 2 Parameters
+  # Array of input files
+  declare -a USELESS_FILES=("${!1}")
+
+  # Name of the output file
+  INPUT_FILE=$2
+
+  for filename in ${USELESS_FILES[@]}
+  do
+    # remove ./ and escape meta characters.
+    pattern=$(echo ${filename:2} | sed 's/\([[\/.*]\|\]\)/\\&/g')
+    # remove line that insert correponding scripts.
+    sed -i -e "/$pattern/d" "./$INPUT_FILE"
+  done
+
+  echo "Useless files have been removed from $INPUT_FILE"
+
+}
+
+function addPath {
+  # 2 Parameters
+  # Array of input files
+  INCLUDE_FILE=$1
+
+  # Correpondig Tag
+  TAG=$2
+
+  # Name of the output file
+  INPUT_FILE=$3
+
+  sed -i -e "/$TAG/a\
+    <script type='text/javascript' src='$INCLUDE_FILE'></script>
+    " "./$INPUT_FILE"
+
+  echo "$INCLUDE_FILE has been added to $INPUT_FILE"
+
+}
+# INDEX.HTML
+declare -a index_useless_files
+index_useless_files=( ${config_input_files[@]}
+                      ${db_input_files[@]}
+                      ${model_input_files[@]}
+                      ${translators_input_files[@]}
+                      ${ui_input_files[@]}
+                      )
+removePath index_useless_files[@] "index.html"
+
+addPath "config.min.js" "Cotton.config" "index.html"
+addPath "db.min.js" "Cotton.db" "index.html"
+addPath "model.min.js" "Cotton.model" "index.html"
+addPath "translators.min.js" "Cotton.translators" "index.html"
+addPath "ui.min.js" "Cotton.ui" "index.html"
+
+# BACKGROUNG.HTML
+
+# WORKER.JS
