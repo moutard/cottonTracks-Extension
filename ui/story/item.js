7564253ce08e3cd51ee8048d4712d739b8d3dfd9
Raphael Korach <raphael.korach@gmail.com>
2012-07-10 09:20:35 +0100

    Vimeo dailymotion integration

--- ui/story/item.js
+++ ui/story/item.js
@@ -10,7 +10,11 @@ Cotton.UI.Story.ItemFactory = function(oVisitItem) {
   if (reg.exec(oVisitItem.url())) {
     return new Cotton.UI.Story.ImageItem(oVisitItem);
   } else if (oUrl.host === "www.youtube.com" && oUrl.dSearch['v']) {
-    return new Cotton.UI.Story.VideoItem(oVisitItem, oUrl.dSearch['v']);
+    return new Cotton.UI.Story.VideoItem(oVisitItem, "youtube", oUrl.dSearch['v']);
+  } else if (oUrl.host === "vimeo.com" && oUrl.pathname.substr(1).match(/[0-9]+/)){
+  	return new Cotton.UI.Story.VideoItem(oVisitItem, "vimeo", oUrl.substr(1));
+  } else if (oUrl.host === "www.dailymotion.com" && oUrl.pathname.split('/')[1] == "video"){
+  	return new Cotton.UI.Story.VideoItem(oVisitItem, "dailymotion", oUrl.pathname.split('/')[2]);  	
   } else {
     return new Cotton.UI.Story.DefaultItem(oVisitItem);
   }
