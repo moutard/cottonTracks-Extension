42eca206bf1cec1f3f053a65ef00b9f890686379
Raphael Korach <raphael.korach@gmail.com>
2012-07-18 23:05:00 +0100

    Fix vimeo integration for channels.

--- ui/story/item.js
+++ ui/story/item.js
@@ -6,13 +6,13 @@ Cotton.UI.Story.ItemFactory = function(oVisitItem) {
   oUrl.fineDecomposition();
   // Detect if it's an image.
   var reg = new RegExp(".(jpg|png|gif)$", "g");
-  var sLastString = oUrl.pathname.split(oUrl.pathname.split('/').length-1);
+  var sLastString = oUrl.pathname.split('/')[oUrl.pathname.split('/').length-1];
 
   if (reg.exec(oVisitItem.url())) {
     return new Cotton.UI.Story.ImageItem(oVisitItem);
   } else if (oUrl.host === "www.youtube.com" && oUrl.dSearch['v']) {
     return new Cotton.UI.Story.VideoItem(oVisitItem, "youtube", oUrl.dSearch['v']);
-  } else if (oUrl.host === "vimeo.com" && sLastString.match(/[0-9]+/)) {
+  } else if (oUrl.host === "vimeo.com" && oUrl.pathname.match(/(\/[0-9]+)$/)) {
   	return new Cotton.UI.Story.VideoItem(oVisitItem, "vimeo", sLastString);
   } else if (oUrl.host === "www.dailymotion.com" && oUrl.pathname.split('/')[1] == "video") {
   	return new Cotton.UI.Story.VideoItem(oVisitItem, "dailymotion", oUrl.pathname.split('/')[2]);  	
