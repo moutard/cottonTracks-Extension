ca201ecd42e484c4e08fb5039c34f084f04b13e5
Raphael Moutard <raphael.moutard@gmail.com>
2012-07-22 12:12:51 +0200

    DB : to solve compress bug

--- db/expand_store.js
+++ db/expand_store.js
@@ -4,23 +4,23 @@
  * Add usefull function to expand store prototype.
  */
 
-
 /**
  * Stories
  */
 Cotton.DB.Stories = {};
 
 Cotton.DB.Stories.addStories = function(oStore, lStories, mCallBackFunction) {
+  console.log(lStories);
   console.debug("DB - add stories");
   var iLength = lStories.length - 1;
   var iCount = 0;
-  for(var i = 0; i < lStories.length; i++){
+  for ( var i = 0; i < lStories.length; i++) {
     var oStory = lStories[lStories.length - 1 - i];
     oStore.put('stories', oStory, function(iId) {
-      if(iCount === iLength){
+      if (iCount === iLength) {
         mCallBackFunction(oStore);
       }
-      iCount +=1;
+      iCount += 1;
     });
   }
 };
@@ -57,37 +57,35 @@ Cotton.DB.Stories.getRange = function(iX, iY, mCallBackFunction) {
   });
 };
 
+Cotton.DB.Stories.getXStories = function(iX, mCallBackFunction) {
+  new Cotton.DB.Store('ct', {
+    'stories' : Cotton.Translators.STORY_TRANSLATORS
+  }, function() {
+    this.getXItems('stories', 10, 'fLastVisitTime', "PREV", function(lStories) {
 
-Cotton.DB.Stories.getXStories = function(iX, mCallBackFunction){
-  new Cotton.DB.Store('ct',
-      { 'stories': Cotton.Translators.STORY_TRANSLATORS },
-      function() {
-       this.getXItems('stories', 10, 'fLastVisitTime', "PREV",
-           function(lStories) {
+      new Cotton.DB.Store('ct', {
+        'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS
+      }, function() {
+        var count = 0;
+        var lStoriesTemp = lStories;
+        for ( var i = 0; i < lStoriesTemp.length; i++) {
+          var oStory = lStoriesTemp[i];
+          this.findGroup('visitItems', 'id', oStory.visitItemsId(), function(
+              lVisitItems) {
 
-         new Cotton.DB.Store('ct',
-             {'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS },
-             function(){
-               var count = 0;
-               var lStoriesTemp = lStories;
-               for(var i=0; i < lStoriesTemp.length; i++){
-                 var oStory = lStoriesTemp[i];
-                 this.findGroup('visitItems', 'id', oStory.visitItemsId(),
-                     function(lVisitItems){
+            lStoriesTemp[count].setVisitItems(lVisitItems);
 
-                       lStoriesTemp[count].setVisitItems(lVisitItems);
-
-                       if(count == (lStoriesTemp.length - 1)){
-                         console.log('forFwouts');
-                         console.log(lStoriesTemp);
-                         mCallBackFunction(lStoriesTemp);
-                       }
-                       count++;
-                 });
-               }
-             });
-       });
-     });
+            if (count == (lStoriesTemp.length - 1)) {
+              console.log('forFwouts');
+              console.log(lStoriesTemp);
+              mCallBackFunction(lStoriesTemp);
+            }
+            count++;
+          });
+        }
+      });
+    });
+  });
 };
 /**
  * VisitItems
