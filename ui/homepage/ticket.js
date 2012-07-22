a1829804b0a0183ee6ff735ef7aa816ae4b4af3d
Hadri <hadrien.raffalli@gmail.com>
2012-07-22 17:40:46 +0200

    Apply responsive grid from dev

--- ui/homepage/ticket.js
+++ ui/homepage/ticket.js
@@ -8,13 +8,17 @@ Cotton.UI.Homepage.Ticket = Class
       init : function(oGrid, sImageUrl, iStoryCount, sTitle, sSiteUrl) {
         var $ticket = this._$ticket = $('<div class="ct-ticket"></div>');
         var $ticketImage = $('<img class="ct-ticketImage"/>');
-        var $ticketHead = $('<div class="ct-ticketHead"></div>');
+        //var $ticketHead = $('<div class="ct-ticketHead"></div>')
+        var $ticketEnvelope = $('<img class="ct-ticketEnvelope" src ="/media/images/home/ticket_bottom.png">')
         var $ticketCount = $('<div class="ct-ticketCount"><b class="ct-ticketCountNumber">99</b> stories</div>');
         var $ticketTitle = $('<h3></h3>');
         var $ticketLink = $('<a class="ct-ticketLink"></div>');
 
-        $ticket.append($ticketLink, $ticketImage, $ticketHead.append(
-            $ticketCount, $ticketTitle));
+       // $ticket.append($ticketLink, $ticketImage, $ticketHead.append(
+       //     $ticketCount, $ticketTitle));
+       
+       //test
+       $ticket.append($ticketLink, $ticketEnvelope, $ticketImage, $ticketCount, $ticketTitle);
 
         $ticketLink.append($ticketImage);
         $ticketImage.attr('src', sImageUrl);
