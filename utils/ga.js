20d9369252e7d9980f9a932c7899b0b4e0b3dfe0
Raphael MOUTARD <rmoutard@ltutech.com>
2012-08-08 13:39:10 +0200

    Compress : compress ga.js

--- utils/ga.js
+++ utils/ga.js
@@ -5,9 +5,12 @@ if (Cotton.Config.Parameters.bAnalytics === true) {
   _gaq.push(['_trackPageview']);
   (function() {
     (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = 'https://ssl.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      var ga = document.createElement('script');
+      ga['type'] = 'text/javascript';
+      ga['async'] = true;
+      ga['src'] = 'https://ssl.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(ga, s);
     })();
   })();
 }
