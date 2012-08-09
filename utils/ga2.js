372fa3ff585cfabbc4c5a61435e3251f3fc597e7
Raphael MOUTARD <rmoutard@ltutech.com>
2012-08-09 14:49:42 +0200

    GA : first method of ANALYTICS

--- utils/ga2.js
+++ utils/ga2.js
@@ -13,20 +13,44 @@ Cotton.Analytics = Class.extend({
     //self._sAccount = Cotton.Config.Parameters._sAccount;
 
     self._gaq = _gaq || [];
+
+
+  if (Cotton.Config.Parameters.bAnalytics === true) {
     _gaq.push(['_setAccount', 'UA-30134257-1']);
     _gaq.push(['_trackPageview']);
 
-    self._oGoogleAnalytics = document.createElement('script');
-    self._oGoogleAnalytics.type = 'text/javascript';
-    self._oGoogleAnalytics.async = true;
-    self._oGoogleAnalytics.src = 'https://ssl.google-analytics.com/ga.js';
-
-    var oScript = document.getElementsByTagName('script')[0];
-    oScript.parentNode.insertBefore(self._oGoogleAnalytics, oScript);
+      self._oGoogleAnalytics = document.createElement('script');
+      self._oGoogleAnalytics.type = 'text/javascript';
+      self._oGoogleAnalytics.async = true;
+      self._oGoogleAnalytics.src = 'https://ssl.google-analytics.com/ga.js';
+      var oScript = document.getElementsByTagName('script')[0];
+      oScript.parentNode.insertBefore(self._oGoogleAnalytics, oScript);
+    }
   },
 
-});
-Cotton.oAnalytics = new Cotton.Analytics();
+  enterStory : function(){
+    _gaq.push([ '_trackEvent', 'Story trafic',
+                'Enter story', 'Click on sticker']);
+  },
 
-_.extend(Cotton.oAnalytics, Backbone.Events);
+  modifyStory = function(){
+    _gaq.push(['_trackEvent', 'Story modification', 'modify']);
+  },
+
+  deleteStory = function(){
+    _gaq.push(['_trackEvent', 'Story modification', 'delete']);
+  },
+
+  editModeOn = function(){
+    _gaq.push([ '_trackEvent', 'Story modification', 'Edit on' ]);
+  },
+
+  editModeoff = function(){
+    _gaq.push([ '_trackEvent', 'Story modification', 'Edit off' ]);
+  },
+});
+
+Cotton.ANALYTICS = new Cotton.Analytics();
+
+_.extend(Cotton.ANALYTICS, Backbone.Events);
 })();
