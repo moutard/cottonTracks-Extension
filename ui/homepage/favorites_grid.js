d2c1926e4e009b2c40c1cec3a53d6e4ab2bbfda8
Raphael Moutard <raphael.moutard@gmail.com>
2012-08-12 00:11:30 +0200

    Refactor : indent

--- ui/homepage/favorites_grid.js
+++ ui/homepage/favorites_grid.js
@@ -8,32 +8,32 @@ Cotton.UI.Homepage.FavoritesGrid = Class
       init : function() {
         this._$FavoritesGrid = $('<div class="ct-favorites-grid">');
         for ( var iI = 0; iI < 1; iI++) {
-          new Cotton.UI.Homepage.Ticket(this, '/media/images/home/tickets/TC.jpg',
-              Math.floor(Math.random() * 80 + 10), 'Techcrunch',
+          new Cotton.UI.Homepage.Ticket(this,
+              '/media/images/home/tickets/TC.jpg', Math
+                  .floor(Math.random() * 80 + 10), 'Techcrunch',
               'http://techcrunch.com');
-          new Cotton.UI.Homepage.Ticket(this, '/media/images/home/tickets/Fubiz.jpg',
-              Math.floor(Math.random() * 80 + 10), 'Fubiz', 'http://fubiz.net');
-          new Cotton.UI.Homepage.Ticket(this, '/media/images/home/tickets/FB.jpg',
-              Math.floor(Math.random() * 80 + 10), 'Facebook',
+          new Cotton.UI.Homepage.Ticket(this,
+              '/media/images/home/tickets/Fubiz.jpg', Math
+                  .floor(Math.random() * 80 + 10), 'Fubiz', 'http://fubiz.net');
+          new Cotton.UI.Homepage.Ticket(this,
+              '/media/images/home/tickets/FB.jpg', Math
+                  .floor(Math.random() * 80 + 10), 'Facebook',
               'http://facebook.com');
           new Cotton.UI.Homepage.Ticket(this,
-              '/media/images/home/tickets/Dribbble.jpg', Math
-                  .floor(Math.random() * 80 + 10), 'Dribbble',
-              'http://dribbble.com');
+              '/media/images/home/tickets/Dribbble.jpg', Math.floor(Math
+                  .random() * 80 + 10), 'Dribbble', 'http://dribbble.com');
           new Cotton.UI.Homepage.Ticket(this,
-              '/media/images/home/tickets/PandoDaily.jpg', Math
-                  .floor(Math.random() * 80 + 10), 'PandoDaily',
-              'http://pandodaily.com');
-          new Cotton.UI.Homepage.Ticket(this, '/media/images/home/tickets/MTV.jpg',
-              Math.floor(Math.random() * 80 + 10), 'MTV', 'http://www.mtv.com');
+              '/media/images/home/tickets/PandoDaily.jpg', Math.floor(Math
+                  .random() * 80 + 10), 'PandoDaily', 'http://pandodaily.com');
           new Cotton.UI.Homepage.Ticket(this,
-              '/media/images/home/tickets/Twitter.jpg', Math
-                  .floor(Math.random() * 80 + 10), 'Twitter',
-              'http://twitter.com');
+              '/media/images/home/tickets/MTV.jpg', Math
+                  .floor(Math.random() * 80 + 10), 'MTV', 'http://www.mtv.com');
           new Cotton.UI.Homepage.Ticket(this,
-              '/media/images/home/tickets/Pinterest.jpg', Math
-                  .floor(Math.random() * 80 + 10), 'Pinterest',
-              'http://pinterest.com');
+              '/media/images/home/tickets/Twitter.jpg', Math.floor(Math
+                  .random() * 80 + 10), 'Twitter', 'http://twitter.com');
+          new Cotton.UI.Homepage.Ticket(this,
+              '/media/images/home/tickets/Pinterest.jpg', Math.floor(Math
+                  .random() * 80 + 10), 'Pinterest', 'http://pinterest.com');
         }
       },
 
