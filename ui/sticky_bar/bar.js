dca5b4e8c103c096af7555c50154046028d4e52b
rmoutard <raphael.moutard@gmail.com>
2012-08-11 23:06:38 +0200

    Issue #20 : stop translate even for the arrows.

--- ui/sticky_bar/bar.js
+++ ui/sticky_bar/bar.js
@@ -34,72 +34,124 @@ Cotton.UI.StickyBar.Bar = Class.extend({
     var bScrolled = false;
 
     // CLICK - On arrow left
-    this._$stickyBar.find('.ct-arrow_left').click(
-        function() {
-          // TODO(fwouts): Use constants.
-          self.translateStickers(self._iTranslateX + 250);
-          if (bScrolled == false) {
-            bScrolled = true;
-            // Event Tracking
-            Cotton.ANALYTICS.scrollWithLeftArrow();
-          }
-        });
+    this._$stickyBar.find('.ct-arrow_left').click(function() {
+      // TODO(fwouts): Use constants.
+
+      var iDelta = 250;
+      var iPosition = self._iTranslateX + iDelta;
+      // position = (1-|K/L|)*(L-K/2)*H + M + M'
+      var K = 10 // self._lStickers.length initial
+      var H = Cotton.UI.StickyBar.HORIZONTAL_SPACING;
+      var M1 = 100;
+      var M2 = 100;
+      var p = (2 * self.$().width()) / 3;
+      // var iPositionMax = (1 - Math.floor(K / self._lStickers.length))
+      // * (self._lStickers.length - K / 2) * H + M1;
+      var iPositionMax = (self._lStickers.length * H) - p + M1;
+      // TODO(rmoutard) : use constants
+      if (iPosition > 580.0) {
+        iPosition = 580;
+      } else if (iPosition < -(iPositionMax + M2)) {
+        // Stop the scroll.
+        iPosition = -(iPositionMax + M2);
+      } else if (iPosition < -(iPositionMax)) {
+        // Update the stickers bar.
+        if (self._bLoading === false) {
+          self._bLoading = true;
+          self.getMoreStories();
+        }
+
+      }
+
+      self.translateStickers(iPosition);
+
+      // Event Tracker on first scroll
+      if (bScrolled == false) {
+        bScrolled = true;
+        Cotton.ANALYTICS.scrollWithLeftArrow();
+      }
+    });
 
     // CLICK - On arrow right
-    this._$stickyBar.find('.ct-arrow_right').click(
-        function() {
-          // TODO(fwouts): Use constants.
-          self.translateStickers(self._iTranslateX - 250);
-          if (bScrolled == false) {
-            bScrolled = true;
-            // Event Tracking
-            Cotton.ANALYTICS.scrollWithRightArrow();
-          }
-        });
+    this._$stickyBar.find('.ct-arrow_right').click(function() {
+      // TODO(fwouts): Use constants.
+
+      var iDelta = -250;
+      var iPosition = self._iTranslateX + iDelta;
+      // position = (1-|K/L|)*(L-K/2)*H + M + M'
+      var K = 10 // self._lStickers.length initial
+      var H = Cotton.UI.StickyBar.HORIZONTAL_SPACING;
+      var M1 = 100;
+      var M2 = 100;
+      var p = (2 * self.$().width()) / 3;
+      // var iPositionMax = (1 - Math.floor(K / self._lStickers.length))
+      // * (self._lStickers.length - K / 2) * H + M1;
+      var iPositionMax = (self._lStickers.length * H) - p + M1;
+      // TODO(rmoutard) : use constants
+      if (iPosition > 580.0) {
+        iPosition = 580;
+      } else if (iPosition < -(iPositionMax + M2)) {
+        // Stop the scroll.
+        iPosition = -(iPositionMax + M2);
+      } else if (iPosition < -(iPositionMax)) {
+        // Update the stickers bar.
+        if (self._bLoading === false) {
+          self._bLoading = true;
+          self.getMoreStories();
+        }
+
+      }
+
+      self.translateStickers(iPosition);
+
+      // Event Tracker on first scroll
+      if (bScrolled == false) {
+        bScrolled = true;
+        Cotton.ANALYTICS.scrollWithRightArrow();
+      }
+    });
 
     // MOUSEWHEEL
-    this._$stickyBar.find('.ct-container').bind(
-        'mousewheel',
-        function(oEvent) {
-          // TODO(fwouts): Use constants.
-          if (oEvent['originalEvent']['wheelDeltaX'] === 0) {
-            var iDelta = oEvent['originalEvent']['wheelDeltaY'] * 0.5;
-          } else {
-            var iDelta = oEvent['originalEvent']['wheelDeltaX'] * 0.5;
-          }
-          var iPosition = self._iTranslateX + iDelta;
-          // position = (1-|K/L|)*(L-K/2)*H + M + M'
-          var K = 10 // self._lStickers.length initial
-          var H = Cotton.UI.StickyBar.HORIZONTAL_SPACING;
-          var M1 = 100;
-          var M2 = 100;
-          var p = (2 * self.$().width()) / 3;
-          // var iPositionMax = (1 - Math.floor(K / self._lStickers.length))
-          // * (self._lStickers.length - K / 2) * H + M1;
-          var iPositionMax = (self._lStickers.length * H) - p + M1;
-          if (iPosition > 580.0) {
-            iPosition = 580;
-          } else if (iPosition < -(iPositionMax + M2)) {
-            // Stop the scroll.
-            iPosition = -(iPositionMax + M2);
-          } else if (iPosition < -(iPositionMax)) {
-            // Update the stickers bar.
-            if (self._bLoading === false) {
-              self._bLoading = true;
-              self.getMoreStories();
-            }
+    this._$stickyBar.find('.ct-container').bind('mousewheel', function(oEvent) {
+      // TODO(fwouts): Use constants.
+      if (oEvent['originalEvent']['wheelDeltaX'] === 0) {
+        var iDelta = oEvent['originalEvent']['wheelDeltaY'] * 0.5;
+      } else {
+        var iDelta = oEvent['originalEvent']['wheelDeltaX'] * 0.5;
+      }
+      var iPosition = self._iTranslateX + iDelta;
+      // position = (1-|K/L|)*(L-K/2)*H + M + M'
+      var K = 10 // self._lStickers.length initial
+      var H = Cotton.UI.StickyBar.HORIZONTAL_SPACING;
+      var M1 = 100;
+      var M2 = 100;
+      var p = (2 * self.$().width()) / 3;
+      // var iPositionMax = (1 - Math.floor(K / self._lStickers.length))
+      // * (self._lStickers.length - K / 2) * H + M1;
+      var iPositionMax = (self._lStickers.length * H) - p + M1;
+      if (iPosition > 580.0) {
+        iPosition = 580;
+      } else if (iPosition < -(iPositionMax + M2)) {
+        // Stop the scroll.
+        iPosition = -(iPositionMax + M2);
+      } else if (iPosition < -(iPositionMax)) {
+        // Update the stickers bar.
+        if (self._bLoading === false) {
+          self._bLoading = true;
+          self.getMoreStories();
+        }
 
-          }
+      }
 
-          self.translateStickers(iPosition, true);
-          oEvent.preventDefault();
+      self.translateStickers(iPosition, true);
+      oEvent.preventDefault();
 
-          // Event Tracker on first scroll
-          if (bScrolled == false) {
-            bScrolled = true;
-            Cotton.ANALYTICS.scrollStorySelector();
-          }
-        });
+      // Event Tracker on first scroll
+      if (bScrolled == false) {
+        bScrolled = true;
+        Cotton.ANALYTICS.scrollStorySelector();
+      }
+    });
 
     this._$stickyBar.animate({
       top : '0px'
