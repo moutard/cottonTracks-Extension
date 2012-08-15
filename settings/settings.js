07a348cdb384d75f6aadf1ca668dbe614252bf4b
Raphael Moutard <raphael.moutard@gmail.com>
2012-08-15 19:48:00 +0200

    Settings : history export and import

--- settings/settings.js
+++ settings/settings.js
@@ -1,13 +1,12 @@
-window.onload = function(){
+window.onload = function() {
 
   /**
-   * PRE FILLED
-   * filled all the settings with value in the local storage, or with default
-   * value
+   * PRE FILLED filled all the settings with value in the local storage, or with
+   * default value
    */
 
   // GRID
-  if(localStorage['ct-settings_grid'] === "MostVisited"){
+  if (localStorage['ct-settings_grid'] === "MostVisited") {
     $('#grid input[value=MostVisited]').attr("checked", "checked");
   } else {
     $('#grid input[value=Favorites]').attr("checked", "checked");
@@ -15,60 +14,53 @@ window.onload = function(){
 
   // Favorites
   var favorites = localStorage['ct-settings_favorites'];
-  if(!favorites || favorites === "undefined"){
-    favorites = {'favorites' : [
-      {
-        'title':'Techcrunch',
+  if (!favorites || favorites === "undefined") {
+    favorites = {
+      'favorites' : [ {
+        'title' : 'Techcrunch',
         'url' : 'http://techcrunch.com',
         'thumbnail' : '/media/images/home/tickets/TC.jpg'
-      },
-      {
-        'title': 'Fubiz',
+      }, {
+        'title' : 'Fubiz',
         'url' : 'http://fubiz.net',
         'thumbnail' : '/media/images/home/tickets/Fubiz.jpg'
-      },
-      {
-        'title': 'Facebook',
+      }, {
+        'title' : 'Facebook',
         'url' : 'http://facebook.com',
         'thumbnail' : '/media/images/home/tickets/FB.jpg'
-      },
-      {
-        'title': 'Dribble',
+      }, {
+        'title' : 'Dribble',
         'url' : 'http://dribbble.com',
-        'thumbnail' :'/media/images/home/tickets/Dribbble.jpg'
-      },
-      {
-        'title': 'PandoDaily',
+        'thumbnail' : '/media/images/home/tickets/Dribbble.jpg'
+      }, {
+        'title' : 'PandoDaily',
         'url' : 'http://pandodaily.com',
-        'thumbnail' :'/media/images/home/tickets/PandoDaily.jpg'
-      },
-      {
-        'title': 'MTV',
+        'thumbnail' : '/media/images/home/tickets/PandoDaily.jpg'
+      }, {
+        'title' : 'MTV',
         'url' : 'http://www.mtv.com',
-        'thumbnail' :'/media/images/home/tickets/MTV.jpg'
-      },
-      {
-        'title': 'Twitter',
+        'thumbnail' : '/media/images/home/tickets/MTV.jpg'
+      }, {
+        'title' : 'Twitter',
         'url' : 'http://twitter.com',
-        'thumbnail' :'/media/images/home/tickets/Twitter.jpg'
-      },
-      {
-        'title': 'Pinterest',
+        'thumbnail' : '/media/images/home/tickets/Twitter.jpg'
+      }, {
+        'title' : 'Pinterest',
         'url' : 'http://pinterest.com',
-        'thumbnail' :'/media/images/home/tickets/Pinterest.jpg'
+        'thumbnail' : '/media/images/home/tickets/Pinterest.jpg'
       },
 
-      ]};
+      ]
+    };
   } else {
     favorites = JSON.parse(favorites);
   }
 
-  for(var i = 0, oFavorite; oFavorite = favorites['favorites'][i]; i++){
-    $('#grid input[name=favorites_title]:eq('+ i +')').val(oFavorite['title']);
-    $('#grid input[name=favorites_url]:eq('+ i +')').val(oFavorite['url']);
+  for ( var i = 0, oFavorite; oFavorite = favorites['favorites'][i]; i++) {
+    $('#grid input[name=favorites_title]:eq(' + i + ')')
+        .val(oFavorite['title']);
+    $('#grid input[name=favorites_url]:eq(' + i + ')').val(oFavorite['url']);
   }
-
-
 
   // ACTION
   // Store the settings using localStorage
@@ -77,13 +69,73 @@ window.onload = function(){
   });
 
   //
-  $('#grid').submit(function() {
-    for(var i = 0, oFavorite; oFavorite = favorites['favorites'][i]; i++){
-      oFavorite['title'] = $('#grid input[name=favorites_title]:eq('+ i +')').val();
-      oFavorite['url'] = $('#grid input[name=favorites_url]:eq('+ i +')').val();
-    }
+  $('#grid')
+      .submit(
+          function() {
+            for ( var i = 0, oFavorite; oFavorite = favorites['favorites'][i]; i++) {
+              oFavorite['title'] = $(
+                  '#grid input[name=favorites_title]:eq(' + i + ')').val();
+              oFavorite['url'] = $(
+                  '#grid input[name=favorites_url]:eq(' + i + ')').val();
+            }
 
-    localStorage['ct-settings_favorites'] = JSON.stringify(favorites);
-  });
+            localStorage['ct-settings_favorites'] = JSON.stringify(favorites);
+          });
+
+  // EXPORT
+  // IMPORT
+  function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    // Loop through the FileList and render image files as thumbnails.
+    for ( var i = 0, f; f = files[i]; i++) {
+
+      // Only process image files.
+      // if (!f.type.match('image.*')) {
+      // continue;
+      // }
+
+      var reader = new FileReader();
+
+      // Closure to capture the file information.
+      reader.onload = (function(theFile) {
+        return function(e) {
+          // Render thumbnail.
+          console.log(e.target.result)
+          Cotton.Utils.importHistory(e.target.result);
+        };
+      })(f);
+
+      // Read in the image file as a data URL.
+      reader.readAsDataURL(f);
+    }
+  }
+
+  // SAVE
 
 };
+
+// EXPORT
+(function(view) {
+  "use strict";
+  var document = view.document, $ = function(id) {
+    return document.getElementById(id);
+  }
+  // only get URL when necessary in case BlobBuilder.js hasn't defined it yet
+  , get_blob_builder = function() {
+    return view.BlobBuilder || view.WebKitBlobBuilder || view.MozBlobBuilder;
+  }
+
+  , text = $("text"), text_options_form = $("text-options"), text_filename = $("text-filename")
+
+  text_options_form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var BB = get_blob_builder();
+    var bb = new BB;
+    Cotton.Utils.exportHistory(function(sHistory) {
+      bb.append(sHistory);
+      saveAs(bb.getBlob("text/plain;charset=" + document.characterSet),
+          "cottonTracks-history.json");
+    });
+  }, false);
+}(self));
