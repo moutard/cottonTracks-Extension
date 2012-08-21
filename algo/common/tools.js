04f61411ec66116635563f88347c7d03af0b5261
Raphael MOUTARD <rmoutard@ltutech.com>
2012-08-21 13:36:45 +0200

    Algo : tools commonWords

--- algo/common/tools.js
+++ algo/common/tools.js
@@ -40,7 +40,7 @@ Cotton.Algo.Tools.extractWordsUrl = function(sUrl) {
   // For the moment exactly the same than extractWords. But just in case we
   // will keep two different functions.
   var oRegexp = /[\w\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+/g;
-  var lMatches = sUrl.match(oRegexp) || [];
+  var lMatches = sTitle.match(oRegexp) || [];
 
   // Lower case to compare correctly.
   for ( var i = 0; i < lMatches.length; i++) {
@@ -57,30 +57,29 @@ Cotton.Algo.Tools.extractWordsUrl = function(sUrl) {
  * Get the number of common words in the title of two visitItems.
  *
  * @param {Object}
- *          oVisitItem1
+ *          oVisitItem1 : pre_treatment computeExtractedWords is needed
  * @param {Object}
- *          oVisitItem2
+ *          oVisitItem2 : pre_treatment computeExtractedWords is needed
  * @returns {int}
  */
 Cotton.Algo.Tools.commonWords = function(oVisitItem1, oVisitItem2) {
-  var lWords1, lWords2, lCommonWords;
 
-  if (oVisitItem1['sTitle'] === "" && oVisitItem2['sTitle'] === "") {
+  if (oVisitItem1['sTitle'] === "" || oVisitItem2['sTitle'] === "") {
     return -1;
   }
 
   if (oVisitItem1['lExtractedWords'] === undefined
       || oVisitItem2['lExtractWords'] === undefined) {
-    // this part may be deleted but may be usefull in some case.
-    lWords1 = Cotton.Algo.Tools.extractWords(oVisitItem1['sTitle']);
-    lWords2 = Cotton.Algo.Tools.extractWords(oVisitItem2['sTitle']);
+    Cotton.Utils.error("lExtractedWords has not been computed. Before using the method.");
+    return -1;
+  } else if(oVisitItem1['lExtractedWords'].length === 0
+      || oVisitItem2['lExtractedWords'].length === 0 ){
+    // We can't compare.
+    return -1;
   } else {
-    // already computed in preTreatment
-    lWords1 = oVisitItem1['sExtractedWords'];
-    lWords2 = oVisitItems2['sExtractedWords'];
+    // return the number of common words.
+    return _.intersection(oVisitItem1['lExtractedWords'],
+                          oVisitItem2['lExtractedWords']).length;
   }
-
-  lCommonWords = _.intersection(lWords1, lWords2);
-  return lCommonWords.length;
 };
 
