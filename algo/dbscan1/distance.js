bd1009a6f13f4671a03a5a4340c5c8ca42c0d02c
Raphael Moutard <raphael.moutard@gmail.com>
2012-08-21 08:47:49 +0200

    Algo : distance meaning

--- algo/dbscan1/distance.js
+++ algo/dbscan1/distance.js
@@ -115,11 +115,37 @@ Cotton.Algo.Distance.distanceKey = function(sKey, oObject1, oObject2) {
  */
 Cotton.Algo.Distance.meaning = function(oVisitItem, oVisitItem) {
 
+  var coeff = Cotton.Config.Parameters.distanceCoeff;
+
+  // Common words
+  // number of common words is high => items close
+  // ordre de grandeur = O(5)
+  // close if 0(1) far if 0.
+  var iCommonWords = Cotton.Algo.Tools.commonWords(oVisitItem1, oVisitItem2);
+  if (iCommonWords === 0) {
+    sum += coeff.penalty; // try to detect parallel stories.
+  } else if (iCommonWords === -1) {
+    // if there is no title do not put the penalty
+    // or put low penalty
+    sum += coeff.penalty;
+  } else {
+    sum -= (coeff.commonWords * (1 + iCommonWords)) / 10;
+  }
+
+  // Query keywords
+  var iCommonQueryKeywords = Cotton.Algo.distanceBetweenGeneratedPages(
+      oVisitItem1, oVisitItem2);
+  sum += (coeff.queryKeywords / ((1 + iCommonQueryKeywords) * (1 + iCommonQueryKeywords)));
+
+  return sum;
 };
 
-// TODO(rmoutard) : Write a better distance, maybe to keep it between [0,1]
-// for instance you need to balance common words
+/**
+ * Compute distance that use every criteria.
+ */
 Cotton.Algo.distanceComplexe = function(oVisitItem1, oVisitItem2) {
+  // TODO(rmoutard) : Write a better distance, maybe to keep it between [0,1]
+  // for instance you need to balance common words
 
   // TODO: (rmoutard) write a class for coefficients
   var coeff = Cotton.Config.Parameters.distanceCoeff;
@@ -162,6 +188,14 @@ Cotton.Algo.distanceComplexe = function(oVisitItem1, oVisitItem2) {
   return sum;
 };
 
+/**
+ * Compute the distance between two generated pages (search page Google)
+ * 
+ * @param {Object}
+ *          oVisitItem1
+ * @param {Object}ÊoVisitItem2
+ * @returns {int} number of common queryKeymords.
+ */
 Cotton.Algo.distanceBetweenGeneratedPages = function(oVisitItem1, oVisitItem2) {
 
   var keywords1 = oVisitItem1['lQueryKeywords'];
