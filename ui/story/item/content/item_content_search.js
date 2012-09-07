6bb2ba89aa234ce65f55fd9a887622e0603cf5ca
Raphael Moutard <raphael.moutard@gmail.com>
2012-09-07 14:14:15 +0200

    UI : featured image on content default and search

--- ui/story/item/content/item_content_search.js
+++ ui/story/item/content/item_content_search.js
@@ -10,6 +10,7 @@ Cotton.UI.Story.Item.Content.Search = Class.extend({
   _$item_content : null,
 
   _$featured_image : null,
+  _$img : null,
   _oItemToolbox : null,
   _oItemDescription : null,
   _oItemDna : null,
@@ -30,15 +31,21 @@ Cotton.UI.Story.Item.Content.Search = Class.extend({
     // set favicon
     this._oItemToolbox._$favicon.attr("src",
         "/media/images/story/google_favicon.png");
-    // Image Url
-    if (this._oItem._oVisitItem.extractedDNA().imageUrl() !== "") {
-      this._$featured_image.attr("src", this._oItem._oVisitItem.extractedDNA()
-          .imageUrl());
+
+    // set image
+    this._$img = $('<img ></img>');
+    if(this._oItem.visitItem().extractedDNA().imageUrl() !== ""){
+      this._$img.attr('src', this._oItem.visitItem().extractedDNA().imageUrl());
+      this._$featured_image.append(this._$img);
+
     }
 
     // create the item
-    this._$item_content.append(this._oItemDescription.$(), this._oItemToolbox
-        .$());
+    this._$item_content.append(
+        this._$featured_image,
+        this._oItemDescription.$(),
+        this._oItemToolbox.$()
+        );
   },
 
   $ : function() {
