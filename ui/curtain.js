ba1ed383f21f31a667c2b6a0ccee7fb6d1f84a1a
Raphael Moutard <raphael.moutard@gmail.com>
2012-09-13 21:46:37 +0200

    UI : don't wait window onload to being to track errors

--- ui/curtain.js
+++ ui/curtain.js
@@ -4,16 +4,15 @@ Cotton.UI.Curtain = Class.extend({
 
   _$curtain : null,
 
-  _$gears :  null,
+  _$gears : null,
   _$board : null,
   _$meter : null,
   _$progressBar : null,
   _$percentage : null,
   _iPercentage : 20,
-  _oErrorHandler : null,
   _$error : null,
 
-  init : function($window){
+  init : function($window) {
 
     this._$curtain = $('ct-curtain');
     this._$gears = $('.gear');
@@ -21,19 +20,20 @@ Cotton.UI.Curtain = Class.extend({
     this._$progressBar = $('.progress_bar');
     this._$percentage = $('.percentage');
     this._$meter = $('.meter');
-    this._oErrorHandler = new Cotton.UI.ErrorHandler(this, $window);
     this._$error = $('<div class="error"></div>');
   },
 
   percentage : function() {
-    return  this._iPercentage;
+    return this._iPercentage;
   },
 
   /**
    * Change the percentage value by the new one.
-   * @param {int} : iPercentage this value should be between 0 and 100.
+   * 
+   * @param {int} :
+   *          iPercentage this value should be between 0 and 100.
    */
-  setPercentage : function(iPercentage){
+  setPercentage : function(iPercentage) {
     this._iPercentage = Math.max(0, Math.min(iPercentage, 100));
 
     // set the ui.
@@ -41,44 +41,46 @@ Cotton.UI.Curtain = Class.extend({
     this.setProgressBar(this._iPercentage);
   },
 
-  set$percentage : function(iPercentage){
+  set$percentage : function(iPercentage) {
     this._$percentage.text(iPercentage + '%');
   },
 
-  setProgressBar : function(iPercentage){
+  setProgressBar : function(iPercentage) {
     this._$progressBar.width(iPercentage + '%');
   },
 
   /**
    * Increase the percentage, sum it with the given value.
-   * @param {int} : iPercentage this value should be between 0 and 100.
+   * 
+   * @param {int} :
+   *          iPercentage this value should be between 0 and 100.
    */
-  increasePercentage : function(iPercentage){
-    var toPercentage = Math.max(0,
-                                Math.min(100, this._iPercentage + iPercentage));
+  increasePercentage : function(iPercentage) {
+    var toPercentage = Math.max(0, Math.min(100, this._iPercentage
+        + iPercentage));
 
     // set the ui.
     this._increase$percentage(toPercentage);
     this.setProgressBar(toPercentage);
   },
 
-  _increase$percentage : function(toPercentage){
+  _increase$percentage : function(toPercentage) {
     var self = this;
     var fromPercentage = this._iPercentage;
     var iTemp = fromPercentage;
-    for( var i=fromPercentage; i <= toPercentage; i++){
-      setTimeout(function(){
+    for ( var i = fromPercentage; i <= toPercentage; i++) {
+      setTimeout(function() {
         console.log(iTemp);
         $('.percentage').text(iTemp + '%');
-        iTemp+=1;
+        iTemp += 1;
       }, (i - fromPercentage) * 50);
     }
-   this._iPercentage = toPercentage;
+    this._iPercentage = toPercentage;
   },
   /**
    * Start animation. (gears + progressbar)
    */
-  start : function(){
+  start : function() {
     this._$gears.find('.shine').addClass('animate');
     this._$gears.find('.perspective').addClass('animate');
     this._$meter.addClass('animate');
@@ -87,19 +89,18 @@ Cotton.UI.Curtain = Class.extend({
   /**
    * Stop animation
    */
-  stop : function(){
+  stop : function() {
     this._$gears.find('.shine').removeClass('animate');
     this._$gears.find('.perspective').removeClass('animate');
     this._$meter.removeClass('animate');
   },
 
-  displayError: function(sMessage, sUrl, iLine){
+  displayError : function(sMessage, sUrl, iLine) {
     this.stop();
     this._$meter.addClass("red");
     this._$meter.removeClass("yellow");
     var $humam_message = $('<p></p>').text(
-        "An error occured during cottonTracks installation :"
-        );
+        "An error occured during cottonTracks installation :");
     var $message = $('<p></p>').text(sMessage);
     var $info = $('<p></p>').text(sUrl + ':' + iLine);
 
