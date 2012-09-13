38d66721321c5c75a18e0e57f8dc50cf0a56f147
Raphael Moutard <raphael.moutard@gmail.com>
2012-09-13 21:46:37 +0200

    UI : don't wait window onload to being to track errors

--- ui/error_handler.js
+++ ui/error_handler.js
@@ -7,15 +7,36 @@ Cotton.UI.ErrorHandler = Class.extend({
   _$p : null,
   _lErrors : [],
 
-  init : function(oCurtain, $global){
+  init : function($global, oCurtain) {
     var self = this;
 
     this._oCurtain = oCurtain;
     this._$window = $global;
 
     this._$window.onerror = function(msg, url, line) {
-         self._lErrors.push(arguments);
-         self._oCurtain.displayError(msg, url, line);
+      var error = {
+        'msg' : msg,
+        'url' : url,
+        'line' : line
+      };
+      self._lErrors.push(error);
+      if (self._oCurtain) {
+        self._oCurtain.displayError(msg, url, line);
+      }
+
     }
   },
+
+  curtain : function() {
+    return this._oCurtain;
+  },
+
+  setCurtain : function(oCurtain) {
+    var self = this;
+    self._oCurtain = oCurtain;
+    for ( var i = 0; i < self._lErrors.length; i++) {
+      var error = self._lErrors[i];
+      self._oCurtain.displayError(error['msg'], error['url'], error['line']);
+    }
+  }
 });
--- ui/loading.js
+++ ui/loading.js
@@ -1,38 +1,39 @@
 'use strict';
 
 Cotton.UI.oCurtain = {};
+Cotton.UI.oErrorHandler = new Cotton.UI.ErrorHandler(window);
 var iTimeoutIndex;
 
-$(window).load(
-    function() {
+$(window)
+    .load(
+        function() {
 
-      // Start the timer.
-      // Throw a timeout if the installation exeed one minute.
-      iTimeoutIndex = setTimeout(function(){
-        throw new Error("Timeout - The installation is too long. Maybe try to close this tab and open a new one.")
-      }, 50000);
+          // Start the timer.
+          // Throw a timeout if the installation exeed one minute.
+          iTimeoutIndex = setTimeout(
+              function() {
+                throw new Error(
+                    "Timeout - The installation is too long. Maybe try to close this tab and open a new one.")
+              }, 50000);
 
+          Cotton.UI.oCurtain = new Cotton.UI.Curtain(window);
+          Cotton.UI.oErrorHandler.setCurtain(Cotton.UI.oCurtain);
+          $('body').addClass('ct-body-loaded');
+          $('.ct-landscape').addClass('running');
+          // If it's not the first time curtain is already open.
+          if (localStorage['CottonFirstOpening'] === undefined
+              || localStorage['CottonFirstOpening'] === "true") {
+          } else {
+            Cotton.UI.openCurtain();
+          }
 
-      Cotton.UI.oCurtain = new Cotton.UI.Curtain(window);
-      $('body').addClass('ct-body-loaded');
-      $('.ct-landscape').addClass('running');
-      // If it's not the first time curtain is already open.
-      if (localStorage['CottonFirstOpening'] === undefined
-          || localStorage['CottonFirstOpening'] === "true") {
-      } else {
-        Cotton.UI.openCurtain();
-      }
+          $(".meter > span").each(function() {
+            $(this).data("origWidth", $(this).width()).width(0).animate({
+              width : $(this).data("origWidth")
+            }, 1200);
+          });
 
-      $(".meter > span").each(function() {
-				$(this)
-					.data("origWidth", $(this).width())
-					.width(0)
-					.animate({
-						width: $(this).data("origWidth")
-					}, 1200);
-			});
-
-    });
+        });
 
 Cotton.UI.openCurtain = function() {
   clearTimeout(iTimeoutIndex);
