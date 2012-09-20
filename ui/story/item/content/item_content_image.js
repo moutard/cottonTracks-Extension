b14188ead4868f3f83e7b5f97b42938c97c554c3
Raphael Moutard <raphael.moutard@gmail.com>
2012-09-20 14:16:43 +0200

    UI : featured image class

--- ui/story/item/content/item_content_image.js
+++ ui/story/item/content/item_content_image.js
@@ -16,14 +16,15 @@ Cotton.UI.Story.Item.Content.Image = Cotton.UI.Story.Item.Content.Element.extend
 
     if (sType === "img") {
       this._$img.attr("src", this._oItem._oVisitItem.url());
+      this._oItemFeaturedImage.setImageUrl(this._oItem._oVisitItem.url());
     }
     if (sType === "imgres") {
       this._$img.attr("src", this._oItem._oVisitItem._oUrl.dSearch['imgurl']);
+      this._oItemFeaturedImage.setImageUrl(this._oItem._oVisitItem._oUrl.dSearch['imgurl']);
     }
     this._$featured_image.append(this._$img);
-
     // create the item
-    this._$item_content.append(this._$featured_image, this._oItemToolbox.$());
+    this._$item_content.append(this._oItemFeaturedImage.$(), this._oItemToolbox.$());
   },
 
 });
--- ui/story/item/content/item_content_search.js
+++ ui/story/item/content/item_content_search.js
@@ -15,18 +15,9 @@ Cotton.UI.Story.Item.Content.Search = Cotton.UI.Story.Item.Content.Element.exten
     this._oItemToolbox._$favicon.attr("src",
         "/media/images/story/google_favicon.png");
 
-    // set image
-    this._$featured_image = $('<div class="ct-featured_image"></div>');
-    this._$img = $('<img ></img>');
-    if(this._oItem.visitItem().extractedDNA().imageUrl() !== ""){
-      this._$img.attr('src', this._oItem.visitItem().extractedDNA().imageUrl());
-      this._$featured_image.append(this._$img);
-
-    }
-
     // create the item
     this._$item_content.append(
-        this._$featured_image,
+        this._oFeaturedImage.$(),
         this._oItemDescription.$(),
         this._oItemToolbox.$()
         );
