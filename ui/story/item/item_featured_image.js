d1f49500598a758c50931d7f1f27db96b961e300
Raphael Moutard <raphael.moutard@gmail.com>
2012-09-20 15:03:36 +0200

    UI : edit set featured image for a visit item

--- ui/story/item/item_featured_image.js
+++ ui/story/item/item_featured_image.js
@@ -45,7 +45,36 @@ Cotton.UI.Story.Item.FeaturedImage = Class
       },
 
       editImage : function(){
+        var self = this;
+        if(!self._sImageAlreadyEditable){
+          self._sImageAlreadyEditable = true;
+          // Create an input field to change the title.
+          var $input_image = $('<input class="ct-editable_image" type="text" name="image">');
 
+          // Set the default value, with the current title.
+          $input_image.val(self._$img.attr('src') || 'http://');
+          $input_image.keypress(function(event) {
+            // on press 'Enter' event.
+            if (event.which == 13) {
+              if(!self._sImageUrl){
+                self._$featured_image.append(self._$img);
+              }
+              self._sImageUrl = $input_image.val();
+              self._$img.attr('src', self._sImageUrl);
+              $input_image.remove();
+              //self._$title.show();
+              self._sImageAlreadyEditable = false;
+
+              // Set the title in the model.
+              self._oItemContent.item().visitItem().extractedDNA().setImageUrl(self._sImageUrl);
+              self._oItemContent.item().visitItemHasBeenSet();
+            }
+          });
+
+          // hide the title and replace it by the input field.
+          //self._$title.hide();
+          self._$featured_image.append($input_image);
+        }
       },
 
 });
