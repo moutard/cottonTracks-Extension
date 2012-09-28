b62868996dfcb52670c5cbe7db3acddfefdb4bcd
Raphael Moutard <raphael.moutard@gmail.com>
2012-09-28 14:50:55 +0200

    Issue #55 : remove some TODO

--- algo/dbscan3/detect_sessions.js
+++ algo/dbscan3/detect_sessions.js
@@ -3,13 +3,11 @@
 /**
  * separate roughly the visit items when the time difference between two
  * consecutive items is more than 5 hours.
- * 
+ *
  * @param {Array}
  *          lVisitItems
  */
 
-// TODO(rmoutard) : use an iterator like in underscore.js to make this method
-// more generale.
 Cotton.Algo.roughlySeparateSession = function(lVisitItems, mCallBack) {
   // 5 hours
   var threshold = 5 * 60 * 60 * 1000;
