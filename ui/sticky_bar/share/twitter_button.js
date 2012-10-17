5160786f9e768e718123c1eefd52321ab97e0ea6
Raphael Moutard <raphael.moutard@gmail.com>
2012-10-17 22:12:38 +0200

    UI : Kippt version

---
+++ ui/sticky_bar/share/twitter_button.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * Twitter functionality.
+ *
+ */
+Cotton.UI.StickyBar.Share.TwitterButton = Class.extend({
+
+  _$twitter_button: null,
+
+  init: function(){
+    var self = this;
+    this._$twitter_button = $('<div class="ct-twitter_button"></div>').click(function(){
+    });
+  },
+
+  $ : function(){
+    return this._$twitter_button;
+  },
+
+});
