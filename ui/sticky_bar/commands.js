7c73e475966024bba33f69704c22d5fcac7131fb
Raphael Moutard <raphael.moutard@gmail.com>
2012-10-17 17:41:23 +0200

    Kippt integration

--- ui/sticky_bar/commands.js
+++ ui/sticky_bar/commands.js
@@ -79,13 +79,71 @@ Cotton.UI.StickyBar.Commands = Class.extend({
 
     this._$search_button.qtip(dQtipParameters);
     this._$recent_button.qtip(dQtipParameters);
-    this._$share_button.qtip(dQtipParameters);
-  },
+    this._$share_button.qtip(dQtipParameters).click(function(){
+      self.connectOnKippt();
+    });
+
+    },
 
   /**
    * @return {HtmlElement}
    */
   $ : function() {
     return this._$commands;
+  },
+
+  connectOnKippt : function(){
+      var self = this;
+      $.ajax({
+          url: 'https://kippt.com/api/account/?include_data=services',
+          type: "GET",
+          dataType: 'json'
+      })
+      .done(function(data){
+          var useriD = data['id'];
+          localStorage.setItem('kipptUserId', data['id']);
+
+          $.each(data.services, function(name, connected) {
+              if (connected) {
+                  //$("#kippt-actions ." + name).toggleClass("connected", connected);
+                  //$("#kippt-actions ." + name).css('display', 'inline-block');
+              }
+          });
+          self.shareOnKippt();
+      })
+      .fail(function(jqXHR, textStatus){
+          // Logged out user, open login page
+          //Kippt.openTab('https://kippt.com/login/');
+          //Kippt.closePopover();
+      });
+
+  },
+
+  shareOnKippt : function(){
+    if(_oCurrentlyOpenStoryline){
+      var type = 'POST';
+      var url = 'https://kippt.com/api/clips/';
+
+      _.each(_oCurrentlyOpenStoryline.story().visitItems(), function(oVisitItem){
+        var msg = JSON.stringify({url: oVisitItem.url()});
+
+        $.ajax({
+            url: url,
+            type: type,
+            dataType: 'json',
+            data: msg
+        })
+        .done(function(){
+            // Clear page cache
+            //localStorage.removeItem('cache-title');
+            //localStorage.removeItem('cache-notes');
+        })
+        .fail(function(jqXHR, textStatus){
+            alert( "Something went wrong when saving. Try again or contact hello@kippt.com");
+        });
+    });
+  } else {
+    console.log("Nothing to share");
+  }
   },
 });
