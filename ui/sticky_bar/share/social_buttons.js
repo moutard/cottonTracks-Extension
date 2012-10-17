1e410f7722fcd936009e61d8a36468caa0b66dd8
Raphael Moutard <raphael.moutard@gmail.com>
2012-10-17 22:29:12 +0200

    Kippt integration: most clean code

--- ui/sticky_bar/share/social_buttons.js
+++ ui/sticky_bar/share/social_buttons.js
@@ -9,11 +9,13 @@ Cotton.UI.StickyBar.Share.SocialButtons = Class.extend({
   _$social_buttons : null,
   _bIsOpen : null,
   _oKipptButton : null,
+  _oTwitterButton : null,
 
   init: function(){
     var self = this;
-    this._$social_buttons = $('<div class="ct-social_buttons"></div>');
+    this._$social_buttons = $('<div class="ct-social_buttons"></div>').hide();
     this._oKipptButton = new Cotton.UI.StickyBar.Share.KipptButton();
+    this._oTwitterButton = new Cotton.UI.StickyBar.Share.TwitterButton();
 
     this._$social_buttons.append(this._oKipptButton.$(), this._oTwitterButton.$());
 
