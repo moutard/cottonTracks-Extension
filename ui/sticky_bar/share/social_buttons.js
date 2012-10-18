106a1aa6e4860d1a5d6adcc493e48fd2256f3d72
Raphael Moutard <raphael.moutard@gmail.com>
2012-10-18 11:26:12 +0200

    UI : design share social buttons according to blue UI V2.0

--- ui/sticky_bar/share/social_buttons.js
+++ ui/sticky_bar/share/social_buttons.js
@@ -7,6 +7,7 @@
 Cotton.UI.StickyBar.Share.SocialButtons = Class.extend({
 
   _$social_buttons : null,
+  _$title : null,
   _bIsOpen : null,
   _oKipptButton : null,
   _oTwitterButton : null,
@@ -14,10 +15,11 @@ Cotton.UI.StickyBar.Share.SocialButtons = Class.extend({
   init: function(){
     var self = this;
     this._$social_buttons = $('<div class="ct-social_buttons"></div>').hide();
+    this._$title = $('<h1>Share this story.</h1>');
     this._oKipptButton = new Cotton.UI.StickyBar.Share.KipptButton();
     this._oTwitterButton = new Cotton.UI.StickyBar.Share.TwitterButton();
-
-    this._$social_buttons.append(this._oKipptButton.$(), this._oTwitterButton.$());
+    var $paragraph = $('<p>Only element marked as public will be share. Private element will stay yours.</p>');
+    this._$social_buttons.append(this._$title, $paragraph, this._oKipptButton.$(), this._oTwitterButton.$());
 
   },
 
--- ui/sticky_bar/share/twitter_button.js
+++ ui/sticky_bar/share/twitter_button.js
@@ -7,10 +7,19 @@
 Cotton.UI.StickyBar.Share.TwitterButton = Class.extend({
 
   _$twitter_button: null,
+  _$twitter_icon: null,
+  _$twitter_title: null,
 
   init: function(){
     var self = this;
-    this._$twitter_button = $('<div class="ct-twitter_button">Tweet it</div>').click(function(){
+    this._$twitter_icon = $('<img src="/media/images/topbar/share/twitter.png"></img>');
+    this._$twitter_title = $('<h2>Tweet it</h2>');
+    this._$twitter_button = $('<div class="ct-social_button ct-twitter"></div>').append(
+      this._$twitter_icon,
+      this._$twitter_title
+    );
+
+    this._$twitter_button.click(function(){
     });
   },
 
