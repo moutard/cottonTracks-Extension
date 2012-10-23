3b32a07d60656ecbe07de7d11c9d0c17726f8945
moutard <raphael.moutard@gmail.com>
2012-10-23 16:41:48 +0200

    Routines : stable version

--- db/routine.js
+++ db/routine.js
@@ -1,14 +1,33 @@
 'use strict';
 
+/**
+ * Routines update the database according to a CRON. They are launched in the
+ * background (maybe the better should be in another worker).
+ *
+ * Routines can be heavy.
+ */
+
 Cotton.DB.Routine = Class.extend({
 
   _oStore : null,
 
+  /**
+   * @constructor
+   * {Cotton.DB.Store} :
+   * TODO(rmoutard) : Routines could be used in the store of the
+   * background controller.
+   */
   init : function(oStore) {
     var self = this;
     self._oStore = oStore;
   },
 
+  /**
+   * Routine : Update Search Keywords.
+   *
+   * Accross all the visitItems. For each visitItems compute the search keywords
+   * then update the corresponding entry in the database 'searchkeywords'.
+   */
   updateSearchKeywords : function() {
     var self = this;
     var lKeywordsAndId = [];
@@ -24,11 +43,17 @@ Cotton.DB.Routine = Class.extend({
         })
       });
       console.log(lKeywordsAndId);
-      self.updateOneSearchKeyword(lKeywordsAndId, 0);
+      self._updateOneSearchKeyword(lKeywordsAndId, 0);
     });
   },
 
-  updateOneSearchKeyword : function(lKeywordsAndId, i){
+  /**
+   * Private method used by the routine updateSearchKeywords.
+   *
+   * This is a recursive method, because we need to wait that the previous
+   * keyword has been updated before find for the other one.
+   */
+  _updateOneSearchKeyword : function(lKeywordsAndId, i){
     var self = this;
 
     var _i = i;
@@ -51,5 +76,57 @@ Cotton.DB.Routine = Class.extend({
     }
   },
 
+  /**
+   * Routine : Update Search Keywords for stories.
+   *
+   * Accross all the visitItems. For each visitItems compute the search keywords
+   * then update the corresponding entry in the database 'searchkeywords'.
+   */
+  updateSearchKeywordsForStories : function() {
+    var self = this;
+    var lKeywordsAndId = [];
 
+    self._oStore.getList('stories', function(lStories){
+      _.each(lStories, function(oStory){
+        _.each(oStory.searchKeywords(), function(sKeyword){
+          var oKeywordAndId = {
+              'sKeyword': sKeyword,
+              'iStoryId' : oStory.id()
+          };
+          lKeywordsAndId.push(oKeywordAndId);
+        })
+      });
+      console.log(lKeywordsAndId);
+      self._updateOneSearchKeywordForStories(lKeywordsAndId, 0);
+    });
+  },
+
+  /**
+   * Private method used by the routine updateSearchKeywords.
+   *
+   * This is a recursive method, because we need to wait that the previous
+   * keyword has been updated before find for the other one.
+   */
+  _updateOneSearchKeywordForStories : function(lKeywordsAndId, i){
+    var self = this;
+
+    var _i = i;
+    var oKeywordAndId = lKeywordsAndId[_i];
+    if(oKeywordAndId){
+      self._oStore.find('searchKeywords', 'sKeyword', oKeywordAndId['sKeyword'], function(oSearchKeyword){
+        // If not find, oSearchKeyword is null.
+        if(!oSearchKeyword) {
+          oSearchKeyword = new Cotton.Model.SearchKeyword(oKeywordAndId['sKeyword']);
+        }
+
+        oSearchKeyword.addReferringStoryId(oKeywordAndId['iStoryId']);
+
+        self._oStore.put('searchKeywords', oSearchKeyword, function(iId){
+          // Becarefull with asynchronous.
+          console.log('update id :' + iId + 'i:' + _i);
+          self._updateOneSearchKeywordForStories(lKeywordsAndId, _i+1);
+        });
+      });
+    }
+  },
 });
