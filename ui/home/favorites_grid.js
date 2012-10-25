36a0f90c8dba124213fe110d7b247513b687e497
rmoutard <raphael.moutard@gmail.com>
2012-10-25 23:07:57 +0200

    UI : stable grid

--- ui/home/favorites_grid.js
+++ ui/home/favorites_grid.js
@@ -1,10 +1,30 @@
 'use strict';
 
+// DO NOT FORGET THE PADDING (5 left and 5 right) for ticket.
+// DO NOT FORGET THE BORDER.
+Cotton.UI.Home.SMALL_WIDTH = 100;
+Cotton.UI.Home.SMALL_WIDTH_WITHOUT_PADDING = 88;
+Cotton.UI.Home.SMALL_MARGIN = 10;
+
+Cotton.UI.Home.MEDIUM_WIDTH = 150;
+Cotton.UI.Home.MEDIUM_WIDTH_WITHOUT_PADDING = 138;
+Cotton.UI.Home.MEDIUM_MARGIN = 15;
+
+Cotton.UI.Home.LARGE_WIDTH = 200;
+Cotton.UI.Home.LARGE_WIDTH_WITHOUT_PADDING = 188;
+Cotton.UI.Home.LARGE_MARGIN = 20;
+
+Cotton.UI.Home.MIN_PADDING = 40;
+Cotton.UI.Home.NB_COLUMN = 5;
+
+
 Cotton.UI.Home.FavoritesGrid = Class
     .extend({
 
       _$FavoritesGrid : null,
       _lFavoritesTicket : [],
+      _iNbRow : 1,
+      _iNbColumn : 5,
 
       init : function() {
         var self = this;
@@ -12,28 +32,28 @@ Cotton.UI.Home.FavoritesGrid = Class
         self._$FavoritesGrid = $('<div class="ct-favorites-grid">');
         self._lFavoritesTicket = [];
 
-          self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
-              '/media/images/home/tickets/TC.jpg', 'Techcrunch',
-              'http://techcrunch.com'));
-          self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
-              '/media/images/home/tickets/Fubiz.jpg', 'Fubiz', 'http://fubiz.net'));
-          self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
-              '/media/images/home/tickets/FB.jpg', 'Facebook',
-              'http://facebook.com'));
-          self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
-              '/media/images/home/tickets/Dribbble.jpg', 'Dribbble', 'http://dribbble.com'));
-          self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
-              '/media/images/home/tickets/PandoDaily.jpg', 'PandoDaily', 'http://pandodaily.com'));
-          self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
-              '/media/images/home/tickets/MTV.jpg', 'MTV', 'http://www.mtv.com'));
-          self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
-              '/media/images/home/tickets/Twitter.jpg', 'Twitter', 'http://twitter.com'));
-          self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
-              '/media/images/home/tickets/Pinterest.jpg', 'Pinterest', 'http://pinterest.com'));
+        self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
+            '/media/images/home/tickets/TC.jpg', 'Techcrunch',
+            'http://techcrunch.com'));
+        self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
+            '/media/images/home/tickets/Fubiz.jpg', 'Fubiz', 'http://fubiz.net'));
+        self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
+            '/media/images/home/tickets/FB.jpg', 'Facebook',
+            'http://facebook.com'));
+        self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
+            '/media/images/home/tickets/Dribbble.jpg', 'Dribbble', 'http://dribbble.com'));
+        self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
+            '/media/images/home/tickets/PandoDaily.jpg', 'PandoDaily', 'http://pandodaily.com'));
+        self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
+            '/media/images/home/tickets/MTV.jpg', 'MTV', 'http://www.mtv.com'));
+        self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
+            '/media/images/home/tickets/Twitter.jpg', 'Twitter', 'http://twitter.com'));
+        self._lFavoritesTicket.push(new Cotton.UI.Home.Ticket(this,
+            '/media/images/home/tickets/Pinterest.jpg', 'Pinterest', 'http://pinterest.com'));
 
          $(window).resize(function(){
-          console.log($(this).width());
-          });
+           self.recomputeGrid();
+         });
 
           self.recomputeGrid();
       },
@@ -57,16 +77,56 @@ Cotton.UI.Home.FavoritesGrid = Class
       recomputeGrid : function() {
         var self = this;
 
-        var iEstmatedWidth = à;
-        _.each(self._lFavoritesTicket, function(oTicket){
-          oTicket.setWidth(200);
-          oTicket.setMargin(10);
-          self.append(oTicket.$());
-        });
+        var iEstmatedLargeWidth = (Cotton.UI.Home.NB_COLUMN * Cotton.UI.Home.LARGE_WIDTH)
+          + ((2 * Cotton.UI.Home.NB_COLUMN) * Cotton.UI.Home.LARGE_MARGIN);
+        var iEstmatedMediumWidth = (Cotton.UI.Home.NB_COLUMN * Cotton.UI.Home.MEDIUM_WIDTH)
+          + ((2 * Cotton.UI.Home.NB_COLUMN) * Cotton.UI.Home.MEDIUM_MARGIN);
+        var iEstmatedSmallWidth = (Cotton.UI.Home.NB_COLUMN *  Cotton.UI.Home.SMALL_WIDTH)
+          + ((2 * Cotton.UI.Home.NB_COLUMN) * Cotton.UI.Home.SMALL_MARGIN);
 
-        //var iMargin = ($(window).width() -) / 2;
-        //self._$FavoritesGrid.css('margin-left', iMargin + "px");
-        //self._$FavoritesGrid.css('margin-right', iMargin + "px");
+        if(($(window).width() - iEstmatedLargeWidth) > 2 * Cotton.UI.Home.MIN_PADDING){
+          // Large size fit well.
+
+          _.each(self._lFavoritesTicket, function(oTicket){
+            oTicket.setLarge();
+            self.append(oTicket.$());
+          });
+
+          self._$FavoritesGrid.width(iEstmatedLargeWidth);
+          var iMargin = ($(window).width() - iEstmatedLargeWidth) / 2;
+          self._$FavoritesGrid.css('margin-left', iMargin + "px");
+          //self._$FavoritesGrid.css('margin-right', iMargin + "px");
+
+        } else if(($(window).width() - iEstmatedMediumWidth) > 2 * Cotton.UI.Home.MIN_PADDING) {
+          // Medium size fit well.
+
+          _.each(self._lFavoritesTicket, function(oTicket){
+            oTicket.setMedium();
+            self.append(oTicket.$());
+          });
+
+          self._$FavoritesGrid.width(iEstmatedMediumWidth);
+          var iMargin = ($(window).width() - iEstmatedMediumWidth) / 2;
+          self._$FavoritesGrid.css('margin-left', iMargin + "px");
+          //self._$FavoritesGrid.css('margin-right', iMargin + "px");
+
+        } else {
+          // Small size fit well.
+
+          _.each(self._lFavoritesTicket, function(oTicket){
+            oTicket.setSmall();
+            self.append(oTicket.$());
+          });
+
+          self._$FavoritesGrid.width(iEstmatedSmallWidth);
+          var iMargin = ($(window).width() - iEstmatedSmallWidth) / 2;
+          self._$FavoritesGrid.css('margin-left', iMargin + "px");
+          //self._$FavoritesGrid.css('margin-right', iMargin + "px");
+        }
+
+
+
+
 
       },
     });
