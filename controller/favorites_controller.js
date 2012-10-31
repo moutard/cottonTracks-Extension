e505593a62730315b7c1e57ba3df8dccfaa95b53
Raphael Moutard <raphael.moutard@gmail.com>
2012-10-31 15:18:41 +0100

    Controller : favorites add

---
+++ controller/favorites_controller.js
@@ -0,0 +1,19 @@
+'use strict';
+/**
+ * Controller
+ *
+ * Inspired by MVC pattern.
+ *
+ * Handles DB, and UI.
+ *
+ */
+Cotton.FavoritesController = Class.extend({
+
+  _lFavorites : undefined,
+
+  init : function(){
+    var self = this;
+    self._lFavorites =
+  },
+
+});
