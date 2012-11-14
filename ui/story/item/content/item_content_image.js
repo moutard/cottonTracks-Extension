a05de7baca57e4a8a0efefc4373d287b39773f7d
Raphael Korach <raphael.korach@gmail.com>
2012-11-14 11:04:05 +0000

    replace hexa code in url for images

--- ui/story/item/content/item_content_image.js
+++ ui/story/item/content/item_content_image.js
@@ -19,12 +19,26 @@ Cotton.UI.Story.Item.Content.Image = Cotton.UI.Story.Item.Content.Element.extend
       this._oItemFeaturedImage.setImageUrl(this._oItem._oVisitItem.url());
     }
     if (sType === "imgres") {
-      this._$img.attr("src", this._oItem._oVisitItem._oUrl.dSearch['imgurl']);
-      this._oItemFeaturedImage.setImageUrl(this._oItem._oVisitItem._oUrl.dSearch['imgurl']);
+      var sImgSrc = this.replaceHexa(this._oItem._oVisitItem._oUrl.dSearch['imgurl']);
+      this._$img.attr("src", sImgSrc);
+      this._oItemFeaturedImage.setImageUrl(sImgSrc);
     }
     this._$featured_image.append(this._$img);
     // create the item
     this._$item_content.append(this._oItemFeaturedImage.$(), this._oItemToolbox.$());
   },
-
+  
+  replaceHexa : function(sImageUrl) {
+    var sImgUrl = sImageUrl
+    sImgUrl = sImgUrl.replace('%2521','!');  	
+    sImgUrl = sImgUrl.replace('%2522','"');
+    sImgUrl = sImgUrl.replace('%2523','#');
+    sImgUrl = sImgUrl.replace('%2524','$');
+    sImgUrl = sImgUrl.replace('%2525','%');
+    sImgUrl = sImgUrl.replace('%2526','&');
+    sImgUrl = sImgUrl.replace('%2527',"'");	
+    sImgUrl = sImgUrl.replace('%253D','=');  
+    sImgUrl = sImgUrl.replace('%253F','?');
+    return sImgUrl;
+  },
 });
