4f40f7e07d6a9428149f14fb21147d3b15426664
Raphael Moutard <raphael.moutard@gmail.com>
2012-11-19 11:06:43 +0100

    .gitignore is now working

--- parameters-compress.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# -- GLOBAL VARIABLES MAC ---------------------------------------------------------
-GOOGLE_CLOSURE_COMPILER='/Users/rmoutard/src/google_closure_compiler/compiler.jar'
-SOURCE_PATH='/Users/rmoutard/src/'
-SOURCE_NAME='SubZoom-Proto1'
-DESTINATION_PATH='/Users/rmoutard/Downloads/'
-TAR_NAME='cottontracks-beta'
-VERSION='0.1'
