461feb4e58a7107fe5ab26f9d29d242382252ac7
moutard <raphael.moutard@gmail.com>
2012-11-20 09:55:56 +0100

    Compress : add parameters compress

---
+++ parameters-compress.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+GOOGLE_CLOSURE_COMPILER='/usr/local/rmoutard/compiler.jar'
+SOURCE_PATH='/usr/local/rmoutard/sz/'
+SOURCE_NAME='SubZoom-Proto1'
+DESTINATION_PATH='/usr/local/rmoutard/'
+TAR_NAME='cottontracks-beta'
+VERSION='0.1'
