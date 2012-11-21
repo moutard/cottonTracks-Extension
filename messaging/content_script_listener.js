c839487a74fc56d4d7f681265eaa6218992ef355
moutard <raphael.moutard@gmail.com>
2012-11-21 16:58:36 +0100

    Issue #119 : CRASH - limit sendResponse to create visit (maybe not solved the problem completly)

--- messaging/content_script_listener.js
+++ messaging/content_script_listener.js
@@ -50,92 +50,96 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     var oVisitItem = oTranslator.dbRecordToObject(
                                                 request['params']['visitItem']
                                                   );
+    DEBUG && console.debug("Messaging - create_visit_item");
     DEBUG && console.debug(oVisitItem.url());
-    // Compute the referer id as it should be returned by the Chrome Extension
-    // History API. We need this algorithm because in some cases, such as when
-    // you open a link in a new tab, the referer id is not filled by Chrome, so
-    // we need to fill it ourselves.
-    // TODO(fwouts): Move out of here.
 
-//    var mGetVisitsHandler = function(lChromeReferrerVisitItems) {
-      // Select the last one the visit items.
+    // TODO(rmoutard) : use DB system, or a singleton.
+    var oExcludeContainer = new Cotton.Utils.ExcludeContainer();
 
-      /*if (lChromeReferrerVisitItems && lChromeReferrerVisitItems.length > 0) {
-        var iIndex = lChromeReferrerVisitItems.length - 1;
-        var oReferrerVisitItem = lChromeReferrerVisitItems[iIndex];
-        // Update the visit item accordingly.
-        oVisitItem.setChromeReferringVisitId(oReferrerVisitItem.visitId);
-      }*/
+    var sPutId = ""; // put return the auto-incremented id in the database.
 
-      // Other processing following this.
+    // Put the visitItem only if it's not a Tool, and it's not in the exluded
+    // urls.
+    // TODO (rmoutard) : parseUrl is called twice. avoid that.
+    if (!oExcludeContainer.isExcluded(oVisitItem.url())) {
+      var oStore = new Cotton.DB.Store('ct', {
+        'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS,
+        'searchKeywords' : Cotton.Translators.SEARCH_KEYWORD_TRANSLATORS
+      }, function() {
 
-      // TODO(rmoutard) : use DB system, or a singleton.
-      var oExcludeContainer = new Cotton.Utils.ExcludeContainer();
+        // you want to create it for the first time.
+        oStore.put('visitItems', oVisitItem, function(iId) {
+          DEBUG && console.debug("visitItem added" + iId);
+          sPutId = iId;
+          var _iId = iId;
+          _.each(oVisitItem.searchKeywords(), function(sKeyword){
+            // PROBLEM if not find.
+            oStore.find('searchKeywords', 'sKeyword', sKeyword, function(oSearchKeyword){
+              if(!oSearchKeyword) {
+                oSearchKeyword = new Cotton.Model.SearchKeyword(sKeyword);
+              }
 
-      var sPutId = ""; // put return the auto-incremented id in the database.
+              oSearchKeyword.addReferringVisitItemId(_iId);
 
-      // Put the visitItem only if it's not a Tool, and it's not in the exluded
-      // urls.
-      // TODO (rmoutard) : parseUrl is called twice. avoid that.
-      if (!oExcludeContainer.isExcluded(oVisitItem.url())) {
-        var oStore = new Cotton.DB.Store('ct', {
-          'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS,
-          'searchKeywords' : Cotton.Translators.SEARCH_KEYWORD_TRANSLATORS
-        }, function() {
-
-          // you want to create it for the first time.
-          oStore.put('visitItems', oVisitItem, function(iId) {
-            DEBUG && console.debug("visitItem added" + iId);
-            sPutId = iId;
-            var _iId = iId;
-            _.each(oVisitItem.searchKeywords(), function(sKeyword){
-              // PROBLEM if not find.
-              oStore.find('searchKeywords', 'sKeyword', sKeyword, function(oSearchKeyword){
-                if(!oSearchKeyword) {
-                  oSearchKeyword = new Cotton.Model.SearchKeyword(sKeyword);
-                }
-
-                oSearchKeyword.addReferringVisitItemId(_iId);
-
-                oStore.put('searchKeywords', oSearchKeyword, function(iiId){
-                  // Return nothing to let the connection be cleaned up.
-                  sendResponse({
-                    'received' : "true",
-                    'id' : sPutId,
-                  });
+              oStore.put('searchKeywords', oSearchKeyword, function(iiId){
+                // Return nothing to let the connection be cleaned up.
+                sendResponse({
+                  'received' : "true",
+                  'id' : sPutId,
                 });
               });
             });
-
-
           });
 
-        });
-      } else {
-        DEBUG && console
-            .debug("Content Script Listener - This visit item is a tool or an exluded url.");
-      }
-//    };
 
-    /*
-    var sReferringUrl = oVisitItem.referrerUrl();
-    if (sReferringUrl) {
-      chrome.history.getVisits({
-        url : sReferringUrl
-      }, mGetVisitsHandler);
+        });
+
+      });
     } else {
-      mGetVisitsHandler([]);
+      DEBUG && console
+          .debug("Content Script Listener - This visit item is a tool or an exluded url.");
     }
-    */
 
     // to allow sendResponse
     //return true;
+    break;
 
-  /**
-   * DEPRECATED
-   */
   case 'update_visit_item':
-    return true;
+    /**
+     * Because Model are compiled in two different way by google closure
+     * compiler we need a common structure to communicate throught messaging.
+     * We use dbRecord, and translators give us a simple serialisation process.
+     */
+    var lTranslators = Cotton.Translators.VISIT_ITEM_TRANSLATORS;
+    var oTranslator = lTranslators[lTranslators.length - 1];
+    var oVisitItem = oTranslator.dbRecordToObject(
+                                                request['params']['visitItem']
+                                                  );
+    DEBUG && console.debug("Messaging - update_visit_item");
+    DEBUG && console.debug(oVisitItem.url());
+    // TODO(rmoutard) : use DB system, or a singleton.
+    var oExcludeContainer = new Cotton.Utils.ExcludeContainer();
+
+    var sPutId = ""; // put return the auto-incremented id in the database.
+
+    // Put the visitItem only if it's not a Tool, and it's not in the exluded
+    // urls.
+    // TODO (rmoutard) : parseUrl is called twice. avoid that.
+    if (!oExcludeContainer.isExcluded(oVisitItem.url())) {
+      var oStore = new Cotton.DB.Store('ct', {
+        'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS,
+        'searchKeywords' : Cotton.Translators.SEARCH_KEYWORD_TRANSLATORS
+      }, function() {
+        // The visit item already exists, just update it.
+        oStore.put('visitItems', oVisitItem, function(iId) {
+          DEBUG && console.debug("Messaging - visitItem updated" + iId);
+        });
+      });
+    } else {
+      DEBUG && console
+          .debug("Content Script Listener - This visit item is a tool or an exluded url.");
+    }
+    break;
 
   /**
    *
@@ -197,10 +201,10 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 
 
                       wDBSCAN3.postMessage(lAllVisitDict);
-                      sendResponse({
-                        'received' : "true",
-                        'id' : sPutId,
-                      });
+                      //sendResponse({
+                      //  'received' : "true",
+                      //  'id' : sPutId,
+                      //});
 
                     });
                   });
@@ -209,9 +213,12 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
           });
       });
     //return true;
-
+    break;
+  default:
+    break;
   }
 
+  return true;
 });
 
 chrome.runtime.onInstalled.addListener(function() {
