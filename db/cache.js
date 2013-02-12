1f908ced4c9cad75d3aaa02473d380bc1f781dae
rmoutard <raphael.moutard@gmail.com>
2013-02-12 22:44:06 +0100

    Refactor : new store architecture

--- db/cache.js
+++ db/cache.js
@@ -1,50 +1,29 @@
 'use strict';
 
 Cotton.DB.Cache = Class.extend({
-  init : function(){
-    //if(!localStorage['ct-stories-cache']){
-      localStorage['ct-stories-cache'] = JSON.stringify([
-      {
-        fLastVisitTime: 1360338084196.688,
-        fRelevance: null,
-        id: 1,
-        lTags: [],
-        lVisitItemsId: [1141],
-        sFeaturedImage: "",
-        sFormatVersion: "0.1",
-        sTitle: "My Reviews | git.corp Code Review",
-      },
-      {
-        fLastVisitTime: 1360331471984.9082,
-        fRelevance: null,
-        lTags: [],
-        lVisitItemsId: [1141],
-        sFeaturedImage: "",
-        sFormatVersion: "0.1",
-        sTitle: "LTU",
-      },
-      {
-        fLastVisitTime: 1360329780640.9968,
-        fRelevance: null,
-        lTags: [],
-        lVisitItemsId: [1141],
-        sFeaturedImage: "",
-        sFormatVersion: "0.1",
-        sTitle: "Add New Promotion ‹ LTU — WordPress",
-      },
-      {
-        fLastVisitTime: 1359718583764.991,
-        fRelevance: null,
-        lTags: [],
-        lVisitItemsId: [1141],
-        sFeaturedImage: "http://www.spclarke.com/wp-content/uploads/2011/11/platters-singing.jpg",
-        sFormatVersion: "0.1",
-        sTitle: "bogart",
-      }
-      ]);
-    //}
+  init : function(sDatabaseName, dTranslators, mOnReadyCallback) {
+    var self = this;
+    this._dTranslators = dTranslators;
+
+    var dIndexesForObjectStoreNames = {};
+    _.each(dTranslators, function(lTranslators, sObjectStoreName) {
+      dIndexesForObjectStoreNames[sObjectStoreName] = self._lastTranslator(sObjectStoreName).indexDescriptions();
+    });
+
+    this._oEngine = new Cotton.DB.Engine(
+        sDatabaseName,
+        dIndexesForObjectStoreNames,
+        function() {
+          mOnReadyCallback.call(self);
+    });
   },
   get : function(){
-    return JSON.parse(localStorage['ct-stories-cache']);
+    return JSON.parse(localStorage.getItem('ct-stories-cache'));
+  },
+  set : function(llStories){
+    localStorage.setItem('ct-stories-cache', JSON.stringify(llStories));
+  },
+  empty : function(){
+    localStorage.removeItem('ct-stories-cache');
   },
 });
