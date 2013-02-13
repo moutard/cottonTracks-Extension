3c7758b12ef9bab8c85a9e9f18c8f1bad67b5bcd
Raphael Moutard <raphael.moutard@gmail.com>
2013-02-13 18:58:46 +0100

    Refactor : compiler remove files for prod version

--- compile.py
+++ compile.py
@@ -1,6 +1,7 @@
 import argparse
 from py.sed import *
 from py.test_runner import runTest
+from py.precompile import PreCompiler
 from py.compilers.prod_compiler import PRODCompiler
 from py.compilers.dev_compiler import DEVCompiler
 
@@ -12,21 +13,22 @@ DESTINATION_PATH='/usr/local/rmoutard/cottontracks-beta/'
 #SOURCE_PATH='/Users/rmoutard/src/SubZoom-Proto1'
 #DESTINATION_PATH='/Users/rmoutard/src/ct-compiled'
 
-def compileProject(pbPRODMode, pbDEVMode):
-  """Given a directory where the project is, compress all the js and less
-  files replace the includes in the worker and html. Remove useless files.
-    Args:
-      - lsOption :
-      "PROD" generate a extremely compiled code with a tar.gz you only need to upload.,
-      "DEV" generate a code partialy compiled so debug is easyier,
-      "ALL" generate both version in 2 differents directories.
-  """
-  if(pbPRODMode):
-    loPRODCompiler = PRODCompiler(SOURCE_PATH, DESTINATION_PATH, GOOGLE_CLOSURE_COMPILER)
-    loPRODCompiler.compile()
-  if(pbDEVMode):
-    loDEVCompiler = DEVCompiler(SOURCE_PATH, DESTINATION_PATH)
-    loDEVCompiler.compile()
+class ProjectCompiler(PreCompiler):
+
+  def __init__(self, pbPRODMode, pbDEVMode, pbForce):
+
+    self._SOURCE = SOURCE_PATH
+    self._LIB = os.path.join(SOURCE_PATH, 'lib')
+    self._EXTERNS = os.path.join(SOURCE_PATH, 'lib', 'externs')
+    self.removeOld(DESTINATION_PATH, pbForce)
+
+    if(pbPRODMode):
+      loPRODCompiler = PRODCompiler(SOURCE_PATH, DESTINATION_PATH, GOOGLE_CLOSURE_COMPILER)
+      loPRODCompiler.compile()
+    if(pbDEVMode):
+      loDEVCompiler = DEVCompiler(SOURCE_PATH, DESTINATION_PATH)
+      loDEVCompiler.compile()
+
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Compile the project.')
@@ -34,10 +36,10 @@ if __name__ == '__main__':
       help='compile in DEV mode')
   parser.add_argument('--prod', dest='PROD',action='store_true',
       help='compile in PROD mode')
-   parser.add_argument('--f', dest='FORCE',action='store_true',
+  parser.add_argument('--f', dest='FORCE',action='store_true',
       help='force the compilation. do not ask permission to remove old version.')
 
   args = parser.parse_args()
   if(not args.PROD and not args.DEV):
     parser.error('No MODE precised, please provide --dev, --prod or both')
-  compileProject(args.PROD, args.DEV)
+  ProjectCompiler(args.PROD, args.DEV, args.FORCE)
