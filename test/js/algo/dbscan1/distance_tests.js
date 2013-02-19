a99f826afab0011b3f1356eb35939708af4375d8
Raphael Moutard <raphael.moutard@gmail.com>
2013-02-19 17:40:42 +0100

    Refactor : test for distance

--- test/js/algo/dbscan1/distance_tests.js
+++ test/js/algo/dbscan1/distance_tests.js
@@ -1,4 +1,15 @@
 'use strict';
+var oVisitItem1 = {
+  lExtractedWords : ['alice', 'wonderland', 'movie', 'Burton', 'Tim'],
+  lQueryWords : ['alice', 'wonderland', 'film'],
+};
+
+var oVisitItem2 = {
+  lExtractedWords : ['alice', 'adventure', 'wonderland', 'lewis', 'carroll'],
+  lQueryWords : ['alice', 'wonderland', 'novel'],
+};
+
+
 module(
     "Cotton.Algo.Distance",
     {
@@ -15,5 +26,14 @@ module(
 test("Cotton.Algo.Distance - distance key.", function() {
   deepEqual(Cotton.Algo.Distance.distanceKey("foo", {foo: 3}, {foo:4}), 1);
   deepEqual(Cotton.Algo.Distance.distanceKey("foo", {foo: 4}, {foo:3}), 1);
+});
+
+test("Cotton.Algo.Distance - distance meaning for the same value.", function() {
+  deepEqual(Cotton.Algo.Distance.meaning(oVisitItem1, oVisitItem1), 0);
+});
+
+test("Cotton.Algo.Distance - distance meaning.", function() {
+  deepEqual(Cotton.Algo.Distance.meaning(oVisitItem1, oVisitItem2), 1);
+  deepEqual(Cotton.Algo.Distance.meaning(oVisitItem2, oVisitItem1), 1);
 });
 
