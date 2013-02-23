743fa8752271d41ff5e9ccce625e4d613c4a2f7e
Raphael Korach <raphael.korach@gmail.com>
2013-02-23 15:29:37 -0300

    Controller: create lightyear controller

---
+++ controller/lightyear.js
@@ -0,0 +1,24 @@
+'use strict';
+/**
+ * Controller
+ *
+ * Inspired by MVC pattern.
+ *
+ * Handles DB, and UI.
+ *
+ */
+Cotton.Controllers.Lightyear = Class.extend({
+
+  /**
+   * @constructor
+   */
+  init : function(){
+
+    var self = this;
+    LOG && console.log("Controller - init -");
+
+  },
+
+});
+
+Cotton.Controllers.LIGHTYEAR = new Cotton.Controllers.Lightyear();
