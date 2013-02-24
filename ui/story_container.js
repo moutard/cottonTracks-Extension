bb61df56d93857bd76c0f6f0d8478249bdc6e3d0
Raphael Korach <raphael.korach@gmail.com>
2013-02-24 14:20:45 -0300

    UI: call story in the db from container
    This should be better done from the controller, then passed to the
    container, but don't know yet how to do it well.

--- ui/story_container.js
+++ ui/story_container.js
@@ -12,11 +12,28 @@ Cotton.UI.StoryContainer = Class.extend({
   init: function(){
 	  var self = this;
   	this._$story_container = $('.ct-story_container');
+		this.buildStory();
   },
   
   container: function() {
     return this._$container;
   },
+
+  buildStory: function() {
+		self._oStore = new Cotton.DB.StoreIndexedDB('ct', {
+          'stories' : Cotton.Translators.STORY_TRANSLATORS,
+          'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS
+    }, function() {
+      self._oStore.getLast('stories', 'fLastVisitTime', function(oLastStory) {
+        DEBUG && console.debug(oLastStory);
+        DEBUG && console.debug(oLastStory.id());
+        self._oStore.findGroup('visitItems', 'id', oLastStory.visitItemsId(), function(lVisitItems) {
+		      DEBUG && console.debug(lVisitItems);
+		  		DEBUG && console.debug(lVisitItems[0].storyId());
+				});
+      });
+    });
+}
   
 });
 
