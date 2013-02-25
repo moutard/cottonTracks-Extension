097d2a9f38e0b95c585d8efcbcf1471d24fe9fbb
Raphael Korach <raphael.korach@gmail.com>
2013-02-25 14:37:35 -0300

    UI: clean default item integration

--- ui/story/item/content/item_content_default.js
+++ ui/story/item/content/item_content_default.js
@@ -6,28 +6,35 @@
 Cotton.UI.Story.Item.Content.Default = Cotton.UI.Story.Item.Content.Element.extend({
 
   _oItemFeaturedImage : null,
-  _oItemInfo : null,
+  _$itemInfo : null,
+  _oItemTitle : null,
+  _oItemDate : null,
+  _oItemQuoteIndicator : null,
+  _oItemLabel : null,
   _oItemMenu : null,
-  _$featured_image : null,
-  _$img : null,
 
   init : function(oItem) {
     this._super(oItem);
     this._oItemFeaturedImage = new Cotton.UI.Story.Item.FeaturedImage(this);
-    this._oItemInfo = new Cotton.UI.Story.Item.Info(this);
-    this._oItemMenu = new Cotton.UI.Story.Item.Menu(this);
+    this._$itemInfo = $('<div class="ct-item_info"></div>');
+    this._oItemTitle = new Cotton.UI.Story.Item.Title(this);
+    this._oItemDate = new Cotton.UI.Story.Item.Date(this);
+    this._oItemQuoteIndicator = new Cotton.UI.Story.Item.QuoteIndicator(this);
+    this._oItemLabel = new Cotton.UI.Story.Item.SmallLabel(this);
+    this._oItemMenu = new Cotton.UI.Story.Item.SmallMenu(this);
 
     // create the item
     oItem.$().addClass('ct-item-default');
     this._$item_content.append(
       this._oItemFeaturedImage.$(),
-      this._oItemInfo.$(),
-      this._oItemMenu.$()
+      this._$itemInfo.append(
+	      this._oItemTitle.$(),
+	      this._oItemDate.$(),
+	      this._oItemQuoteIndicator.$(),
+	      this._oItemLabel.$(),
+        this._oItemMenu.$()
+	    )
     );
   },
-
-  info : function() {
-	  return this._oItemInfo;
-  }
 
 });
