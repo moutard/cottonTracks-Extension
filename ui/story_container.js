3c1047baf40279cf5638d8d53e1d61401dbb55af
Raphael Korach <raphael.korach@gmail.com>
2013-02-26 15:45:53 -0300

    Controller: build story from controller

--- ui/story_container.js
+++ ui/story_container.js
@@ -12,27 +12,11 @@ Cotton.UI.StoryContainer = Class.extend({
   init: function(){
 	  var self = this;
   	this._$storyContainer = $('.ct-story_container');
-		this.buildStory();
   },
   
   container: function() {
     return this._$storyContainer;
   },
-
-  buildStory: function() {
-		self._oDatabase = new Cotton.DB.IndexedDB.Wrapper('ct', {
-          'stories' : Cotton.Translators.STORY_TRANSLATORS,
-          'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS
-    }, function() {
-      self._oDatabase.getLast('stories', 'fLastVisitTime', function(oLastStory) {
-        self._oDatabase.findGroup('visitItems', 'id', oLastStory.visitItemsId(), function(lVisitItems) {
-		      _.each(lVisitItems,function(oVisitItem){
-						var oItem = new Cotton.UI.Story.Item.Element(oVisitItem);
-					});
-				});
-      });
-    });
-}
   
 });
 
