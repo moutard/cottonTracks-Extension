9fc61c1276d8e8fdc722217b24ad68e8d5289d5d
Raphael Korach <raphael.korach@gmail.com>
2013-02-26 16:36:16 -0300

    UI: Integrate menu.

--- ui/side_menu/sticker.js
+++ ui/side_menu/sticker.js
@@ -4,59 +4,26 @@ Cotton.UI.SideMenu.Sticker = Class.extend({
 
   _oMenu : null,
   _$sticker : null,
-  _$featuredImageContainer : null,
-  _$featuredImage : null,
+  _oStickerImage : null,
+  _oStickerInfos : null,
 
   init: function(oMenu){
-
 	  this._oMenu = oMenu;
-	  this._$sticker = $(".ct-sticker");
-	  this._$featuredImageContainer = $('<div class="ct-story_image"></div>');
-	  this._$featuredImage = $('<img class="resize" src="">');
 
-    //set values
-    var sFeaturedImage = this._oMenu.story().featuredImage();
-    if (sFeaturedImage != ""){
-      this._$featuredImage.attr('src', sFeaturedImage);
-      this._$featuredImageContainer.css({'background':' #4d4d4d'})
-    }
+	  this._$sticker = $('<div class="ct-sticker"></div>');
+	  this._oStickerImage = new Cotton.UI.SideMenu.StickerImage(this._oMenu);
+	  this._oStickerInfos = new Cotton.UI.SideMenu.StickerInfos(this._oMenu);
 
     //construct element
-	  this._$sticker.prepend(
-	    this._$featuredImageContainer.append(
-	      this._$featuredImage
-	    )
+	  this._$sticker.append(
+	    this._oStickerImage.$(),
+	    this._oStickerInfos.$()
 	  );
-
-	  this.resize(this._$featuredImage);
 
   },
 
-	resize : function($img) {
-	  $img.load(function(){
-      var self = $(this);
-
-      //image size and ratio
-      var iImWidth = self.width();
-      var iImHeight = self.height();
-      var fImRatio = iImWidth/iImHeight;
-
-      //div size and ratio
-      var iDivWidth = self.parent().width();
-      var iDivHeight = self.parent().height();
-      var fDivRatio = iDivWidth/iDivHeight;
-
-      if (fImRatio > fDivRatio) {
-        self.css('height',iDivHeight);
-        var iOverflow = self.width()-iDivWidth;
-        self.css('left',-iOverflow*0.5);
-      } else {
-        self.css('width',iDivWidth);
-        var iOverflow = self.height()-iDivHeight;
-        self.css('top',-iOverflow*0.25);
-      }
-      $(this).show();
-    });
-  }
+  $ : function(){
+	  return this._$sticker;
+  },
 
 });
