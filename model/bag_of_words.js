6eb1bbbe603453753cf85eec959a5a8c48ed3ec7
Raphael Moutard <raphael.moutard@gmail.com>
2013-02-27 15:40:18 +0100

    DBSCAN2 : increase word score in bag of words

--- model/bag_of_words.js
+++ model/bag_of_words.js
@@ -6,23 +6,36 @@
  * List of words present in the doc with their score. So the dna can be
  * represented as a vector.
  * Score can be frequency for example, but we can imagine more sophisticated
- * with bonus if the words was copy pasted or highlighted.
+ * with bonus if the words was copy pasted or highlighted. But this intelligence
+ *  shouldn't be in the bag of words structure.
  */
 
 Cotton.Model.BagOfWords = Class.extend({
 
-  _lBag : {}, // dictionary has some advantages and drawbacks but let's use it for the moemnt.
+  /**
+   * {Dictionnary} _lBag:
+   *  - key: {String} word
+   *  - value: {Float} score of the word. Bigger means that this word is
+   *  more important in this document.
+   *
+   *  Advantages of dictionnary structure :
+   *  words are sorted by alphabetical order. So easy to made a vector mode.
+   */
+  _lBag : {},
 
-  init : function(){
+  init : function() {
     var self = this;
   },
 
-  addWord : function(sWord, iScore){
-    var self = this;
-    self._lBag[sWord] = iScore;
+  addWord : function(sWord, iScore) {
+    this._lBag[sWord] = iScore;
   },
 
-  get : function(){
+  increaseWordScore : function(sWord, iScore) {
+    this._lBag[sWord] += iScore;
+  },
+
+  get : function() {
     return this._lBag;
   },
 });
