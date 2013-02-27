a8e10aee969ff766d2d489a0ad02b47466edefc1
Raphael Korach <raphael.korach@gmail.com>
2013-02-27 19:31:13 -0300

    Refactor: clean useless code

--- ui/story/item/content/item_content.js
+++ ui/story/item/content/item_content.js
@@ -17,26 +17,12 @@ Cotton.UI.Story.Item.Content.Element = Class.extend({
 
   _$item_content : null,
 
-  _oItemFeaturedImage : null,
-  _oItemInfo : null,
-  _oItemSummaryInfo : null,
-  _oItemMenu : null,
-  _oItemLargeMenu : null,
-  _oItemDna : null,
-
   init : function(oItem) {
     // current parent element.
     this._oItem = oItem;
 
     // current item.
     this._$item_content = this._oItem.$();
-
-    //     this._oItemFeaturedImage = new Cotton.UI.Story.Item.FeaturedImage(this);
-    //     this._oItemInfo = new Cotton.UI.Story.Item.Info(this);
-    //     this._oItemSummaryInfo = new Cotton.UI.Story.Item.SummaryInfo(this);
-    //     this._oItemMenu = new Cotton.UI.Story.Item.Menu(this);
-    //     this._oItemLargeMenu = new Cotton.UI.Story.Item.LargeMenu(this);
-    //     this._oItemDna = new Cotton.UI.Story.Item.Dna(this);
 
     // the construction of the element depends on the its type. So create the
     // element directly in the sub class.
@@ -48,15 +34,7 @@ Cotton.UI.Story.Item.Content.Element = Class.extend({
 
   item : function(){
     return this._oItem;
-  },
-
-  itemInfo : function(){
-	  return this._oItemInfo;
-  },
-
-  appendTo : function(oStoryLine) {
-    this._oItem.$().append(this._$item_content);
-  },
+  }
 
 });
 
