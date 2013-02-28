9ea6ab5089a8633030f5c0dd7a83fb59715e11e8
Raphael Korach <raphael.korach@gmail.com>
2013-02-28 16:22:54 -0300

    Compiler: add rkorach parameters in comments

--- py/parameters.py
+++ py/parameters.py
@@ -6,3 +6,6 @@ DESTINATION_PATH='/usr/local/rmoutard/cottontracks-beta/'
 #SOURCE_PATH='/Users/rmoutard/src/SubZoom-Proto1'
 #DESTINATION_PATH='/Users/rmoutard/src/ct-compiled'
 
+#GOOGLE_CLOSURE_COMPILER = "/Users/raphaelkorach/Code/workspace/google_closure_compiler/compiler.jar"
+#SOURCE_PATH='/Users/raphaelkorach/Code/workspace/cottonTracks/SubZoom-Proto1/'
+#DESTINATION_PATH='/Users/raphaelkorach/Code/workspace/cottonTracks/compiled/'
