a26cf2673bcfcebcdb80439ab589dad5cb97f751
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-03 15:14:29 +0100

    Tests : UrlParser tests

--- test/js/utils/url_parser_tests.js
+++ test/js/utils/url_parser_tests.js
@@ -9,7 +9,7 @@ module("Parse Url",{
 
 test("Parse Simple Url", function() {
   var urlSimple   = 'http://www.google.com/search?p=dede&q=keyword1+keyword2+keyword3&aq=autre';
-  var a = new parseUrl(urlSimple);
+  var a = new UrlParser(urlSimple);
   deepEqual(a.keywords,         // result
             ['keyword1', 'keyword2', 'keyword3'], // expected
             'unexpected keywords');
@@ -17,7 +17,7 @@ test("Parse Simple Url", function() {
 
 test("Parse google search url", function() {
   var urlComplexe = 'http://www.google.fr/search?sourceid=chrome-instant&ix=seb&ie=UTF-8&ion=1#hl=fr&gs_nf=1&cp=10&gs_id=3n&xhr=t&q=jennifer+aniston&pq=tets&pf=p&sclient=psy-ab&site=webhp&source=hp&pbx=1&oq=jennifer+a&aq=0&aqi=g4&aql=&gs_sm=&gs_upl=&bav=on.2,or.r_gc.r_pw.r_cp.,cf.osb&fp=6fc8c6804cede81f&ix=seb&ion=1&biw=1438&bih=727';
-  var b = new parseUrl(urlComplexe);
+  var b = new UrlParser(urlComplexe);
 
   deepEqual(b.keywords,
             ['jennifer', 'aniston'],
@@ -27,10 +27,11 @@ test("Parse google search url", function() {
 
 test("Parse google webhp Url", function() {
   var urlComplexe = 'http://www.google.fr/webhp?sourceid=chrome-instant&ix=seb&ie=UTF-8&ion=1#hl=fr&gs_nf=1&cp=10&gs_id=3n&xhr=t&q=jennifer+aniston&pq=tets&pf=p&sclient=psy-ab&site=webhp&source=hp&pbx=1&oq=jennifer+a&aq=0&aqi=g4&aql=&gs_sm=&gs_upl=&bav=on.2,or.r_gc.r_pw.r_cp.,cf.osb&fp=6fc8c6804cede81f&ix=seb&ion=1&biw=1438&bih=727';
-  var b = new parseUrl(urlComplexe);
+  var b = new UrlParser(urlComplexe);
 
+  // keywords are not generated for webph, they are only generated for search path name.
   deepEqual(b.keywords,
-            ['jennifer', 'aniston'],
+            undefined,
             'unexpected keywords');
 });
 
