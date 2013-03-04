3d24207cd61cf0a4fa8ff3e057a841915f482c4e
Raphael Korach <raphael.korach@gmail.com>
2013-03-04 00:56:47 -0300

    Refactor: console.log > DEBUG && console.debug

--- controller/background.js
+++ controller/background.js
@@ -263,7 +263,7 @@ Cotton.Controllers.Background = Class.extend({
 
 
 	                          oDatabase.put('visitItems', oVisitItem, function() {
-	                            console.log("update queryKeywords");
+	                            DEBUG && console.debug("update queryKeywords");
 	                          });
 	                        }
 
