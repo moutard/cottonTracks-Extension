1927bb87e3142964c4530ab6561f269ce01226c5
Raphael Korach <raphael.korach@gmail.com>
2013-03-06 16:50:11 -0300

    UI: getContent only for default items

--- ui/story/item/item_small_menu.js
+++ ui/story/item/item_small_menu.js
@@ -5,69 +5,86 @@
  */
 Cotton.UI.Story.Item.SmallMenu = Class.extend({
 
-  _oItemContent : null,
+    _oItemContent : null,
 
-  _$itemMenu : null,
+    _$itemMenu : null,
 
-  _$remove : null,
-  _$openLink : null,
-  _$open : null,
-  _$expand : null,
-  _$collapse : null,
-  _$getContent : null,
-  _$loading : null,
-  _bGettingContent : null,
+    _$remove : null,
+    _$openLink : null,
+    _$open : null,
+    _$expand : null,
+    _$collapse : null,
+    _$getContent : null,
+    _$loading : null,
+    _bGettingContent : null,
 
-  init : function(oItemContent) {
-    var self = this;
+    init : function(oItemContent) {
+      var self = this;
 
-    // current parent element.
-    this._oItemContent = oItemContent;
+      // current parent element.
+      this._oItemContent = oItemContent;
 
-    // current item
-    this._$itemMenu = $('<div class="ct-label-small-menu"></div>');
+      // current item
+      this._$itemMenu = $('<div class="ct-label-small-menu"></div>');
 
-    // current sub elements
-    this._$remove = $('<p>Remove</p>');
-    this._$openLink = $('<a href="" target="_blank"></a>');
-    this._$open = $('<p>Open</p>');
-    var bParagraph = ((oItemContent.item().visitItem().extractedDNA().allParagraphs().length > 0)
-      || (oItemContent.item().visitItem().extractedDNA().paragraphs().length > 0)
-      || (oItemContent.item().visitItem().extractedDNA().firstParagraph() != "") );
-    this._$expand = (bParagraph) ? $('<p class="expand">Expand</p>') : $('');
-    //do not append 'Get Content' if it has already been performed or
-    //if there is a paragraph
-    this._$getContent = (bParagraph
-        || this._oItemContent.item().isReloaded()) ? $('')
-      : $('<p class="get_content">Get Content</p>');
-    this._$collapse =  $('<p class="collapse">Collapse</p>');
-    this._$loading =  $('<img class="loading" src="/media/images/story/item/default_item/loading.gif">');
+      // current sub elements
+      this._$remove = $('<p>Remove</p>');
+      this._$openLink = $('<a href="" target="_blank"></a>');
+      this._$open = $('<p>Open</p>');
+      var bParagraph = ((oItemContent.item().visitItem().extractedDNA().allParagraphs().length > 0)
+        || (oItemContent.item().visitItem().extractedDNA().paragraphs().length > 0)
+        || (oItemContent.item().visitItem().extractedDNA().firstParagraph() != "") );
+      this._$expand = (bParagraph) ? $('<p class="expand">Expand</p>') : $('');
+      //do not append 'Get Content' if it has already been performed or
+      //if there is a paragraph
+      this._$getContent = (bParagraph || this._oItemContent.item().isReloaded()
+        || oItemContent.item().itemType() !== "default") ? $('')
+        : $('<p class="get_content">Get Content</p>');
+      this._$collapse =  $('<p class="collapse">Collapse</p>');
+      this._$loading =  $('<img class="loading" src="/media/images/story/item/default_item/loading.gif">');
 
-    //set actions on buttons
+      //set actions on buttons
 
-    //remove element
-    this._$remove.click(function(){
-      //TODO(rkorach): use only one db for the whole UI
-      self._oDatabase = new Cotton.DB.IndexedDB.Wrapper('ct', {
-        'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS
-      }, function() {
-      self._oDatabase.delete('visitItems', self._oItemContent.item().visitItem().id(),
-        function() {
-          self._oItemContent.item().container().isotope('remove',
-            self._oItemContent.item().$());
+      //remove element
+      this._$remove.click(function(){
+        //TODO(rkorach): use only one db for the whole UI
+        self._oDatabase = new Cotton.DB.IndexedDB.Wrapper('ct', {
+          'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS
+        }, function() {
+          self._oDatabase.delete('visitItems', self._oItemContent.item().visitItem().id(),
+            function() {
+              self._oItemContent.item().container().isotope('remove', self._oItemContent.item().$());
+          });
         });
       });
-    });
 
-    //get content
-	  this._$getContent.click(function(){
-		  chrome.tabs.create({
-			  "url" : self._oItemContent.item().visitItem().url(),
-			  "active" : false
-		  });
-		  self._bGettingContent = true;
-		  $(this).parent().append(self._$loading);
-      $(this).hide();
+      //expand reader
+		  this._$expand.click(function(){
+        oItemContent.item().$().css('height', '630px');
+        self.$().addClass("visible_action_menu");
+        oItemContent.item().container().isotope('reLayout');
+        $(this).hide();
+        self._$collapse.show();
+      });
+
+      //collapse reader
+      this._$collapse.click(function(){
+        oItemContent.item().$().css('height', '150px');
+        oItemContent.item().container().isotope('reLayout');
+        self.$().removeClass("visible_action_menu");
+        $(this).hide();
+        self._$expand.show();
+      });
+
+      //get content
+	    this._$getContent.click(function(){
+		    chrome.tabs.create({
+			    "url" : self._oItemContent.item().visitItem().url(),
+			    "active" : false
+		    });
+		    self._bGettingContent = true;
+		    $(this).parent().append(self._$loading);
+        $(this).hide();
 	  });
 
 	  chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
