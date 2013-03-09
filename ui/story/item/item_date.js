315cdabac06c9892918d72e16cd4aa67ff393b18
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-09 00:31:33 -0300

    Refactor: date format

--- ui/story/item/item_date.js
+++ ui/story/item/item_date.js
@@ -1,46 +1,55 @@
 'use strict';
 
 /**
- * Item Date Contains date and clock icon
+ * Item Date Contains clock icon and date
  */
-Cotton.UI.Story.Item.Date = Class
-    .extend({
+Cotton.UI.Story.Item.Date = Class.extend({
 
-      _oItemContent : null,
+  _oItemContent : null,
 
-      _$itemDate : null,
-      _$date : null,
-      _$clock : null,
+  _$itemDate : null,
+  _$date : null,
+  _$clock : null,
 
-      init : function(oItemContent) {
-        var self = this;
+  init : function(oItemContent) {
+    var self = this;
 
-        // current parent element.
-        this._oItemContent = oItemContent;
+    // current parent element.
+    this._oItemContent = oItemContent;
 
-        // current element
-        this._$itemDate = $('<div class="ct-item_date"></div>');
+    // current element
+    this._$itemDate = $('<div class="ct-item_date"></div>');
 
-        // current sub element
-        this._$clock = $('<img src="media/images/story/item/date_clock.png"/>')
-        this._$date = $('<h4></h4>');
+    // current sub elements
+    this._$clock = $('<img src="media/images/story/item/date_clock.png"/>')
+    this._$date = $('<h4></h4>');
 
-        // date
-        var oDate = new Date(this._oItemContent._oItem._oVisitItem.visitTime());
-        var lDate = oDate.toDateString().split(" ");
-        this._$date.text(lDate[2] + " " + lDate[1]);
+    // set date
+    var oDate = new Date(this._oItemContent._oItem._oVisitItem.visitTime());
+    var sDay = oDate.getDate();
+    var lMonth = ["January","February","March","April","May","June",
+                  "July","August","September","October","November","December"];
+    var sMonth = lMonth[oDate.getMonth()];
+    if (oDate.getHours() < 12){
+      var sHours = oDate.getHours();
+      var sAmPm = "am";
+    } else if (oDate.getHours() == 12){
+      var sHours = oDate.getHours();
+      var sAmPm = "pm";
+    } else {
+      var sHours = oDate.getHours() - 12;
+      var sAmPm = "pm";
+    }
+    var sMinutes = oDate.getMinutes();
+    this._$date.text(sDay + " " + sMonth + " - " + sHours + ":" + sMinutes + " " + sAmPm);
 
-        // construct item
-        self._$itemDate.append(self._$clock, self._$date);
+    // construct item
+    self._$itemDate.append(self._$clock, self._$date);
 
-      },
+  },
 
-      $ : function() {
-        return this._$itemDate;
-      },
+  $ : function() {
+    return this._$itemDate;
+  }
 
-      appendTo : function(oItemContent) {
-        oItemContent.$().append(this._$itemDate);
-      },
-
-    });
+});
