9b9294057bf35dbc7f89c7b3ee98df1f96fae21e
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-10 12:40:06 +0100

    Parameters

--- py/parameters.py
+++ py/parameters.py
@@ -2,10 +2,6 @@
 #SOURCE_PATH='/usr/local/rmoutard/sz/SubZoom-Proto1/'
 #DESTINATION_PATH='/usr/local/rmoutard/cottontracks-beta/'
 
-#GOOGLE_CLOSURE_COMPILER = "/Users/rmoutard/src/google_closure_compiler/compiler.jar"
-#SOURCE_PATH='/Users/rmoutard/src/SubZoom-Proto1'
-#DESTINATION_PATH='/Users/rmoutard/src/ct-compiled'
-
-GOOGLE_CLOSURE_COMPILER = "/Users/raphaelkorach/Code/workspace/google_closure_compiler/compiler.jar"
-SOURCE_PATH='/Users/raphaelkorach/Code/workspace/cottonTracks/SubZoom-Proto1/'
-DESTINATION_PATH='/Users/raphaelkorach/Code/workspace/cottonTracks/compiled/'
+GOOGLE_CLOSURE_COMPILER = "/Users/rmoutard/src/google_closure_compiler/compiler.jar"
+SOURCE_PATH='/Users/rmoutard/src/SubZoom-Proto1'
+DESTINATION_PATH='/Users/rmoutard/src/ct-compiled'
