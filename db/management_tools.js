4c1b7b2d6577ffdd9c8101739b4c6ba730bbd27f
moutard <raphael.moutard@gmail.com>
2013-03-11 10:29:43 +0100

    ManagementTools: update searchKeywords after updating the bag of words.

--- db/management_tools.js
+++ db/management_tools.js
@@ -42,11 +42,20 @@ Cotton.DB.ManagementTools.setStoryBagOfWords = function(iStoryId, dBagOfWords){
 
   self._oDatabase = new Cotton.DB.IndexedDB.Wrapper('ct', {
     'stories' : Cotton.Translators.STORY_TRANSLATORS,
+    'searchKeywords' : Cotton.Translators.SEARCH_KEYWORD_TRANSLATORS,
   }, function() {
     self._oDatabase.find('stories', 'id', iStoryId, function(oStory){
       oStory.dna().bagOfWords().setBag(dBagOfWords);
       self._oDatabase.put('stories', oStory, function(){
         console.log('bagOfWords updated.');
+        for(var sKey in dBagOfWords){
+          var oSearchKeyword = new Cotton.Model.SearchKeyword(sKey);
+          oSearchKeyword.addReferringStoryId(iStoryId);
+          self._oDatabase.putUniqueKeyword('searchKeywords', oSearchKeyword,
+            function(){
+              console.log('searchKeywords updated.');
+            });
+        }
       });
     });
   });
