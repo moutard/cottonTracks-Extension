b1eab3372c31004c44875fc7e3dea41525b50cb8
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-12 19:08:11 -0400

    Compiler: Keep pink parameters in comments for easier switch

--- py/parameters.py
+++ py/parameters.py
@@ -1,3 +1,7 @@
+#GOOGLE_CLOSURE_COMPILER = "/Users/raphaelkorach/Code/workspace/google_closure_compiler/compiler.jar"
+#SOURCE_PATH='/Users/raphaelkorach/Code/workspace/cottonTracks/SubZoom-Proto1/'
+#DESTINATION_PATH='/Users/raphaelkorach/Code/workspace/cottonTracks/compiled/'
+
 #GOOGLE_CLOSURE_COMPILER = "/usr/local/rmoutard/compiler.jar"
 #SOURCE_PATH='/usr/local/rmoutard/sz/SubZoom-Proto1/'
 #DESTINATION_PATH='/usr/local/rmoutard/cottontracks-beta/'
