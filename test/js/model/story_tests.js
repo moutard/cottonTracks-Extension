a4043c866f241e50790cf5125d195a09699d21ba
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-12 15:26:21 +0100

    Tests : add test to story model.

--- test/js/model/story_tests.js
+++ test/js/model/story_tests.js
@@ -1,3 +1,58 @@
+var lHistoryItems = [
+  {
+    'id':1,
+    'sStoryId' : "UNCLASSIFIED",
+    'sUrl': "http://cottontracks.com",
+    'sTitle': "cottonTracks - the application.",
+    'iLastVisitTime': 1362754370120.657
+
+  },
+  {
+    'id':2,
+    'sStoryId' : "UNCLASSIFIED",
+    'sUrl': "http://cottontracks.com",
+    'sTitle': "cottonTracks - the application.",
+    'iLastVisitTime': 1362740369641.147
+
+  },
+  {
+    'id':3,
+    'sStoryId' : "UNCLASSIFIED",
+    'sUrl': "http://cottontracks.com",
+    'sTitle': "cottonTracks - the application.",
+    'iLastVisitTime': 1362739338323.528
+
+  },
+  {
+    'id':4,
+    'sStoryId' : "UNCLASSIFIED",
+    'sUrl': "http://cottontracks.com",
+    'sTitle': "cottonTracks - the application.",
+    'iLastVisitTime': 1362739338323.528
+
+  },
+  {
+    'id':4,
+    'sStoryId' : "UNCLASSIFIED",
+    'sUrl': "http://cottontracks.com",
+    'sTitle': "cottonTracks - the application.",
+    'iLastVisitTime': 1362657401358.013
+
+  }
+];
+module(
+    "Cotton.Algo.Common.ClusterStory",
+    {
+      setup : function() {
+
+      },
+      teardown : function() {
+        // runs after each test
+      }
+    }
+);
+
+
 module("Cotton.Model.Story",{
   setup: function() {
     // runs before each test
@@ -11,3 +66,13 @@ test("init.", function() {
   var oStory = new Cotton.Model.Story();
   ok(oStory)
 });
+
+test("addDbRecordHistoryItem.", function() {
+  var oStory = new Cotton.Model.Story();
+  for(var i = 0; i < lHistoryItems.length; i++){
+    oStory.addDbRecordHistoryItem(lHistoryItems[i]);
+  }
+  equal(oStory.lastVisitTime(), 1362754370120.657);
+  deepEqual(oStory.historyItemsId(), [1,2,3,4]);
+});
+
