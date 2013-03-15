moutard/SubZoom-Proto1

UI: Count items

controller/lightyear.js
@@ -35,26 +35,50 @@ Cotton.Controllers.Lightyear = Class.extend({
   },
 
 	buildStory : function() {
-		self = this;
+		var self = this;
 		self._oDatabase = new Cotton.DB.IndexedDB.Wrapper('ct', {
           'stories' : Cotton.Translators.STORY_TRANSLATORS,
           'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS
     }, function() {
+	    self = self;
       self._oDatabase.getLast('stories', 'fLastVisitTime', function(oLastStory) {
         self._oDatabase.findGroup('visitItems', 'id', oLastStory.visitItemsId(), function(lVisitItems) {
 		      _.each(lVisitItems,function(oVisitItem){
 						var oItem = new Cotton.UI.Story.Item.Element(oVisitItem);
 					});
 					//place items on the grid with isotope
-					$('.ct-story_container').isotope({
-					  itemSelector : '.ct-story_item',
-					  layoutMode : 'fitColumns',
-					});
+					self.placeItems();
+          self.countItems();
 				});
 				var oSticker = new Cotton.UI.SideMenu.Menu(oLastStory);
       });
     });
-  }
+  },
+
+  countItems: function(){
+    var sAllCount = $('.ct-item').length;
+    $('.all_count').text(sAllCount);
+    var sArticlesCount = $('.ct-item-default').length;
+    $('.articles_count').text(sArticlesCount);
+    var sImagesCount = $('.ct-item-image').length;
+    $('.images_count').text(sImagesCount);
+    var sVideosCount = $('.ct-item-video').length;
+    $('.videos_count').text(sVideosCount);
+    var sMapsCount = $('.ct-item-maps').length;
+    $('.maps_count').text(sMapsCount);
+    var sSoundsCount = $('.ct-item-sound').length;
+    $('.sounds_count').text(sSoundsCount);
+    // ToDo (rkorach) : spécific case for quotes
+    var sQuotesCount = $('.ct-item-quote').length;
+    $('.quotes_count').text(sQuotesCount);
+  },
+
+  placeItems: function(){
+	  $('.ct-story_container').isotope({
+		  itemSelector : '.ct-story_item',
+		  layoutMode : 'fitColumns',
+		});
+	}
   
 });
 

moutard/cottonTracks-Extension

controller/lightyear.js
@@ -35,26 +35,50 @@ Cotton.Controllers.Lightyear = Class.extend({
   },
 
 	buildStory : function() {
-		self = this;
+		var self = this;
 		self._oDatabase = new Cotton.DB.IndexedDB.Wrapper('ct', {
           'stories' : Cotton.Translators.STORY_TRANSLATORS,
           'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS
     }, function() {
+	    self = self;
       self._oDatabase.getLast('stories', 'fLastVisitTime', function(oLastStory) {
         self._oDatabase.findGroup('visitItems', 'id', oLastStory.visitItemsId(), function(lVisitItems) {
 		      _.each(lVisitItems,function(oVisitItem){
 						var oItem = new Cotton.UI.Story.Item.Element(oVisitItem);
 					});
 					//place items on the grid with isotope
-					$('.ct-story_container').isotope({
-					  itemSelector : '.ct-story_item',
-					  layoutMode : 'fitColumns',
-					});
+					self.placeItems();
+          self.countItems();
 				});
 				var oSticker = new Cotton.UI.SideMenu.Menu(oLastStory);
       });
     });
-  }
+  },
+
+  countItems: function(){
+    var sAllCount = $('.ct-story_item').length;
+    $('.all_count').text(sAllCount);
+    var sArticlesCount = $('.ct-item-default').length;
+    $('.articles_count').text(sArticlesCount);
+    var sImagesCount = $('.ct-item-image').length;
+    $('.images_count').text(sImagesCount);
+    var sVideosCount = $('.ct-item-video').length;
+    $('.videos_count').text(sVideosCount);
+    var sMapsCount = $('.ct-item-maps').length;
+    $('.maps_count').text(sMapsCount);
+    var sSoundsCount = $('.ct-item-sound').length;
+    $('.sounds_count').text(sSoundsCount);
+    // ToDo (rkorach) : spécific case for quotes
+    var sQuotesCount = $('.ct-item-quote').length;
+    $('.quotes_count').text(sQuotesCount);
+  },
+
+  placeItems: function(){
+	  $('.ct-story_container').isotope({
+		  itemSelector : '.ct-story_item',
+		  layoutMode : 'fitColumns',
+		});
+	}
   
 });
 