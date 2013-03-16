2b895cdc39d5ec1f0ec5d4e6d09e0800e6a112e1
rmoutard <raphael.moutard@gmail.com>
2013-03-16 13:43:09 -0300

    Translators: add a etra method for historyItem translators.

--- test/js/db/translator_tests.js
+++ test/js/db/translator_tests.js
@@ -24,6 +24,18 @@ test("init", function() {
   ok(oTranslator);
 });
 
+test("extra method for historyItem translator.", function() {
+  var oTranslator = Cotton.Translators.HISTORY_ITEM_TRANSLATORS[0];
+  var oHistoryItem = oTranslator.chromeHistoryItemToObject({
+      "id": "18148",
+      "lastVisitTime": 1345067625020.5059,
+      "title": "(1) Japanese-French-German Roadtrip in France",
+      "typedCount": 0,
+      "url": "https://www.facebook.com/photo.php?fbid=10152068458770657&set=a.10152068448065657.900421.845025656&type=1",
+    });
+  equal(oHistoryItem.title(), "(1) Japanese-French-German Roadtrip in France")
+
+});
 
 module("Cotton.DB.TranslatorsCollection",{
   setup: function() {
