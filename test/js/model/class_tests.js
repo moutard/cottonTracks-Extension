62b59341b465a637224f41f0a19b4e50b198e77b
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-16 17:18:09 -0300

    Tests: class tests more accurate and precise

--- test/js/model/class_tests.js
+++ test/js/model/class_tests.js
@@ -1,7 +1,7 @@
 'use strict';
 var L = [6,7,8];
 
-module("Cotton.Model.Class",{
+module("Cotton.Model.Class.Bad",{
   setup: function() {
     // runs before each test
   },
@@ -10,7 +10,72 @@ module("Cotton.Model.Class",{
   }
 });
 
-var ClassTest = Class.extend({
+var BadClassTest = Class.extend({
+  _int : 0,
+  _array : [],
+  init : function() {
+  },
+  int : function() {
+    return this._int;
+  },
+  array : function() {
+    return this._array;
+  },
+  addInArray : function(u) {
+    this._array.push(u);
+  },
+  setArray : function(array) {
+    this._array = array;
+  }
+});
+
+test("setArray complexe.", function() {
+  var oClass = new GoodClassTest();
+  // Good behavior only in you initialized in the init function.
+  oClass.setArray([1,2,3]);
+  deepEqual(oClass.array(), [1,2,3]);
+
+  var oClass = new GoodClassTest();
+  deepEqual(oClass.array(), []);
+  deepEqual(oClass._array, []);
+
+  // Good behavior here the array is reset by init.
+  var oClass2 = new GoodClassTest();
+  deepEqual(oClass2.array(), []);
+  deepEqual(oClass2._array, []);
+
+});
+
+test("_array IS A STATIC variable shared by all the class if not reinitialized, in the init function.", function() {
+  var oClass = new BadClassTest();
+  deepEqual(oClass.array(), []);
+  // Problem appears with addInArray !
+  for(var i = 0, iLength = L.length; i < iLength; i++){
+    oClass.addInArray(L[i]);
+  };
+  deepEqual(oClass.array(), [6,7,8]);
+
+  oClass = new BadClassTest();
+  // The array variable is not reset if you don't do it in the init.
+  // We would have expected an EMPTY ARRAY !
+  deepEqual(oClass.array(), [6,7,8]);
+
+  var oClass2 = new BadClassTest();
+  // We would have expected an EMPTY ARRAY !
+  deepEqual(oClass2.array(), [6,7,8]);
+
+});
+
+module("Cotton.Model.Class.Good",{
+  setup: function() {
+    // runs before each test
+  },
+  teardown: function() {
+    // runs after each test
+  }
+});
+
+var GoodClassTest = Class.extend({
   _int : undefined,
   _array : null,
   init : function() {
@@ -26,59 +91,57 @@ var ClassTest = Class.extend({
   addInArray : function(u) {
     this._array.push(u);
   },
-
   setArray : function(array) {
     this._array = array;
-  },
-  reject : function(i){
-    this._array = _.reject(this._array, function(a){ return a===i;});
-  },
+  }
 });
 
 test("init.", function() {
-  var oClass = new ClassTest();
+  var oClass = new GoodClassTest();
   equal(oClass.array(), oClass._array);
   equal(oClass.int(), oClass._int);
 
 });
 
 test("setArray.", function() {
-  var oClass = new ClassTest();
+  var oClass = new GoodClassTest();
   oClass.setArray([1,2,3]);
   deepEqual(oClass.array(), [1,2,3]);
   equal(oClass.int(), oClass._int);
 
 });
 
-test("addInArray.", function() {
-  var oClass = new ClassTest();
+test("setArray complexe.", function() {
+  var oClass = new GoodClassTest();
+  // Good behavior only in you initialized in the init function.
+  oClass.setArray([1,2,3]);
+  deepEqual(oClass.array(), [1,2,3]);
+
+  var oClass = new GoodClassTest();
+  deepEqual(oClass.array(), []);
+  deepEqual(oClass._array, []);
+
+  // Good behavior here the array is reset by init.
+  var oClass2 = new GoodClassTest();
+  deepEqual(oClass2.array(), []);
+  deepEqual(oClass2._array, []);
+
+});
+
+test("_array IS A STATIC variable shared by all the class if not reinitialized, in the init function.", function() {
+  var oClass = new GoodClassTest();
   deepEqual(oClass.array(), []);
   for(var i = 0, iLength = L.length; i < iLength; i++){
     oClass.addInArray(L[i]);
   };
   deepEqual(oClass.array(), [6,7,8]);
 
-  oClass = new ClassTest();
+  oClass = new GoodClassTest();
+  // Good behavior here the array is reset by init.
   deepEqual(oClass.array(), []);
 
-  var oClass2 = new ClassTest();
+  var oClass2 = new GoodClassTest();
+  // Good behavior here the array is reset by init.
   deepEqual(oClass2.array(), []);
-
-});
-
-
-test("_array IS A STATIC variable shared by all the class if not reinitialized, in the init function.", function() {
-  var oClass = new ClassTest();
-  oClass.setArray([1,2,3]);
-  deepEqual(oClass.array(), [1,2,3]);
-
-  var oClass = new ClassTest();
-  deepEqual(oClass.array(), [1,2,3]);
-  deepEqual(oClass._array, [1,2,3]);
-
-  // BECAREFUL !!! WE WOULD EXPECT AN EMPTY ARRAY.
-  var oClass2 = new ClassTest();
-  deepEqual(oClass2.array(), [1,2,3]);
-  deepEqual(oClass2._array, [1,2,3]);
 
 });
