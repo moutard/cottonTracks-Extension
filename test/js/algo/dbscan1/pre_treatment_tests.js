8def024dfdca62e7cabf5b69e7d59918e43cf409
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-16 16:55:51 -0300

    DBSCAN1: tests pretreatment

--- test/js/algo/dbscan1/pre_treatment_tests.js
+++ test/js/algo/dbscan1/pre_treatment_tests.js
@@ -1,7 +1,7 @@
 'use strict';
 var oHistoryItem1 = {
-  sUrl:"http://fr.wikipedia.org/wiki/EtherPad",
-  sTitle: "EtherPad - Wikipédia",
+  sUrl:"http://www.imdb.com/title/tt1014759/",
+  sTitle: "Alice in wonderland (2010)",
   iLastVisitTime:1361980612982.948,
   oExtractedDNA : {
     lExtractedWords : ['alice', 'wonderland', 'movie', 'Burton', 'Tim'],
@@ -10,8 +10,8 @@ var oHistoryItem1 = {
 };
 
 var oHistoryItem2 = {
-  sUrl:"http://fr.wikipedia.org/wiki/EtherPad",
-  sTitle: "EtherPad - Wikipédia",
+  sUrl:"http://en.wikipedia.org/wiki/Alice's_Adventures_in_Wonderland",
+  sTitle: "Alice in wonderland - Wikipédia",
   iLastVisitTime:1361980612982.948,
 
   oExtractedDNA : {
@@ -21,7 +21,7 @@ var oHistoryItem2 = {
 };
 
 var oHistoryItem3 = {
-  sUrl:"https://www.google.com/search?q=etherpad&oq=etherpad",
+  sUrl:"https://www.google.com/search?q=alice+in+wonderland&oq=etherpad",
   sTitle: "EtherPad - Google search",
   iLastVisitTime:1361980612982.948,
 
@@ -49,9 +49,27 @@ module(
     }
 );
 
-test("compute closest generated page.", function() {
-  Cotton.Algo.PreTreatment.computeClosestGoogleSearchPage([oHistoryItem1, oHistoryItem2, oHistoryItem3]);
-  equal(oHistoryItem1['oExtractedDNA']['sClosestGoogleSearchPage'], "https://www.google.com/search?q=etherpad&oq=etherpad");
+test("computeParseUrl.", function() {
+  Cotton.Algo.PreTreatment.computeParseUrl([oHistoryItem1, oHistoryItem2, oHistoryItem3]);
+  equal(oHistoryItem1['sHostname'], "www.imdb.com");
 });
 
+
+test("compute closest generated page.", function() {
+
+  Cotton.Algo.PreTreatment.computeParseUrl([oHistoryItem1, oHistoryItem2, oHistoryItem3]);
+  Cotton.Algo.PreTreatment.computeClosestGoogleSearchPage([oHistoryItem1, oHistoryItem2, oHistoryItem3]);
+  equal(oHistoryItem1['oExtractedDNA']['sClosestGoogleSearchPage'],
+    "https://www.google.com/search?q=alice+in+wonderland&oq=etherpad");
+});
+
+test("computeExtractedWords.", function() {
+  var lHistoryItem = japan;
+  // Needed to compute ExtractedWords.
+  // FIXME(rmoutard): avoid dependances.
+  lHistoryItem = Cotton.Algo.PreTreatment.computeParseUrl(lHistoryItem);
+  Cotton.Algo.PreTreatment.computeExtractedWords(lHistoryItem);
+  deepEqual(lHistoryItem[7]['oExtractedDNA']['lExtractedWords'],
+    ["time", "tokyo", "recherche", "google", "search"]);
+});
 
