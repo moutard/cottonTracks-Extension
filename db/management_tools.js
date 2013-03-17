0ed0e90184ae6fabadb12c69868f3eed047aaff2
rmoutard <raphael.moutard@gmail.com>
2013-03-17 18:39:58 -0300

    ManagementTools: create story from file.

--- db/management_tools.js
+++ db/management_tools.js
@@ -22,6 +22,57 @@ Cotton.DB.ManagementTools.createStory = function(sTitle, sFeaturedImage){
   });
 };
 
+Cotton.DB.ManagementTools.createStoryFromList = function(sTitle, sFeaturedImage,
+    lHistoryItems){
+  var self = this;
+  var oStory = new Cotton.Model.Story();
+  oStory.setTitle(sTitle);
+  oStory.setFeaturedImage(sFeaturedImage);
+  oStory.setLastVisitTime(new Date().getTime());
+  var lHistoryItemsId = [];
+  self._oDatabase = new Cotton.DB.IndexedDB.Wrapper('ct', {
+    'stories' : Cotton.Translators.STORY_TRANSLATORS,
+    'historyItems' : Cotton.Translators.HISTORY_ITEM_TRANSLATORS,
+    'searchKeywords' : Cotton.Translators.SEARCH_KEYWORD_TRANSLATORS,
+  }, function() {
+    for(var i = 0, dHistoryItem; dHistoryItem = lHistoryItems[i]; i++){
+      //var oTranslator = self._oDatabase._translatorForDbRecord('historyItems', dHistoryItem);
+      //var oHistoryItem = oTranslator.
+      var oHistoryItem = new Cotton.Model.HistoryItem();
+      oHistoryItem.setLastVisitTime(dHistoryItem['iLastVisitTime']);
+      oHistoryItem.setTitle(dHistoryItem['sTitle']);
+      oHistoryItem.initUrl(dHistoryItem['sUrl']);
+      self._oDatabase.putUniqueHistoryItem('historyItems', oHistoryItem, function(iId){
+        var _iId = iId;
+        console.log(_iId);
+        lHistoryItemsId.push(_iId);
+        console.log(lHistoryItemsId.length + '' + lHistoryItems.length);
+        if(lHistoryItemsId.length === lHistoryItems.length){
+            var oStory = new Cotton.Model.Story();
+            oStory.setTitle(sTitle);
+            oStory.setFeaturedImage(sFeaturedImage);
+            oStory.setLastVisitTime(new Date().getTime());
+            oStory._lHistoryItemsId = lHistoryItemsId;
+            self._oDatabase.put('stories', oStory, function(iStoryId){
+              console.log(iStoryId);
+              console.log('story created');
+
+              var lList = sTitle.split(' ');
+              for(var i=0, sKeyword; sKeyword = lList[i]; i++){
+                var oSearchKeyword = new Cotton.Model.SearchKeyword(sKeyword);
+                oSearchKeyword.addReferringStoryId(iStoryId);
+                self._oDatabase.putUniqueKeyword('searchKeywords',oSearchKeyword,
+                  function(){
+                });
+              }
+            });
+        }
+      });
+    }
+  });
+};
+
+
 Cotton.DB.ManagementTools.addHistoryItemToStory = function(iStoryId, iHistoryItemId){
   var self = this;
 
