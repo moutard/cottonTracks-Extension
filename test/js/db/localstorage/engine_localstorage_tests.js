85afcba0a6bf6fbd45dc3fabb90a76d325e5c0d6
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-17 11:29:03 -0300

    LocalStorage: resolve tests for engine and add more tests

--- test/js/db/localstorage/engine_localstorage_tests.js
+++ test/js/db/localstorage/engine_localstorage_tests.js
@@ -1,9 +1,25 @@
-var oEngine;
+'use strict';
 
 module("Cotton.DB.LocalStorage.Engine",{
   setup: function() {
     // runs before each test
-    oEngine = new Cotton.DB.LocalStorage.Engine('test', {
+    },
+  teardown: function() {
+    // runs after each test
+  }
+});
+
+test("init one store no index.", function() {
+   var oEngine1 = new Cotton.DB.LocalStorage.Engine('test-noindex', {
+    "historyItems": {}
+  });
+
+   ok(oEngine1);
+   oEngine1.purge();
+});
+
+test("init one store 2 indexes.", function() {
+   var oEngine2 = new Cotton.DB.LocalStorage.Engine('test-noindex', {
     "stories": {
       "fLastVisitTime": {
         "unique": false
@@ -11,23 +27,66 @@ module("Cotton.DB.LocalStorage.Engine",{
       "id": {
         "unique": true
       },
-      "lTags": {
-        "multiEntry": true,
-        "unique": false
-      }
-    },
-    "historyItems": {}
+    }
   });
 
-  },
-  teardown: function() {
-    // runs after each test
-    oEngine.purge();
-  }
+   ok(oEngine2);
+   oEngine2.purge();
 });
 
-test("init", function() {
-   ok(oEngine);
+test("_getStoreLocation.", function() {
+   var oEngine2 = new Cotton.DB.LocalStorage.Engine('test-noindex', {
+    "stories": {
+      "fLastVisitTime": {
+        "unique": false
+      },
+      "id": {
+        "unique": true
+      },
+    }
+  });
+
+   equal(oEngine2._getStoreLocation('stories'), 'test-noindex-stories');
+   oEngine2.purge();
+
 });
 
+test("get and put.", function() {
+  expect(3);
+   var oEngine3 = new Cotton.DB.LocalStorage.Engine('test-noindex', {
+    "stories": {
+      "fLastVisitTime": {
+        "unique": false
+      },
+      "id": {
+        "unique": true
+      },
+    }
+  });
+   var dItem = {'id':3, 'title': 'Alice in wonderland'};
+   deepEqual(oEngine3.getList('stories'), []);
+   oEngine3.put('stories', dItem);
+   deepEqual(oEngine3.getList('stories'), [dItem]);
+   oEngine3.purge();
+   throws(function(){
+     oEngine3.getList('stories');
+   }, "store should not exist.");
 
+});
+
+test("throws error on non existing store.", function() {
+   var oEngine3 = new Cotton.DB.LocalStorage.Engine('test-noindex', {
+    "stories": {
+      "fLastVisitTime": {
+        "unique": false
+      },
+      "id": {
+        "unique": true
+      },
+    }
+  });
+  throws(function(){
+    oEngine3.getList('historyItems');
+  },  "store should not exist.")
+
+});
