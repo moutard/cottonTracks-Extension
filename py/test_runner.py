640103690a7be49991801a7dceef08a9501252d9
rmoutard <raphael.moutard@gmail.com>
2013-03-16 22:55:24 -0300

    Compiler: launch algo test

--- py/test_runner.py
+++ py/test_runner.py
@@ -5,9 +5,11 @@ def runTest():
 
   if sPlatform == 'Darwin':
     os.system("open -a Google\ Chrome unit_tests.html")
+    os.system("open -a Google\ Chrome algo_test.html")
   if sPlatform == 'Linux':
     os.system("google-chrome unit_tests.html")
+    os.system("google-chrome algo_test.html")
   if sPlatform == 'Windows':
     pass;
-  
-  
+
+
