6518a02106f5d190f31e25f781bee4c32db9ddbc
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-17 17:07:01 -0300

    Compiler: change configs before preprod compiling and prod compiling

--- py/precompiler.py
+++ py/precompiler.py
@@ -8,6 +8,15 @@ class PreCompiler(object):
     os.system("sed -i '' -e 's/.*bActiveSumup.*/bActiveSumup:false,/' '%s'" % psConfigFile)
     os.system("sed -i '' -e 's/.*bAnalytics.*/bAnalytics:true,/' '%s'" % psConfigFile)
     os.system("sed -i '' -e 's/.*bLoggingEnabled.*/bLoggingEnabled:true,/' '%s'" % psConfigFile)
-  
+
   def setVersion(self):
     pass
+
+  def setProdConfig(self, psConfigFile):
+    # TODO(rmoutard): find a better solution.
+    os.system("sed -i '' -e 's/.*bDevMode.*/bDevMode:false,/' '%s'" % psConfigFile)
+    os.system("sed -i '' -e 's/.*bActiveSumup.*/bActiveSumup:false,/' '%s'" % psConfigFile)
+    os.system("sed -i '' -e 's/.*bAnalytics.*/bAnalytics:true,/' '%s'" % psConfigFile)
+    os.system("sed -i '' -e 's/.*bLoggingEnabled.*/bLoggingEnabled:false,/' '%s'" % psConfigFile)
+
+
