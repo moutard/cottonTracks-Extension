0f02434b55c6b0c77e5a09e4ae38aa0d2bd9ddcf
Raphael Korach <raphael.korach@gmail.com>
2013-03-17 21:48:06 -0400

    Refactor: Get story in database from lightyear.js

--- controller/lightyear.js
+++ controller/lightyear.js
@@ -10,20 +10,31 @@
 Cotton.Controllers.Lightyear = Class.extend({
 
   /**
-   * "Model" in MVC pattern. Global Store, that allow controller to make call to
-   * the database. So it Contains 'historyItems' and 'stories'.
+   * Global Store, that allow controller to make call to
+   * the database. So it Contains 'historyItems', 'stories' and 'searchKeywords'.
    */
   _oDatabase : null,
 
   /**
-   * "View" in MVC pattern. Global view, contains the Menu, the StoryContainer
+   * Global view, contains the Menu, the StoryContainer.
+   * UI elements act as their own controllers.
    */
   _oWorld : null,
 
   /**
-   * Triggered story
+   * state of World creation
+   */
+  _bWorldReady : null,
+
+  /**
+   * Triggered storyId
    **/
   _iStoryId : null,
+
+  /**
+   * Triggered story
+   **/
+  _oStory : null,
 
   /**
    * @constructor
@@ -33,14 +44,44 @@ Cotton.Controllers.Lightyear = Class.extend({
     var self = this;
     LOG && console.log("Controller - init -");
 
-    $(window).ready(function(){
-      Cotton.UI.WORLD = self._oWorld = new Cotton.UI.World();
+    self._oDatabase = new Cotton.DB.IndexedDB.Wrapper('ct', {
+        'stories' : Cotton.Translators.STORY_TRANSLATORS,
+        'historyItems' : Cotton.Translators.HISTORY_ITEM_TRANSLATORS
+    }, function() {
       chrome.extension.sendMessage({
         'action': 'get_trigger_story'
       }, function(response){
-        self._oWorld.buildStory(response['trigger_id']);
+        self._iStoryId = response['trigger_id'];
+        self._oDatabase.find('stories', 'id', self._iStoryId, function(oStory) {
+          self._oDatabase.findGroup('historyItems', 'id', oStory.historyItemsId(),
+          function(lHistoryItems) {
+            self._lHistoryItems = lHistoryItems;
+            self.buildStoryFromWorld();
+          });
+          self._oStory = oStory;
+          self.buildMenuFromWorld();
+        });
       });
     });
+
+    $(window).ready(function(){
+      self._oWorld = new Cotton.UI.World(self);
+      self._bWorldReady = true;
+      self.buildStoryFromWorld();
+      self.buildMenuFromWorld();
+    });
+  },
+
+  buildStoryFromWorld : function(){
+    if (this._lHistoryItems && this._bWorldReady ) {
+      this._oWorld.buildStory(this._lHistoryItems)
+    }
+  },
+
+  buildMenuFromWorld : function(){
+    if (this._oStory && this._bWorldReady ) {
+      this._oWorld.buildMenu(this._oStory)
+    }
   },
 
 });
