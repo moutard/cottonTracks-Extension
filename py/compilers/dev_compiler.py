ac1f73d2f824eae3fb03a4ee58c4797c9e060c8b
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-17 22:35:17 -0300

    Compiler: new compile function for dev_compiler

--- py/compilers/dev_compiler.py
+++ py/compilers/dev_compiler.py
@@ -10,6 +10,11 @@ class DEVCompiler(Compiler):
     self._PROD_DESTINATION_PATH = os.path.join(DESTINATION_PATH, "dev")
     Compiler.__init__(self, SOURCE_PATH, self._PROD_DESTINATION_PATH)
 
+ def compile(self):
+    self.pretreatment(self._SOURCE_PATH, self._DESTINATION_PATH)
+    os.chdir(self._DESTINATION_PATH)
+    Compiler.compile(self)
+
   def compileJs(self, plJavascriptFiles, psOutput="output.min.js"):
     if len(plJavascriptFiles) > 0 :
       print "cat %s > %s" % (" ".join(plJavascriptFiles), psOutput)
