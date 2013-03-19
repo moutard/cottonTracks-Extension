5a31448325436b1818ee99771c0aed44eea8237e
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-19 18:31:28 -0300

    UI: content video add test for sVideoType

--- test/js/ui/story/item/content/item_content_video_tests.js
+++ test/js/ui/story/item/content/item_content_video_tests.js
@@ -16,3 +16,16 @@ test('init.', function() {
   ok(oContent);
 });
 
+test('check sVideo parameters', function() {
+  var oHistoryItem = new Cotton.Model.HistoryItem({
+    'sUrl': "http://www.youtube.com/watch?v=g0lbfEb8MMk",
+    'sTitle' : "Alice In Wonderland - All In The Golden Afternoon - Youtube",
+    'iLastVisitTime' : 1363648480386,
+    'oExtractedDNA' : {}
+  });
+
+  var oContent = new Cotton.UI.Story.Item.Content.Video(
+    oHistoryItem, "youtube", "g0lbfEb8MMk");
+  equal(oContent._sVideoType, "youtube");
+});
+
