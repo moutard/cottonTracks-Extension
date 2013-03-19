bd221d00fab0a251010e9b80a65fe2e2ae6d670c
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-18 22:19:14 -0300

    UI: date in content and simplify

--- ui/story/item/content/date.js
+++ ui/story/item/content/date.js
@@ -3,30 +3,35 @@
 /**
  * Item Date Contains clock icon and date
  */
-Cotton.UI.Story.Item.Date = Class.extend({
+Cotton.UI.Story.Item.Content.Date = Class.extend({
 
   _oItemContent : null,
 
-  _$itemDate : null,
+  _iLastVisitTime : null,
+
+  _$contentDate : null,
   _$date : null,
   _$clock : null,
 
-  init : function(oItemContent) {
+  init : function(iLastVisitTime, oItemContent) {
     var self = this;
 
     // current parent element.
     this._oItemContent = oItemContent;
 
+    this._iLastVisitTime = iLastVisitTime;
+
     // current element
-    this._$itemDate = $('<div class="ct-item_date"></div>');
+    this._$contentDate = $('<div class="ct-item_content_date"></div>');
 
     // current sub elements
     this._$clock = $('<img src="media/images/story/item/date_clock.png"/>')
     this._$date = $('<h4></h4>');
 
     // set date
-    var oDate = new Date(this._oItemContent._oItem._oHistoryItem.lastVisitTime());
+    var oDate = new Date(iLastVisitTime);
     var sDay = oDate.getDate();
+    // TODO(rmoutard) use moment.js
     var lMonth = ["January","February","March","April","May","June",
                   "July","August","September","October","November","December"];
     var sMonth = lMonth[oDate.getMonth()];
@@ -44,12 +49,12 @@ Cotton.UI.Story.Item.Date = Class.extend({
     this._$date.text(sDay + " " + sMonth + " - " + sHours + ":" + sMinutes + " " + sAmPm);
 
     // construct item
-    self._$itemDate.append(self._$clock, self._$date);
+    self._$contentDate.append(self._$clock, self._$date);
 
   },
 
   $ : function() {
-    return this._$itemDate;
+    return this._$contentDate;
   }
 
 });
