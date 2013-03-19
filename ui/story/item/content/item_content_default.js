79a3e467571a661d907f82acc1fa82112459a71f
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-19 12:15:42 -0300

    UI: refactor item_content_default

--- ui/story/item/content/item_content_default.js
+++ ui/story/item/content/item_content_default.js
@@ -5,8 +5,14 @@
  */
 Cotton.UI.Story.Item.Content.Default = Cotton.UI.Story.Item.Content.Element.extend({
 
+  // parent element
+  // _oItem (defined in item_content)
+
+  // current element
+  // _$content (defined in item content)
+
+  // sub elements.
   _oItemFeaturedImage : null,
-  _$itemInfo : null,
   _oItemTitle : null,
   _oItemDate : null,
   _oItemQuoteIndicator : null,
@@ -14,22 +20,23 @@ Cotton.UI.Story.Item.Content.Default = Cotton.UI.Story.Item.Content.Element.exte
   _oItemMenu : null,
   _oItemReader : null,
 
-  init : function(oItem) {
-    this._super(oItem);
-    this._oItemFeaturedImage = new Cotton.UI.Story.Item.FeaturedImage(this);
-    this._$itemInfo = $('<div class="ct-item_info"></div>');
-    this._oItemTitle = new Cotton.UI.Story.Item.Title(this);
-    this._oItemDate = new Cotton.UI.Story.Item.Date(this);
-    this._oItemQuoteIndicator = new Cotton.UI.Story.Item.QuoteIndicator(this);
-    this._oItemLabel = new Cotton.UI.Story.Item.SmallLabel(this);
-    this._oItemMenu = new Cotton.UI.Story.Item.SmallMenu(this);
-    this._oItemReader = new Cotton.UI.Story.Item.Reader(this);
+  init : function(oHistoryItem, oItem) {
+    this._super(oHistoryItem, oItem);
 
+    this._oItemTitle = new Cotton.UI.Story.Item.Content.Title(oHistoryItem.title(), this);
+    this._oItemDate = new Cotton.UI.Story.Item.Content.Date(oHistoryItem.lastVisitTime(), this);
+    this._oItemLabel = new Cotton.UI.Story.Item.Content.SmallLabel(oHistoryItem.url(), this);
+
+    this._oItemFeaturedImage = new Cotton.UI.Story.Item.Content.Dna.FeaturedImage(this);
+    this._oItemQuoteIndicator = new Cotton.UI.Story.Item.Content.Dna.QuoteIndicator(this);
+    this._oItemReader = new Cotton.UI.Story.Item.Content.Dna.Reader(this);
+
+    this._oItemMenu = new Cotton.UI.Story.Item.SmallMenu(this);
     // create the item
     oItem.$().addClass('ct-item-default');
       this._$item_content.append(
         this._oItemFeaturedImage.$(),
-        this._$itemInfo.append(
+        this._$content.append(
           this._oItemTitle.$(),
           this._oItemDate.$(),
           this._oItemQuoteIndicator.$(),
