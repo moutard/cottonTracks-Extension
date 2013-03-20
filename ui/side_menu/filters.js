cc56a07b61f06d03dcfaf6a6bcf98c6ff37b4ac8
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-20 15:37:32 -0300

    UI: refactor filters

--- ui/side_menu/filters.js
+++ ui/side_menu/filters.js
@@ -1,13 +1,22 @@
 'use strict';
 
 /**
- *   Filters for elements in the story.
- **/
+ * Filters for elements in the story.
+ */
 
 Cotton.UI.SideMenu.Filters = Class.extend({
 
+  /**
+   * {Cotton.UI.SideMenu.Menu} parent element.
+   */
   _oMenu : null,
+
+  /**
+   * {DOM} current element.
+   */
   _$filters : null,
+
+
   _$all : null,
   _$article : null,
   _$images : null,
@@ -17,35 +26,36 @@ Cotton.UI.SideMenu.Filters = Class.extend({
   _$quotes : null,
 
  init: function(oMenu){
-	  this._oMenu = oMenu;
-	
-	  this._$filters = $('<div class="ct-filters"></div>');
-	  this._$all = $('<div class="ct-filter all_filter" data-filter="*">');
-		this._$articles = $('<div class="ct-filter articles_filter" data-filter=".ct-item-default">');
-		this._$images = $('<div class="ct-filter images_filter" data-filter=".ct-item-image">');
-		this._$videos = $('<div class="ct-filter videos_filter" data-filter=".ct-item-video">');
-		this._$maps = $('<div class="ct-filter maps_filter" data-filter=".ct-item-map">');
-		this._$sounds = $('<div class="ct-filter sounds_filter" data-filter=".ct-item-quote">');
-		this._$quotes = $('<div class="ct-filter quotes_filter">');
-		
-		//set values
-		var sAllCount = 'All (<span class="all_count"></span>)'
-		this._$all.html(sAllCount);
-		var sArticlesCount = 'Articles (<span class="articles_count"></span>)'
-		this._$articles.html(sArticlesCount);
-		var sImagesCount = 'Images (<span class="images_count"></span>)'
-		this._$images.html(sImagesCount);
-		var sVideosCount = 'Videos (<span class="videos_count"></span>)'
-		this._$videos.html(sVideosCount);
-		var sMapsCount = 'Maps (<span class="maps_count"></span>)'
-		this._$maps.html(sMapsCount);
-		var sSoundsCount = 'Sounds (<span class="sounds_count"></span>)'
-		this._$sounds.html(sSoundsCount);
-		var sQuotesCount = 'Quotes (<span class="quotes_count"></span>)'
-		this._$quotes.html(sQuotesCount);
-		
+    this._oMenu = oMenu;
+
+    // FIXME(rmoutard) ??? why it's defined twice ?
+    this._$filters = $('<div class="ct-filters"></div>');
+    this._$all = $('<div class="ct-filter all_filter" data-filter="*">');
+    this._$articles = $('<div class="ct-filter articles_filter" data-filter=".ct-item-default">');
+    this._$images = $('<div class="ct-filter images_filter" data-filter=".ct-item-image">');
+    this._$videos = $('<div class="ct-filter videos_filter" data-filter=".ct-item-video">');
+    this._$maps = $('<div class="ct-filter maps_filter" data-filter=".ct-item-map">');
+    this._$sounds = $('<div class="ct-filter sounds_filter" data-filter=".ct-item-quote">');
+    this._$quotes = $('<div class="ct-filter quotes_filter">');
+
+    //set values
+    var sAllCount = 'All (<span class="all_count"></span>)'
+    this._$all.html(sAllCount);
+    var sArticlesCount = 'Articles (<span class="articles_count"></span>)'
+    this._$articles.html(sArticlesCount);
+    var sImagesCount = 'Images (<span class="images_count"></span>)'
+    this._$images.html(sImagesCount);
+    var sVideosCount = 'Videos (<span class="videos_count"></span>)'
+    this._$videos.html(sVideosCount);
+    var sMapsCount = 'Maps (<span class="maps_count"></span>)'
+    this._$maps.html(sMapsCount);
+    var sSoundsCount = 'Sounds (<span class="sounds_count"></span>)'
+    this._$sounds.html(sSoundsCount);
+    var sQuotesCount = 'Quotes (<span class="quotes_count"></span>)'
+    this._$quotes.html(sQuotesCount);
+
     //construct element
-	  this._$filters.append( 
+    this._$filters.append(
       this._$all,
       this._$articles,
       this._$images,
@@ -53,15 +63,12 @@ Cotton.UI.SideMenu.Filters = Class.extend({
       this._$maps,
       this._$sounds,
       this._$quotes
-	  );
-	
-	  this._$filters.children().click(function(){
-		  //do something
-	  });
+    );
+
   },
 
   $ : function(){
-	  return this._$filters;
+    return this._$filters;
   },
 
 });
