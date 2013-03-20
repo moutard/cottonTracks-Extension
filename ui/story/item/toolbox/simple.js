b8d81ad9285942273e3218649ca2509cbd57c449
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-20 14:40:19 -0300

    UI: refactor toolbox simple and complexe

--- ui/story/item/toolbox/simple.js
+++ ui/story/item/toolbox/simple.js
@@ -1,65 +1,42 @@
 'use strict';
 
 /**
- * Item Large Menu is the action menu for videos and maps items
+ * Toolbox Large is the action menu for videos and maps items
  * Contains open and remove buttons
  */
-Cotton.UI.Story.Item.LargeMenu = Class.extend({
+Cotton.UI.Story.Item.Toolbox.Simple = Class.extend({
 
-  _oItemContent : null,
+  _oItem : null,
 
-  _$itemLargeMenu : null,
+  _$toolbox : null,
 
   _$remove : null,
   _$openLink : null,
   _$open : null,
 
-  init : function(oItemContent) {
-    var self = this;
+  init : function(sUrl, oItem) {
 
     // current parent element.
-    this._oItemContent = oItemContent;
+    this._oItem = oItem;
 
     // current item
-    this._$itemLargeMenu = $('<div class="ct-label-large-menu"></div>');
+    this._$toolbox = $('<div class="ct-toolbox"></div>');
 
     // current sub elements
     this._$remove = $('<p>Remove</p>');
-    this._$openingLink = $('<a href="" target="blank"></a>');
+    this._$openingLink = $('<a href="' + sUrl + '" target="blank"></a>');
     this._$open = $('<p>Open</p>');
 
-    // set values
-    // url
-    var sUrl = this._oItemContent.item().historyItem().url();
-    self._$openingLink.attr('href',sUrl);
-
-    //remove element
-    this._$remove.click(function(){
-      //TODO(rkorach): use only one db for the whole UI
-      self._oDatabase = new Cotton.DB.IndexedDB.Wrapper('ct', {
-          'historyItems' : Cotton.Translators.HISTORY_ITEM_TRANSLATORS
-      }, function() {
-        self._oDatabase.delete('historyItems',
-          self._oItemContent.item().historyItem().id(),
-          function() {
-            self._oItemContent.item().container().isotope('remove',
-              self._oItemContent.item().$(), function() {
-                Cotton.UI.WORLD.countItems();
-            });
-        });
-      });
-    });
-
     // construct item
-    self._$itemLargeMenu.append(
-      self._$openingLink.append(self._$open),
-      self._$remove
+    this._$toolbox.append(
+      this._$openingLink.append(this._$open),
+      this._$remove
     );
 
   },
 
   $ : function() {
-    return this._$itemLargeMenu;
+    return this._$toolbox;
   }
 
 });
