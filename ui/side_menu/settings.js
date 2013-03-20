1b85ac4b0fb135ac3cf21a0f41dccde7eab064c2
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-20 15:38:32 -0300

    UI: refactor menu

--- ui/side_menu/settings.js
+++ ui/side_menu/settings.js
@@ -5,23 +5,23 @@
  **/
 
 Cotton.UI.SideMenu.Settings = Class.extend({
-	
-	_oMenu : null,
-	
-	init : function(oMenu){		
-		this._oMenu = oMenu;
+
+  _oMenu : null,
+
+  init : function(oMenu){
+    this._oMenu = oMenu;
 
     this._$settings = $('<div class="ct-settings"></div>');
 
     //set value
 
-	  //trigger settings page
-	  this._$settings.click(function(){
-		  //do something
-	  });
-	},
+    //trigger settings page
+    this._$settings.click(function(){
+      //do something
+    });
+  },
 
   $ : function(){
-	  return this._$settings;
+    return this._$settings;
   }
 });
