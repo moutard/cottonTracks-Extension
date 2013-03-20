686ad023c12a9c5484179ec1b56cd4852333391a
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-20 15:33:06 -0300

    UI: refactor sticker

---
+++ ui/side_menu/preview/sticker/sticker.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/**
+ * Sticker in SumUp, contains story thumbnail, and story info block (with title,
+ * and content detail == number of elements)
+ **/
+
+Cotton.UI.SideMenu.Preview.Sticker.Element = Class.extend({
+
+  /**
+   * {Cotton.UI.SideMenu.Preview} parent element.
+   */
+  _oPreview : null,
+
+  /**
+   * {DOM} current element.
+   */
+  _$sticker : null,
+
+  /**
+   * {Cotton.UI.SideMenu.Preview.Sticker.Image}
+   */
+  _oStickerImage : null,
+
+  /**
+   * {Cotton.UI.SideMenu.Preview.Sticker.Infos}
+   */
+  _oStickerInfos : null,
+
+  init: function(sTile, sImage, oPreview) {
+	  this._oPreview = oPreview;
+
+	  this._$sticker = $('<div class="ct-sticker"></div>');
+	  this._oStickerImage = new Cotton.UI.SideMenu.Preview.Sticker.Image(sImage, this);
+	  this._oStickerInfos = new Cotton.UI.SideMenu.Preview.Sticker.Infos(sTile, this);
+
+    // Construct element.
+	  this._$sticker.append(
+	    this._oStickerImage.$(),
+	    this._oStickerInfos.$()
+	  );
+
+  },
+
+  $ : function() {
+	  return this._$sticker;
+  },
+
+  preview : function() {
+    return this._oPreview;
+  },
+
+  stickerImage : function() {
+    return this._oStickerImage;
+  },
+
+  stickerInfos : function() {
+    return this._oStickerInfos;
+  }
+
+
+});
