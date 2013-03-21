1e387838881ece4fba25a333c7f10ceb0ef16efe
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-21 17:06:54 -0300

    UI: change type from "default" to "article" in theme

--- ui/side_menu/filters.js
+++ ui/side_menu/filters.js
@@ -16,59 +16,66 @@ Cotton.UI.SideMenu.Filters = Class.extend({
    */
   _$filters : null,
 
+  /**
+   * Dictionnary that contains
+   *  key : {String} filter name
+   *  value : {DOM} corresponding DOM element.
+   */
+  _dFilters : {},
 
-  _$all : null,
-  _$article : null,
-  _$images : null,
-  _$videos : null,
-  _$maps : null,
-  _$sounds : null,
-  _$quotes : null,
-
- init: function(oMenu){
+  init: function(oDispacher, oMenu) {
+    var self = this;
     this._oMenu = oMenu;
+    this._dFilters = {};
 
-    // FIXME(rmoutard) ??? why it's defined twice ?
+    this._lFilters = ['all', 'article', 'image', 'video', 'map',
+      'sound', 'quote'];
+
+    this._oDispacher = oDispacher;
+
+    this._oDispacher.suscribe('update_filters', this, function(dFiltersCount){
+      for (var sFilter in dFiltersCount) {
+        self.setFilterCount(sFilter, dFiltersCount[sFilter]);
+      }
+    });
     this._$filters = $('<div class="ct-filters"></div>');
-    this._$all = $('<div class="ct-filter all_filter" data-filter="*">');
-    this._$articles = $('<div class="ct-filter articles_filter" data-filter=".ct-item-default">');
-    this._$images = $('<div class="ct-filter images_filter" data-filter=".ct-item-image">');
-    this._$videos = $('<div class="ct-filter videos_filter" data-filter=".ct-item-video">');
-    this._$maps = $('<div class="ct-filter maps_filter" data-filter=".ct-item-map">');
-    this._$sounds = $('<div class="ct-filter sounds_filter" data-filter=".ct-item-quote">');
-    this._$quotes = $('<div class="ct-filter quotes_filter">');
 
-    //set values
-    var sAllCount = 'All (<span class="all_count"></span>)'
-    this._$all.html(sAllCount);
-    var sArticlesCount = 'Articles (<span class="articles_count"></span>)'
-    this._$articles.html(sArticlesCount);
-    var sImagesCount = 'Images (<span class="images_count"></span>)'
-    this._$images.html(sImagesCount);
-    var sVideosCount = 'Videos (<span class="videos_count"></span>)'
-    this._$videos.html(sVideosCount);
-    var sMapsCount = 'Maps (<span class="maps_count"></span>)'
-    this._$maps.html(sMapsCount);
-    var sSoundsCount = 'Sounds (<span class="sounds_count"></span>)'
-    this._$sounds.html(sSoundsCount);
-    var sQuotesCount = 'Quotes (<span class="quotes_count"></span>)'
-    this._$quotes.html(sQuotesCount);
+    function createFilterDOM(sFilter, sFilterCount) {
+      sFilterCount = sFilterCount || 0;
+      self._dFilters[sFilter] = $('<span class="all_count"></span>').text(sFilterCount);
+      return  $('<div class="ct-filter "></div>').append(
+          $('<span></span>').text(sFilter + 's  ('),
+          self._dFilters[sFilter],
+          $('<span></span>').text(')')).click(function(){
+            // Update the story on click.
+            if(sFilter === "all") {
+              sFilter = '*';
+            } else {
+              sFilter = '.' + sFilter;
+            }
+            self._oDispacher.send('story:filter', {
+              'filter': sFilter
+            });
+          });
+    };
 
+    var lDOMFiltersElements = [];
+    for (var i = 0, sFilter; sFilter=this._lFilters[i]; i++){
+      lDOMFiltersElements.push(createFilterDOM(sFilter));
+    }
     //construct element
-    this._$filters.append(
-      this._$all,
-      this._$articles,
-      this._$images,
-      this._$videos,
-      this._$maps,
-      this._$sounds,
-      this._$quotes
-    );
+    this._$filters.append(lDOMFiltersElements);
 
   },
 
   $ : function(){
     return this._$filters;
+  },
+
+  setFilterCount : function(sFilter, iCount) {
+    if (this._dFilters[sFilter]) {
+      this._dFilters[sFilter].text(iCount);
+    }
   },
 
 });
