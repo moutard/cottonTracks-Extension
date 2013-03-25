e9d6c53d33a3e8dab81db8dd9d243de31901c0df
Raphael Korach <raphael.korach@gmail.com>
2013-03-25 09:28:32 -0400

    UI: add a link to titles

--- ui/story/item/content/brick/title.js
+++ ui/story/item/content/brick/title.js
@@ -21,13 +21,16 @@ Cotton.UI.Story.Item.Content.Brick.Title = Class.extend({
   _$title : null,
 
 
-  init : function(sTitle, oItemContent) {
+  init : function(sTitle, oItemContent, sUrl) {
 
     // current parent element.
     this._oItemContent = oItemContent;
 
     // current item
     this._$title = $('<h3></h3>').text(sTitle);
+    if (sUrl){
+      this._$title = $('<a href="' + sUrl + '" target="blank"></a>').append(this._$title);
+    }
 
   },
 
