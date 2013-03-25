2cc914f65d90534bc4de2e1a9280174f6dda4c7f
rmoutard <raphael.moutard@gmail.com>
2013-03-25 13:37:06 -0300

    Algo: test dbscan3 worker

--- test/js/algo/dbscan3/dbscan3_tests.js
+++ test/js/algo/dbscan3/dbscan3_tests.js
@@ -46,4 +46,34 @@ test("algo of dbscan3 worker.", function() {
   });
 
 });
+/*
+test("algo of dbscan3 worker for hadrien.", function() {
+  var lSampleHistoryItems = hadrien_source;
+  var lHistoryItems = Cotton.Algo.PreTreatment.suite(lSampleHistoryItems);
 
+  // PARAMETERS
+  // Max Distance between neighborhood
+  var fEps = Cotton.Config.Parameters.fEps;
+  // Min Points in a cluster
+  var iMinPts = Cotton.Config.Parameters.iMinPts;
+
+  Cotton.Algo.roughlySeparateSession(lHistoryItems, function(lSession) {
+    // For each rough session, launch dbscan1.
+    console.log("New session : " + lSession.length);
+    console.log(lSession);
+    // TODO(rmoutard) : Maybe create a worker, by session. or use a queue.
+    var iNbCluster = Cotton.Algo.DBSCAN(lSession, fEps, iMinPts,
+        Cotton.Algo.Distance.meaning);
+    //
+    // This worker has no access to window or DOM. So update DOM should be done
+    // in the main thread.
+
+    var dData = {};
+    dData['iNbCluster'] = iNbCluster;
+    dData['lHistoryItems'] = lSession;
+
+    equal(iNbCluster, 0);
+  });
+
+});
+*/
