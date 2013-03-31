686e96f36bec74587fe031e784aba3d8dc1e3350
Raphael Moutard <raphael.moutard@gmail.com>
2013-03-31 10:57:35 -0300

    Algo: update black list with Hadrien words

--- algo/common/words/black_list.js
+++ algo/common/words/black_list.js
@@ -1,5 +1,14 @@
 Cotton.Algo.Common.Words.isInBlackList = function(sWord) {
-  var blackList = ['htm', 'html', 'and', 'the', 'pdf'];
+var blackList = ['htm', 'and', 'the', 'pdf',
+"and", "also", "or", "no", "so", "for", "else", "but", "yet", "still", "anyway",
+"both", "not", "also", "only", "too", "either", "neither", "rather", "than",
+"more", "less", "whether", "neither", "besides", "because", "since", "rather",
+"though", "unless", "even", "although", "while", "whereas", "despite", "spite",
+"regardless", "where", "wherever", "while", "since", "until", "the", "once",
+"furthermore", "moreover", "additionally", "besides", "firstly", "secondly",
+"finally", "instead", "otherwise", "thus", "hence", "accordingly", "anyhow",
+"nevertheless", "nonetheless", "however", "meanwhile", "subsequently", "afterward"
+];
 
   return blackList.indexOf(sWord) !== -1;
 };
