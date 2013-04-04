a2837615f884ad28c9cc194ae4638170902a9edf
Raphael Moutard <raphael.moutard@gmail.com>
2013-04-04 19:43:38 -0300

    Config: use specific fEps and iMinPts for dbscan3

--- config/config.js
+++ config/config.js
@@ -19,47 +19,22 @@ Cotton.Config.Parameters = {
   ],
   iSliceTime : 1000 * 60 * 5, // closestGeneratedPages
 
-  // DBSCAN
-  fEpsTime : 1000 * 60 * 5,
-  fEps : 0.40, // Max Distance between neighborhood
-  iMinPts : 5, // Min Points in a cluster
-  iMaxResult : 10000, // The maximum number of results to retrieve in the
-  // chrome history
-  distanceCoeff : {
-    id : 0.10,
-    lastVisitTime : 0.35,
-    commonWords : 0.70,
-    queryWords : 0.30,
-    penalty : 0.3,
-  },
-
-  // storySELECT
-  bUseRelevance : true,
-  iMaxNumberOfStories : 10,
-  computeRelevanceCoeff : {
-    'length' : 0.2,
-    'lastVisitTime' : 0.2,
-    'hostname' : 0.2,
-    'search' : 0.2
+   // DBSCAN2
+  dbscan2 : {
+    fEps : 17,
+    iMinPts : 6,
   },
 
   // DBSCAN3
-  distanceMeaning : {
-    fEps : 0.40,
-    iMinPts : 5,
-  },
-  distanceVisitTime : {
-    fEps : 3 * 60 * 1000,
-    iMinPts : 5,
+  dbscan3 : {
+    iMaxResult : 10000, // The maximum number of results to retrieve in the
+    fEps : 17,
+    iMinPts : 6,
   },
 
   // BAG OF WORDS
   scoreForExtractedWords : 3,
   scoreForQueryWords : 5,
-
-  // UI
-  sGrid : "MostVisited", // choose among "MostVisited" or "Favorites"
-  iNbMostVisited : 10,
 
   // PROD
   bDevMode : true,
