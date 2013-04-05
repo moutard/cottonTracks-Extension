745770a86627956c86c5c7c6da602671f44b408a
Raphael Moutard <raphael.moutard@gmail.com>
2013-04-05 11:56:28 -0300

    Test: solve: side menu tests

--- test/js/ui/side_menu/preview/preview_tests.js
+++ test/js/ui/side_menu/preview/preview_tests.js
@@ -7,7 +7,9 @@ module('Cotton.UI.SideMenu.Preview.Element', {
 test('init.', function() {
   var sTitle = "Alice in wonderland";
   var sStoryImage = "http://upload.wikimedia.org/wikipedia/commons/6/6d/Alice_in_wonderland_1951.jpg";
+
+  var oDispacher = new MockDispacher();
   var oPreviewElement = new Cotton.UI.SideMenu.Preview.Element(
-    sTitle, sStoryImage);
+    sTitle, sStoryImage, oDispacher);
   ok(oPreviewElement);
 });
--- test/js/ui/side_menu/preview/sticker/sticker_tests.js
+++ test/js/ui/side_menu/preview/sticker/sticker_tests.js
@@ -7,7 +7,8 @@ module('Cotton.UI.SideMenu.Preview.Sticker.Element', {
 test('init.', function() {
   var sTitle = "Alice in wonderland"
   var sStoryImage = "http://upload.wikimedia.org/wikipedia/commons/6/6d/Alice_in_wonderland_1951.jpg";
+  var oDispacher = new MockDispacher();
   var oStickerElement = new Cotton.UI.SideMenu.Preview.Sticker.Element(
-    sTitle, sStoryImage);
+    sTitle, sStoryImage, oDispacher);
   ok(oStickerElement);
 });
