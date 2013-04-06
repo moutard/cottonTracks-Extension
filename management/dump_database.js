58feb31fc4131fcb803a6c9c5ca950429e5b0675
Raphael Korach <raphael.korach@gmail.com>
2013-04-05 21:09:09 -0400

    Management: DumpRawStory(InFile)

--- management/dump_database.js
+++ management/dump_database.js
@@ -40,7 +40,7 @@ Cotton.Management.dumpStoryFromDb = function(iStoryId, mActionWithStory){
 };
 
 /**
- * Dump the chrome history in it's raw version in a file.
+ * Dump a selected story in a file.
  */
 Cotton.Management.dumpStoryFromDbInFile = function (iStoryId) {
   Cotton.Management.dumpStoryFromDb(iStoryId, function(dStoryOfHistoryDict){
@@ -48,4 +48,68 @@ Cotton.Management.dumpStoryFromDbInFile = function (iStoryId) {
     var sUriContent = "data:application/octet-stream," + encodeURIComponent(sRecord);
     window.open(sUriContent, 'chrome_history_source_yourname');
   });
+};
+
+/**
+ * Dumps a story with its complete visitItems selected with its id from the db
+ * @param function
+ */
+Cotton.Management.dumpRawStoryFromDb = function(iStoryId, mActionWithStory){
+  var dStory = {};
+  var oDatabase = new Cotton.DB.IndexedDB.Wrapper('ct', {
+      'stories' : Cotton.Translators.STORY_TRANSLATORS,
+      'historyItems' : Cotton.Translators.HISTORY_ITEM_TRANSLATORS,
+      'searchKeywords' : Cotton.Translators.SEARCH_KEYWORD_TRANSLATORS
+    }, function() {
+      oDatabase.find('stories', 'id', iStoryId, function(oStory){
+
+        var lStoryTranslators = Cotton.Translators.STORY_TRANSLATORS;
+        var oStoryTranslator = lStoryTranslators[lStoryTranslators.length - 1];
+        dStory['story'] = oStoryTranslator.objectToDbRecord(oStory);
+
+        var lHistoryItemsIds = oStory.historyItemsId();
+        var iLengthOfStory = lHistoryItemsIds.length;
+        oDatabase.findGroup('historyItems', 'id', lHistoryItemsIds, function(lHistoryItems){
+          dStory['historyItems'] = [];
+          var lHistoryItemTranslators = Cotton.Translators.HISTORY_ITEM_TRANSLATORS;
+          var oHistoryItemTranslator = lHistoryItemTranslators[lHistoryItemTranslators.length - 1];
+
+          for (var i = 0, oHistoryItem; oHistoryItem = lHistoryItems[i]; i++){
+            dStory['historyItems'][i] = oHistoryItemTranslator.objectToDbRecord(oHistoryItem);
+          }
+          mActionWithStory (dStory);
+        });
+      });
+  });
+};
+
+/**
+ * Dump a selected story unaltered in a JSON file.
+ */
+Cotton.Management.dumpRawStoryFromDbInFile = function (iStoryId) {
+  Cotton.Management.dumpRawStoryFromDb(iStoryId, function(dStory){
+    var sRecord = JSON.stringify(dStory);
+    var sUriContent = "data:application/octet-stream," + encodeURIComponent(sRecord);
+    window.open(sUriContent, 'chrome_history_source_yourname');
+  });
+};
+
+/**
+ * Dump a selected story unaltered in a JSON file.
+ */
+Cotton.Management.dumpGroupRawStoryFromDbInFile = function (lStoryIds) {
+  var iNumberOfStories = lStoryIds.length;
+  var lStories = [];
+  var iCount = 0;
+  for (var i = 0, iStoryId; iStoryId = lStoryIds[i]; i++){
+    Cotton.Management.dumpRawStoryFromDb(iStoryId, function(dStory){
+      lStories.push(dStory);
+      iCount++;
+      if (iCount === iNumberOfStories){
+        var sRecord = JSON.stringify(lStories);
+        var sUriContent = "data:application/octet-stream," + encodeURIComponent(sRecord);
+        window.open(sUriContent, 'chrome_history_source_yourname');
+      }
+    });
+  }
 };
