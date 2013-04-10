b307988f3425799c5f809263b98781855862811e
Raphael Korach <raphael.korach@gmail.com>
2013-04-10 13:24:02 -0400

    Test: fix populate_db test

--- test/js/db/populate_db_tests.js
+++ test/js/db/populate_db_tests.js
@@ -11,6 +11,6 @@ module("Cotton.DB.Populate",{
 test("preRemoveTools.", function() {
   var lChromeHistoryItem = chrome_history_source_japan;
   var lResult = Cotton.DB.Populate.preRemoveTools(lChromeHistoryItem);
-  equal(lResult.length, 30);
+  equal(lResult.length, 29);
 });
 
