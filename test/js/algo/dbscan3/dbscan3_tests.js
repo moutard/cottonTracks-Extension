258cb5c60f765067aadccbbbceccea58def5d616
Raphael Korach <raphael.korach@gmail.com>
2013-04-10 13:24:42 -0400

    Test: update algo tests with new algo structure

--- test/js/algo/dbscan3/dbscan3_tests.js
+++ test/js/algo/dbscan3/dbscan3_tests.js
@@ -51,32 +51,45 @@ Cotton.Algo.simpleCluster = function(lHistoryItems, iNbCluster){
 
 };
 test('detect session for visititems.', function() {
-  var lSampleHistoryItems = chrome_visit_source_green.slice();
-  lSampleHistoryItems.sort(function(a,b){
+  var lSampleVisitItems = chrome_visit_source_green.slice();
+  lSampleVisitItems.sort(function(a,b){
     return b['lastVisitTime'] - a['lastVisitTime'];
   });
-  DEBUG && console.debug(lSampleHistoryItems);
+  DEBUG && console.debug(lSampleVisitItems);
   var iSessionId = 0;
-  Cotton.Algo.roughlySeparateSession(lSampleHistoryItems,
+  Cotton.Algo.roughlySeparateSession(lSampleVisitItems,
     function(lNewRoughSession){
       if(lNewRoughSession.length < 10) {
         DEBUG && console.debug("A session with length < 10:");
         DEBUG && console.debug(lNewRoughSession);
         iSessionId += 1;
       }
-      ok((iSessionId < 30), iSessionId + ' ' + lNewRoughSession.length);
+      ok((iSessionId < 40), iSessionId + ' ' + lNewRoughSession.length);
   });
 });
 
 test("algo of dbscan3 worker.", function() {
-  var lSampleHistoryItems = cotton_history_source_japan;
-  var lHistoryItems = Cotton.Algo.PreTreatment.suite(lSampleHistoryItems);
+  var lSampleHistoryItems = cotton_history_source_hadrien;
 
+  // we need oHistoryItems objects to compute bagOfWords
+  var lTranslators = Cotton.Translators.HISTORY_ITEM_TRANSLATORS;
+  var oTranslator = lTranslators[lTranslators.length - 1];
+
+  var lHistoryItems = [];
+  for (var i = 0, dHistoryItem; dHistoryItem = lSampleHistoryItems[i]; i++){
+    lHistoryItems.push(oTranslator._mDbRecordToObjectConverter(dHistoryItem));
+  }
+  lHistoryItems = Cotton.DB.Populate.computeBagOfWordsForHistoryItemsList(lHistoryItems);
+  lHistoryItems = Cotton.DB.Populate.removeHistoryItemsWithoutBagOfWords(lHistoryItems);
+  // then back to dictionaries for the algo, just like in the worker
+  for (var i = 0, oHistoryItem; oHistoryItem = lHistoryItems[i]; i++){
+    lHistoryItems[i] = oTranslator._mObjectToDbRecordConverter(oHistoryItem);
+  }
   // PARAMETERS
   // Max Distance between neighborhood
   var fEps = Cotton.Config.Parameters.dbscan3.fEps;
   // Min Points in a cluster
-  var iMinPts = Cotton.Config.Parameters.iMinPts;
+  var iMinPts = Cotton.Config.Parameters.dbscan3.iMinPts;
 
   Cotton.Algo.roughlySeparateSession(lHistoryItems, function(lSession) {
     // For each rough session, launch dbscan1.
@@ -84,7 +97,7 @@ test("algo of dbscan3 worker.", function() {
     DEBUG && console.debug(lSession);
     // TODO(rmoutard) : Maybe create a worker, by session. or use a queue.
     var iNbCluster = Cotton.Algo.DBSCAN(lSession, fEps, iMinPts,
-        Cotton.Algo.Distance.meaning);
+        Cotton.Algo.Score.DBRecord.HistoryItem);
     /**
      * This worker has no access to window or DOM. So update DOM should be done
      * in the main thread.
@@ -93,186 +106,91 @@ test("algo of dbscan3 worker.", function() {
     dData['iNbCluster'] = iNbCluster;
     dData['lHistoryItems'] = lSession;
 
-    equal(iNbCluster, 0);
+    ok(true, "dbscan3 runs");
   });
 
 });
 
-test("algo of dbscan3 worker for hadrien WITH COSINE with very high fEps (200)", function() {
-  DEBUG && console.debug('HADRIEN');
+test("algo of dbscan3 worker for hadrien with very low fEps (0)", function() {
 
-  // Take the chrome history raw.
-  var lSampleHistoryItems = chrome_history_source_foot;
+  var lSampleHistoryItems = cotton_history_source_hadrien;
 
-  DEBUG && console.debug("Start with chrome raw history that contains elements :");
-  DEBUG && console.debug(lSampleHistoryItems.length);
-  // Make pretreatment and translate into cotton history item.
-  var lHistoryItems = Cotton.DB.Populate.Suite(lSampleHistoryItems);
+  // we need oHistoryItems objects to compute bagOfWords
+  var lTranslators = Cotton.Translators.HISTORY_ITEM_TRANSLATORS;
+  var oTranslator = lTranslators[lTranslators.length - 1];
 
-  DEBUG && console.debug("After pre treatment history that contains elements :");
-  DEBUG && console.debug(lHistoryItems.length);
+  var lHistoryItems = [];
+  for (var i = 0, dHistoryItem; dHistoryItem = lSampleHistoryItems[i]; i++){
+    lHistoryItems.push(oTranslator._mDbRecordToObjectConverter(dHistoryItem));
+  }
+  lHistoryItems = Cotton.DB.Populate.computeBagOfWordsForHistoryItemsList(lHistoryItems);
+  lHistoryItems = Cotton.DB.Populate.removeHistoryItemsWithoutBagOfWords(lHistoryItems);
+  // then back to dictionaries for the algo, just like in the worker
+  for (var i = 0, oHistoryItem; oHistoryItem = lHistoryItems[i]; i++){
+    lHistoryItems[i] = oTranslator._mObjectToDbRecordConverter(oHistoryItem);
+  }
+  // PARAMETERS
+  // Max Distance between neighborhood
+  var fEps = 0;
+  // Min Points in a cluster
+  var iMinPts = Cotton.Config.Parameters.dbscan3.iMinPts;
 
-  // Do not really populate db.
+  var iNbCluster = Cotton.Algo.DBSCAN(lHistoryItems, fEps, iMinPts,
+      Cotton.Algo.Score.DBRecord.HistoryItem);
+  /**
+   * This worker has no access to window or DOM. So update DOM should be done
+   * in the main thread.
+   */
+  var dData = {};
+  dData['iNbCluster'] = iNbCluster;
+  dData['lHistoryItems'] = lHistoryItems;
 
-  // As the dbscan is launch in a worker we need to serialize data.
-  var oTranslator = Cotton.Translators.HISTORY_ITEM_TRANSLATORS[0];
-  var lSerializedHistoryItems = [];
-  for(var i = 0, oIDBHistoryItem; oIDBHistoryItem = lHistoryItems[i]; i++) {
-    lSerializedHistoryItems.push(oTranslator.objectToDbRecord(oIDBHistoryItem));
+  var dStories = Cotton.Algo.simpleCluster(dData['lHistoryItems'],
+                                          dData['iNbCluster']);
+
+  DEBUG && console.debug(dStories);
+  equal(iNbCluster, 1);
+  equal(dStories[0].length, lHistoryItems.length);
+});
+
+test("algo of dbscan3 worker for hadrien with high fEps (200)", function() {
+
+  var lSampleHistoryItems = cotton_history_source_hadrien;
+
+  // we need oHistoryItems objects to compute bagOfWords
+  var lTranslators = Cotton.Translators.HISTORY_ITEM_TRANSLATORS;
+  var oTranslator = lTranslators[lTranslators.length - 1];
+
+  var lHistoryItems = [];
+  for (var i = 0, dHistoryItem; dHistoryItem = lSampleHistoryItems[i]; i++){
+    lHistoryItems.push(oTranslator._mDbRecordToObjectConverter(dHistoryItem));
+  }
+  lHistoryItems = Cotton.DB.Populate.computeBagOfWordsForHistoryItemsList(lHistoryItems);
+  lHistoryItems = Cotton.DB.Populate.removeHistoryItemsWithoutBagOfWords(lHistoryItems);
+  // then back to dictionaries for the algo, just like in the worker
+  for (var i = 0, oHistoryItem; oHistoryItem = lHistoryItems[i]; i++){
+    lHistoryItems[i] = oTranslator._mObjectToDbRecordConverter(oHistoryItem);
   }
   // PARAMETERS
   // Max Distance between neighborhood
   var fEps = 200;
   // Min Points in a cluster
-  var iMinPts = Cotton.Config.Parameters.iMinPts;
+  var iMinPts = Cotton.Config.Parameters.dbscan3.iMinPts;
 
-  var iNbCluster = Cotton.Algo.DBSCAN(lSerializedHistoryItems, fEps, iMinPts,
-      Cotton.Algo.Distance.CosineHistoryItem);
-  //
-  // This worker has no access to window or DOM. So update DOM should be done
-  // in the main thread.
-
+  var iNbCluster = Cotton.Algo.DBSCAN(lHistoryItems, fEps, iMinPts,
+      Cotton.Algo.Score.DBRecord.HistoryItem);
+  /**
+   * This worker has no access to window or DOM. So update DOM should be done
+   * in the main thread.
+   */
   var dData = {};
   dData['iNbCluster'] = iNbCluster;
-  dData['lHistoryItems'] = lSerializedHistoryItems;
-  // because we don't have visittime for this example use simple cluster.
+  dData['lHistoryItems'] = lHistoryItems;
+
   var dStories = Cotton.Algo.simpleCluster(dData['lHistoryItems'],
                                           dData['iNbCluster']);
-  DEBUG && console.debug(dStories);
-  equal(iNbCluster, 1);
-  equal(dStories[0].length, 79);
 
-});
-test("algo of dbscan3 worker for hadrien WITH COSINE with very small fEps (10)", function() {
-  DEBUG && console.debug('HADRIEN');
-
-  // Take the chrome history raw.
-  var lSampleHistoryItems = chrome_history_source_foot;
-
-  DEBUG && console.debug("Start with chrome raw history that contains elements :");
-  DEBUG && console.debug(lSampleHistoryItems.length);
-  // Make pretreatment and translate into cotton history item.
-  var lHistoryItems = Cotton.DB.Populate.Suite(lSampleHistoryItems);
-
-  DEBUG && console.debug("After pre treatment history that contains elements :");
-  DEBUG && console.debug(lHistoryItems.length);
-
-  // Do not really populate db.
-
-  // As the dbscan is launch in a worker we need to serialize data.
-  var oTranslator = Cotton.Translators.HISTORY_ITEM_TRANSLATORS[0];
-  var lSerializedHistoryItems = [];
-  for(var i = 0, oIDBHistoryItem; oIDBHistoryItem = lHistoryItems[i]; i++) {
-    lSerializedHistoryItems.push(oTranslator.objectToDbRecord(oIDBHistoryItem));
-  }
-  // PARAMETERS
-  // Max Distance between neighborhood
-  var fEps = 10;
-  // Min Points in a cluster
-  var iMinPts = Cotton.Config.Parameters.iMinPts;
-
-  var iNbCluster = Cotton.Algo.DBSCAN(lSerializedHistoryItems, fEps, iMinPts,
-      Cotton.Algo.Distance.CosineHistoryItem);
-  //
-  // This worker has no access to window or DOM. So update DOM should be done
-  // in the main thread.
-
-  var dData = {};
-  dData['iNbCluster'] = iNbCluster;
-  dData['lHistoryItems'] = lSerializedHistoryItems;
-  // because we don't have visittime for this example use simple cluster.
-  var dStories = Cotton.Algo.simpleCluster(dData['lHistoryItems'],
-                                          dData['iNbCluster']);
   DEBUG && console.debug(dStories);
   equal(iNbCluster, 0);
-
-});
-test("algo of dbscan3 worker for hadrien WITH COSINE with very small fEps (10)", function() {
-  DEBUG && console.debug('HADRIEN');
-
-  // Take the chrome history raw.
-  var lSampleHistoryItems = chrome_history_source_foot;
-
-  DEBUG && console.debug("Start with chrome raw history that contains elements :");
-  DEBUG && console.debug(lSampleHistoryItems.length);
-  // Make pretreatment and translate into cotton history item.
-  var lHistoryItems = Cotton.DB.Populate.Suite(lSampleHistoryItems);
-
-  DEBUG && console.debug("After pre treatment history that contains elements :");
-  DEBUG && console.debug(lHistoryItems.length);
-
-  // Do not really populate db.
-
-  // As the dbscan is launch in a worker we need to serialize data.
-  var oTranslator = Cotton.Translators.HISTORY_ITEM_TRANSLATORS[0];
-  var lSerializedHistoryItems = [];
-  for(var i = 0, oIDBHistoryItem; oIDBHistoryItem = lHistoryItems[i]; i++) {
-    lSerializedHistoryItems.push(oTranslator.objectToDbRecord(oIDBHistoryItem));
-  }
-  // PARAMETERS
-  // Max Distance between neighborhood
-  var fEps = 50;
-  // Min Points in a cluster
-  var iMinPts = Cotton.Config.Parameters.iMinPts;
-
-  var iNbCluster = Cotton.Algo.DBSCAN(lSerializedHistoryItems, fEps, iMinPts,
-      Cotton.Algo.Distance.CosineHistoryItem);
-  //
-  // This worker has no access to window or DOM. So update DOM should be done
-  // in the main thread.
-
-  var dData = {};
-  dData['iNbCluster'] = iNbCluster;
-  dData['lHistoryItems'] = lSerializedHistoryItems;
-  // because we don't have visittime for this example use simple cluster.
-  var dStories = Cotton.Algo.simpleCluster(dData['lHistoryItems'],
-                                          dData['iNbCluster']);
-  DEBUG && console.debug(dStories);
-  equal(iNbCluster, 1);
-  equal(dStories[0].length, 30);
-
-});
-
-
-test("algo of dbscan3 worker for small cluster", function() {
-  DEBUG && console.debug('small cluster');
-
-  // Take the chrome history raw.
-  var lSampleHistoryItems = chrome_history_source_hadrien.slice();
-
-  // Make pretreatment and translate into cotton history item.
-  var lHistoryItems = Cotton.DB.Populate.Suite(lSampleHistoryItems);
-
-  // Do not populate db but put id.
-
-  // As the dbscan is launch in a worker we need to serialize data.
-  var oTranslator = Cotton.Translators.HISTORY_ITEM_TRANSLATORS[0];
-  var lSerializedHistoryItems = [];
-  for(var i = 0, oIDBHistoryItem; oIDBHistoryItem = lHistoryItems[i]; i++) {
-    oIDBHistoryItem.initId(lSampleHistoryItems[i]['id']);
-    lSerializedHistoryItems.push(oTranslator.objectToDbRecord(oIDBHistoryItem));
-  }
-  // PARAMETERS
-  // Max Distance between neighborhood
-  var fEps = 18;
-  // Min Points in a cluster
-  var iMinPts = Cotton.Config.Parameters.iMinPts;
-
-  Cotton.Algo.cutInEqualCluster(200, lSerializedHistoryItems, function(lSession){
-    var iNbCluster = Cotton.Algo.DBSCAN(lSession, fEps, iMinPts,
-      Cotton.Algo.Distance.CosineHistoryItem);
-    //
-    // This worker has no access to window or DOM. So update DOM should be done
-    // in the main thread.
-
-    var dData = {};
-    dData['iNbCluster'] = iNbCluster;
-    dData['lHistoryItems'] = lSession;
-    var dStories = Cotton.Algo.clusterStory(dData['lHistoryItems'],
-                                            dData['iNbCluster']);
-    DEBUG && console.debug(dStories);
-    equal(iNbCluster, 0);
-
-  });
-
+  equal(dStories.length, 0);
 });
