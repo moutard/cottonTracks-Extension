dd7a6480361ee08cde6e89d53ee48032a277c3fe
Raphael Korach <raphael.korach@gmail.com>
2013-04-11 17:04:13 -0400

    Algo: update blacklist

--- algo/common/words/black_list.js
+++ algo/common/words/black_list.js
@@ -4,11 +4,11 @@ var blackList = ['htm', 'and', 'the', 'pdf',
 "both", "not", "also", "only", "too", "either", "neither", "rather", "than",
 "more", "less", "whether", "neither", "besides", "because", "since", "rather",
 "though", "unless", "even", "although", "while", "whereas", "despite", "spite",
-"regardless", "where", "wherever", "while", "since", "until", "the", "once",
+"regardless", "where", "wherever", "until", "the", "once",
 "furthermore", "moreover", "additionally", "besides", "firstly", "secondly",
 "finally", "instead", "otherwise", "thus", "hence", "accordingly", "anyhow",
 "nevertheless", "nonetheless", "however", "meanwhile", "subsequently", "afterward",
-"are", "com", "fr", "with", "you"
+"are", "com", "fr", "with", "you", "in"
 ];
 
   return blackList.indexOf(sWord) !== -1;
