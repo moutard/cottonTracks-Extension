3aa03b16460a5cec651e3ef9aa1071a2cb049bf5
Raphael Korach <raphael.korach@gmail.com>
2013-04-19 17:25:56 -0400

    Test: add test for querywords

--- test/js/model/history_item_dna_tests.js
+++ test/js/model/history_item_dna_tests.js
@@ -14,3 +14,23 @@ test("init.", function() {
   ok(oHistoryItemDNA);
 });
 
+test("setQueryWords.", function() {
+  var oHistoryItemDNA = new Cotton.Model.HistoryItemDNA();
+  oHistoryItemDNA.setQueryWords(['the', 'Magical', 'Mystery', 'Tour', 'of', 'the', 'Beatles']);
+  var lQueryWords = ['the', 'Magical', 'Mystery', 'Tour', 'of', 'the', 'Beatles'];
+  deepEqual(oHistoryItemDNA.queryWords(), lQueryWords);
+});
+
+test("setStrongQueryWords.", function(){
+  var oHistoryItemDNA = new Cotton.Model.HistoryItemDNA();
+  oHistoryItemDNA.setStrongQueryWords(['Magical', 'Mystery', 'Tour', 'Beatles']);
+  var dBagOfWords = {"magical": 5, "mystery": 5, "tour": 5, "beatles": 5}
+  deepEqual(oHistoryItemDNA.bagOfWords().get(), dBagOfWords);
+});
+
+test("setWeakQueryWords.", function(){
+  var oHistoryItemDNA = new Cotton.Model.HistoryItemDNA();
+  oHistoryItemDNA.setWeakQueryWords(['the', 'of', 'the']);
+  var dBagOfWords = {"the": 2, "of": 2}
+  deepEqual(oHistoryItemDNA.bagOfWords().get(), dBagOfWords);
+});
