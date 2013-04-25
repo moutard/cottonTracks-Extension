0087035efe34aab5341dc1e8ca5d2c2aa1c7dd77
Raphael Korach <raphael.korach@gmail.com>
2013-04-24 20:12:54 -0400

    UI: put + ADD ELEMENT block at the end of the story

---
+++ ui/story/item/add_item.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * In charge of letting user add elements from the pool in the current story.
+ */
+Cotton.UI.Story.Item.AddItem = Class.extend({
+
+  // parent element.
+  _oStory : null,
+
+  // dispatcher.
+  _oDispatcher : null,
+
+  _$add_item : null,
+
+  init : function(oDispatcher, oStoryElement) {
+    var self = this;
+    this._oDispatcher = oDispatcher;
+    this._oStoryElement = oStoryElement;
+
+    this._$add_item = $('<div class="ct-story_item ct-add_item"></div>');
+    this._$add_text = $('<p>+ ADD A NEW ELEMENT</p>');
+
+    this._$add_item.append(this._$add_text);
+  },
+
+  $ : function() {
+    return this._$add_item;
+  }
+});
