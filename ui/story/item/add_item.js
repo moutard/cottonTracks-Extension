63cd39ffda61f55f8b72fb648766df4b415c2511
Raphael Korach <raphael.korach@gmail.com>
2013-04-24 22:05:41 -0400

    UI: navigation bar on add_element item

--- ui/story/item/add_item.js
+++ ui/story/item/add_item.js
@@ -19,14 +19,34 @@ Cotton.UI.Story.Item.AddItem = Class.extend({
     this._oStoryElement = oStoryElement;
 
     this._$add_item = $('<div class="ct-story_item ct-add_item"></div>');
-    this._$add_text = $('<p>+ ADD A NEW ELEMENT</p>').click(function(){
+    this._$default_add_text = $('<p>+ ADD A NEW ELEMENT</p>').click(function(){
       self._oDispatcher.publish('show_elements', {});
     });
 
-    this._$add_item.append(this._$add_text);
+    this._$add_item.append(this._$default_add_text);
   },
 
   $ : function() {
     return this._$add_item;
+  },
+
+  showItems : function(lItemsFromPool) {
+    this._$default_add_text.addClass('hidden');
+    if (lItemsFromPool && lItemsFromPool.length > 3){
+      this.appendNavigationBar();
+    }
+  },
+
+  appendNavigationBar : function(){
+    this._$navigation_bar = $('<div class="navigation_bar"></div>');
+    this._$next_items_arrow =
+      $('<img class="arrow next_items" src="/media/images/story/item/add_item/arrow.png"/>');
+    this._$previous_items_arrow =
+      $('<img class="arrow previous_items" src="/media/images/story/item/add_item/arrow.png"/>').addClass('hidden');
+    this._$add_item.append(
+      this._$navigation_bar.append(
+      )
+    );
   }
+
 });
