5b41164a1c23cf5288b898d47822dadb0caa98b4
Raphael Korach <raphael.korach@gmail.com>
2013-04-29 17:17:42 -0400

    Test: update menu tests

--- test/js/ui/side_menu/preview/sticker/sticker_tests.js
+++ test/js/ui/side_menu/preview/sticker/sticker_tests.js
@@ -5,10 +5,9 @@ module('Cotton.UI.SideMenu.Preview.Sticker.Element', {
 });
 
 test('init.', function() {
-  var sTitle = "Alice in wonderland"
-  var sStoryImage = "http://upload.wikimedia.org/wikipedia/commons/6/6d/Alice_in_wonderland_1951.jpg";
   var oDispacher = new MockDispacher();
+  var oStory = new Cotton.Model.Story();
   var oStickerElement = new Cotton.UI.SideMenu.Preview.Sticker.Element(
-    sTitle, sStoryImage, oDispacher);
+    oStory, oDispacher);
   ok(oStickerElement);
 });
