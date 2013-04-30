bf94013ad7b88e5f9daba78480ebabbc116261d2
Raphael Korach <raphael.korach@gmail.com>
2013-04-30 18:56:20 -0400

    UI: set play button on video items thumbnails

--- ui/story/item/content/brick/dna/video.js
+++ ui/story/item/content/brick/dna/video.js
@@ -21,13 +21,15 @@ Cotton.UI.Story.Item.Content.Brick.Dna.Video = Class.extend({
     // Uses the right embed code depending on the video provider
     if (this._sVideoType == "youtube") {
       this._$thumbnail = $('<img class="resize thumbnail" src="http://img.youtube.com/vi/'
-        + sEmbedCode + '/hqdefault.jpg" />').click(function(){
-          var sEmbedUrl = "http://www.youtube.com/embed/" + sEmbedCode + "?autoplay=1";
-          self._$video = $('<iframe width="360" height="260" src="' + sEmbedUrl + '" frameborder="0" allowfullscreen></iframe>');
-          self._$thumbnail.removeClass('show').addClass('hidden');
-          self._$video_container.append(self._$video)
+        + sEmbedCode + '/hqdefault.jpg" />');
+      this._$play = $('<div class="video_play"><div>').click(function(){
+        var sEmbedUrl = "http://www.youtube.com/embed/" + sEmbedCode + "?autoplay=1";
+        self._$video = $('<iframe width="360" height="260" src="' + sEmbedUrl + '" frameborder="0" allowfullscreen></iframe>');
+        self._$thumbnail.removeClass('show').addClass('hidden');
+        self._$play.addClass('hidden');
+        self._$video_container.append(self._$video)
       });
-      this._$video_container.append(this._$thumbnail);
+      this._$video_container.append(this._$thumbnail, this._$play);
       this.resize(this._$thumbnail);
 
     } else if (this._sVideoType == "vimeo") {
@@ -36,30 +38,34 @@ Cotton.UI.Story.Item.Content.Brick.Dna.Video = Class.extend({
       $.ajax({
         url: 'http://vimeo.com/api/v2/video/' + sEmbedCode + '.json',
       }).done(function ( data ) {
-        console.log(data);
           thumbnail_src = data[0]['thumbnail_large'];
-          console.log(thumbnail_src);
-          self._$thumbnail = $('<img class="resize thumbnail" src="' + thumbnail_src + '"/>').click(function(){
+          self._$thumbnail = $('<img class="resize thumbnail" src="' + thumbnail_src + '"/>')
+          self._$play = $('<div class="video_play"><div>').click(function(){
             var sEmbedUrl = "http://player.vimeo.com/video/" + sEmbedCode
               + "?title=1&byline=0&portrait=0&autoplay=1";
             self._$video = $('<iframe width="360" height="260" src="'+ sEmbedUrl +'" frameborder="0" webkitAllowFullscreen></iframe>');
             self._$video.attr('src', sEmbedUrl);
             self._$thumbnail.removeClass('show').addClass('hidden');
+            self._$play.addClass('hidden');
             self._$video_container.append(self._$video)
           });
+          self._$video_container.append(self._$thumbnail, self._$play);
+          self.resize(self._$thumbnail);
       });
 
     } else if (this._sVideoType == "dailymotion") {
       this._$thumbnail = $('<img class="resize thumbnail" src="http://www.dailymotion.com/thumbnail/video/'
-        + sEmbedCode + '"/>').click(function(){
-          var sEmbedUrl = "http://www.dailymotion.com/embed/video/"
-              + sEmbedCode
-              + "?background=3E3E3E&foreground=EEEEEE&highlight=5bab7d&autoplay=1";
-          self._$video = $('<iframe width="360" height="260" src="'+ sEmbedUrl +'" frameborder="0"></iframe>');
-          self._$thumbnail.removeClass('show').addClass('hidden');
-          self._$video_container.append(self._$video)
+        + sEmbedCode + '"/>');
+      self._$play = $('<div class="video_play"><div>').click(function(){
+        var sEmbedUrl = "http://www.dailymotion.com/embed/video/"
+            + sEmbedCode
+            + "?background=3E3E3E&foreground=EEEEEE&highlight=5bab7d&autoplay=1";
+        self._$video = $('<iframe width="360" height="260" src="'+ sEmbedUrl +'" frameborder="0"></iframe>');
+        self._$thumbnail.removeClass('show').addClass('hidden');
+        self._$play.addClass('hidden');
+        self._$video_container.append(self._$video)
       });
-      self._$video_container.append(self._$thumbnail);
+      self._$video_container.append(self._$thumbnail, self._$play);
       self.resize(self._$thumbnail);
     }
 
@@ -76,7 +82,6 @@ Cotton.UI.Story.Item.Content.Brick.Dna.Video = Class.extend({
    */
   resize : function($img) {
     $img.load(function(){
-
       //image size and ratio
       var iImWidth = $img.width();
       var iImHeight = $img.height();
