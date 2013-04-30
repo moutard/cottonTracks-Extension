47a83cd9244d2c6b4c64144358b850c425999e51
Raphael Moutard <raphael.moutard@gmail.com>
2013-04-30 16:32:25 -0400

    fixup! Utils: add dom utils file

--- utils/dom.js
+++ utils/dom.js
@@ -1,13 +1,23 @@
 (function(){
   'use strict';
 
-  Cotton.Utils.ancestor = function (sSelector, oNode) {
+  Cotton.Utils.ancestor = function (sSelector, oNode, bIncludeChildren) {
     var oParent = oNode.parentNode;
     var oMatches = document.querySelectorAll(sSelector);
     var lMatches = Array.prototype.slice.call(oMatches);
     while(oParent !== document) {
       if (lMatches.indexOf(oParent) > -1) {
         return oParent;
+      }
+      if (bIncludeChildren) {
+        var oChildren = oParent.children;
+        var lChildren = Array.prototype.slice.call(oChildren);
+        for (var i = 0, len = lChildren.length; i < len; i++) {
+          var oChild = lChildren[i];
+          if (lMatches.indexOf(oChild) > -1) {
+            return oChild;
+          }
+        }
       }
       oParent = oParent.parentNode;
     }
