b7fc946f43ae2e6f10087e7afddc51826192497f
Raphael Korach <raphael.korach@gmail.com>
2013-05-01 19:30:34 -0400

    UI: include google search images results as images

--- ui/story/item/item_factory.js
+++ ui/story/item/item_factory.js
@@ -29,6 +29,10 @@ Cotton.UI.Story.Item.Factory = function(oHistoryItem, oDispatcher) {
     // Image, from google search image result.
     var sImageUrl = oUrl.replaceHexa(oUrl.dSearch['imgurl']);
     return new Cotton.UI.Story.Item.Image(sImageUrl, oHistoryItem, oDispatcher);
+  } else if (oUrl.searchImage) {
+      // Image, from google search image result.
+      var sImageUrl = oUrl.searchImage;
+      return new Cotton.UI.Story.Item.Image(sImageUrl, oHistoryItem, oDispatcher);
   } else if (oUrl.hostname === "www.youtube.com" && oUrl.dSearch['v']) {
     // Video - Youtube
     return new Cotton.UI.Story.Item.Video(oUrl.dSearch['v'], "youtube",
