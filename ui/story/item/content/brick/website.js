cf81440b9a1b01cb492bcd150cef761329635fac
Raphael Korach <raphael.korach@gmail.com>
2013-05-23 01:37:30 -0700

    UI: fix favicon on Opera

--- ui/story/item/content/brick/website.js
+++ ui/story/item/content/brick/website.js
@@ -37,7 +37,7 @@ Cotton.UI.Story.Item.Content.Brick.Website = Class.extend({
     if (sDomain.match(oRegExp)){
       this._$favicon = $('<img class="favicon" src="http://www.google.com/images/google_favicon_128.png">');
     } else {
-      this._$favicon = $('<img class="favicon" src="chrome://favicon/'+sUrl+'">');
+      this._$favicon = $('<img class="favicon" src="opera://favicon/'+sUrl+'">');
     }
 
     // construct item.
