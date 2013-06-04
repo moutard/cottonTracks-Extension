60e270e59d1da55dc8aa533cf630e0054dcda124
rmoutard <raphael.moutard@gmail.com>
2013-06-04 21:52:39 +0200

    Refactor: change comment of worker.

--- algo/dbscan3/worker_dbscan3.js
+++ algo/dbscan3/worker_dbscan3.js
@@ -22,13 +22,13 @@ importScripts('../../algo/dbscan1/score/dbrecord_score.js');
 importScripts('../../algo/dbscan1/dbscan.js');
 importScripts('../../algo/dbscan3/detect_sessions.js');
 
+/**
+ * This method has 3 steps : - Separate Roughly the historyItems in sessions. -
+ * Then compute for each rough sessions dbscan1 with distance only on the
+ * time, to refine session. - For each session, compute dbscan1 with the
+ * meaning distance.
+ */
 function handleHistoryItems3(lHistoryItems, lVisitItems) {
-  /**
-   * This method has 3 steps : - Separate Roughly the historyItems in sessions. -
-   * Then compute for each rough sessions dbscan1 with distance only on the
-   * time, to refine session. - For each session, compute dbscan1 with the
-   * meaning distance.
-   */
 
   // Max Distance between neighborhood.
   var fEps = Cotton.Config.Parameters.dbscan3.fEps;
@@ -62,11 +62,11 @@ function handleHistoryItems3(lHistoryItems, lVisitItems) {
   });
 }
 
+/**
+ * Connect worker with main thread. Worker starts when it receive
+ * postMessage(). Data received are serialized. i.e. it's non
+ * Cotton.Model.HistoryItem, but object.
+ */
 self.addEventListener('message', function(e) {
-  /**
-   * Connect worker with main thread. Worker starts when it receive
-   * postMessage(). Data received are serialized. i.e. it's non
-   * Cotton.Model.HistoryItem, but object.
-   */
   handleHistoryItems3(e.data['historyItems'], e.data['visitItems']);
 }, false);
