f2b3a50215e493f34155b6b05cae30a00c8fd38f
Raphael Moutard <raphael.moutard@gmail.com>
2013-06-05 15:56:11 -0700

    Core: installer for Opera

---
+++ core/opera/installer.js
@@ -0,0 +1,52 @@
+'use strict';
+
+Cotton.Core.Installer = Class.extend({
+
+  _oDatabase : null,
+
+  /**
+   * call back method called when the installation is finished.
+   */
+  _mIsFinished : null,
+
+  init: function(oDatabase, mCallback){
+    var self = this;
+    self._oDatabase = oDatabase;
+    self._mIsFinished = mCallback;
+
+    // When everything is ready call the install.
+    self.install();
+  },
+
+  /**
+   * Install
+   *
+   * First installation, the database is empty. Need to populate. Then launch,
+   * DBSCAN1 on the results.
+   *
+   */
+  install : function(mCallback){
+    var self = this;
+
+    // Disable the button and open the howTo page.
+    chrome.tabs.create({'url': 'http://www.cottontracks.com/howto.html'});
+    DEBUG && console.debug("Controller - install");
+    // Set cohort for analytics.
+    var date = new Date();
+    var month = date.getMonth() + 1;
+    // TODO(rmoutard): use a ct-cohort to avoid conflit on local storage.
+    localStorage.setItem('cohort', month + "/" + date.getFullYear());
+    Cotton.ANALYTICS.setCohort(month + "/" + date.getFullYear());
+    self._mIsFinished();
+  },
+
+  /**
+   * Update
+   *
+   * If something is needed. But nothing for the moment.
+   */
+  update : function(){
+    DEBUG && console.debug("update");
+  },
+
+});
