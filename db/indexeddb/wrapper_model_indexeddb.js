c87a8e1deef28d38eab568e10a691138810a03d5
rmoutard <raphael.moutard@gmail.com>
2013-06-07 22:54:09 +0200

    DB: wrapper with new model

--- db/indexeddb/wrapper_model_indexeddb.js
+++ db/indexeddb/wrapper_model_indexeddb.js
@@ -6,7 +6,7 @@ Cotton.DB.IndexedDB.WrapperModel = Class.extend({
 
   init : function(sDatabaseName, dModels, mOnReadyCallback) {
     var self = this;
-    this._sDatabaseName = sDatabaseName;
+    self._sDatabaseName = sDatabaseName;
     self._dModels = dModels;
 
     var dIndexesForObjectStoreNames = {};
@@ -15,68 +15,386 @@ Cotton.DB.IndexedDB.WrapperModel = Class.extend({
       console.log(dIndexesForObjectStoreNames);
     });
 
-    var oEngine = new Cotton.DB.IndexedDB.Engine(
-        sDatabaseName,
+    this._oEngine = new Cotton.DB.IndexedDB.Engine(sDatabaseName,
         dIndexesForObjectStoreNames,
         function() {
           mOnReadyCallback.call(self);
     });
 
-    this._oEngine = oEngine;
-
   },
 
-  empty : function(sObjectStoreName, mResultElementCallback){
-     var self = this;
+  /**
+   * Given a dDBRecord return an javascript object that corresponds to the
+   * right class.
+   * @param {String} sObjectStoreName: name of the store.
+   * @param {Dictionnary} dDbRecord: record stored in the database and
+   * return by the engine.
+   * @return {Cotton.Model}
+   */
+  _dbRecordToObject: function(sObjectStoreName, dDbRecord) {
+    return new this._dModels[sObjectStoreName](dDbRecord);
+  },
 
-    this._oEngine.empty(sObjectStoreName, function(bIsEmpty){
-       mResultElementCallback.call(self, bIsEmpty);
+  /**
+   * Given an object return a dbRecord.
+   * @param {String} sObjectStoreName:
+   * @param {Cotton.Model} : oObject
+   */
+  _objectToDBRecord: function(sObjectStoreName, oObject) {
+    return oObject._dbRecord;
+  },
+
+  iterList: function(sObjectStoreName, mResultElementCallback) {
+    var self = this;
+    this._oEngine.iterList(sObjectStoreName, function(oDBRecord) {
+      var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+      mResultElementCallback.call(self, oObject);
     });
   },
-  _translatorForDbRecord: function(sObjectStoreName, dDbRecord) {
-    return this._translator(sObjectStoreName, dDbRecord['sFormatVersion']);
+
+
+  listInverse: function(sObjectStoreName, mResultElementCallback) {
+    var self = this;
+    this._oEngine.listInverse(sObjectStoreName, function(oDBRecord) {
+      var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+      mResultElementCallback.call(self, oObject);
+    });
   },
 
-  _translatorForObject: function(sObjectStoreName, oObject) {
-    return this._lastTranslator(sObjectStoreName);
-  },
+  getList: function(sObjectStoreName, mResultElementCallback){
+    var self = this;
 
-  /**
-   * Returns the translator matching the given type and format version. Throws
-   * an exception if there is no such translator.
-   *
-   * @param {String} sObjectStoreName:
-   *  name of the store (table in the database).
-   * @param {String} sFormatVersion:
-   *  version of the model.
-   */
-  _translator: function(sObjectStoreName, sFormatVersion) {
-    var lTranslators = this._dTranslators[sObjectStoreName];
-    // TODO(fwouts): Store the translators as a hash using versions as keys?
-    for (var iI = 0, oTranslator; oTranslator = lTranslators[iI]; iI++) {
-      if (oTranslator.formatVersion() == sFormatVersion) {
-        break;
+    this._oEngine.getList(sObjectStoreName, function(oResult) {
+      var lList = new Array();
+      for(var i = 0, oDbRecord; oDbRecord = oResult[i]; i++){
+        var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+        lList.push(oObject);
       }
-    }
-    if (!oTranslator) {
-      throw "No translator matching record version (" + sFormatVersion + " for type " + sObjectStoreName + ")."
-    }
-    return oTranslator;
+      mResultElementCallback.call(self, lList);
+    });
   },
 
-  /**
-   * Returns the last translator for the given type. Throws an exception if the
-   * type does not have any translators.
-   *
-   * @param {String} sObjectStoreName:
-   *  name of the store (table in the database).
-   */
-  _lastTranslator: function(sObjectStoreName) {
-    var lTranslators = this._dTranslators[sObjectStoreName];
-    if (!lTranslators) {
-      throw "Unknown type."
+  iterRange: function(sObjectStoreName, iLowerBound, iUpperBound,
+                        mResultElementCallback) {
+    var self = this;
+    this._oEngine.iterRange(sObjectStoreName,
+      iLowerBound, iUpperBound, function(oDBRecord) {
+      var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+      mResultElementCallback.call(self, oObject);
+    });
+  },
+
+  getRange: function(sObjectStoreName, iLowerBound, iUpperBound,
+                      mResultElementCallback) {
+    var self = this;
+
+    var lAllObjects = new Array();
+    this._oEngine.getRange(sObjectStoreName,
+      iLowerBound, iUpperBound,
+      function(oResult) {
+        if (!oResult) {
+          // If there was no result, send back null.
+          mResultElementCallback.call(self, lAllObjects);
+          return;
+        }
+        // else oResult is a list of Items.
+        for(var i = 0, oDBRecord; oDBRecord = oResult[i]; i++ ){
+          var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+          lAllObjects.push(oObject);
+        }
+
+        mResultElementCallback.call(self, lAllObjects);
+    });
+  },
+
+
+  getUpperBound: function(sObjectStoreName, sIndexKey, iUpperBound,
+                            iDirection, bStrict,
+                            mResultElementCallback) {
+    var self = this;
+
+    var lAllObjects = new Array();
+    this._oEngine.getUpperBound(
+      sObjectStoreName, sIndexKey, iUpperBound, iDirection, bStrict,
+      function(oResult) {
+        if (!oResult) {
+          // If there was no result, send back null.
+          mResultElementCallback.call(self, lAllObjects);
+          return;
+        }
+        // else oResult is a list of Items.
+        for(var i = 0, oDBRecord; oDBRecord = oResult[i]; i++ ){
+          var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+          lAllObjects.push(oObject);
+        }
+
+        mResultElementCallback.call(self, lAllObjects);
+    });
+  },
+
+  getLowerBound: function(sObjectStoreName, sIndexKey, iLowerBound,
+                            iDirection, bStrict,
+                            mResultElementCallback) {
+    var self = this;
+
+    var lAllObjects = new Array();
+    this._oEngine.getLowerBound(
+      sObjectStoreName, sIndexKey, iLowerBound, iDirection, bStrict,
+      function(oResult) {
+        if (!oResult) {
+          // If there was no result, send back null.
+          mResultElementCallback.call(self, lAllObjects);
+          return;
+        }
+        // else oResult is a list of Items.
+        for(var i = 0, oDBRecord; oDBRecord = oResult[i]; i++ ){
+          var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+          lAllObjects.push(oObject);
+        }
+
+        mResultElementCallback.call(self, lAllObjects);
+    });
+  },
+
+  getBound: function(sObjectStoreName, sIndexKey,
+                      iLowerBound, lUpperBound, iDirection,
+                      bStrictLower, bStrictUpper,
+                      mResultElementCallback) {
+    var self = this;
+
+    var lAllObjects = new Array();
+    this._oEngine.getBound(
+      sObjectStoreName, sIndexKey, iLowerBound, iUpperBound, iDirection,
+      bStrictLower, bStrictUpper,
+      function(oResult) {
+        if (!oResult) {
+          // If there was no result, send back null.
+          mResultElementCallback.call(self, lAllObjects);
+          return;
+        }
+        // else oResult is a list of Items.
+        for(var i = 0, oDBRecord; oDBRecord = oResult[i]; i++ ){
+          var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+          lAllObjects.push(oObject);
+        }
+
+        mResultElementCallback.call(self, lAllObjects);
+    });
+  },
+
+  getLastEntry: function(sObjectStoreName, mResultElementCallback) {
+    var self = this;
+
+    this._oEngine.getLastEntry(sObjectStoreName, function(oDBRecord) {
+      var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+      mResultElementCallback.call(self, oObject);
+    });
+  },
+
+  getLast: function(sObjectStoreName, sIndexKey, mResultElementCallback) {
+    var self = this;
+
+    this._oEngine.getLast(sObjectStoreName, sIndexKey, function(oDBRecord) {
+      var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+      mResultElementCallback.call(self, oObject);
+    });
+  },
+
+  getXItems: function(sObjectStoreName, iX, sIndexKey, iDirection,
+      mResultElementCallback) {
+    var self = this;
+
+    var lAllObjects = new Array();
+    this._oEngine.getXItems(
+        sObjectStoreName, iX, sIndexKey, iDirection,
+        function(oResult) {
+          if (!oResult) {
+            // If there was no result, send back null.
+            mResultElementCallback.call(self, lAllObjects);
+            return;
+          }
+          // else oResult is a list of Items.
+          for(var i = 0, oDBRecord; oDBRecord = oResult[i]; i++ ){
+            var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+            lAllObjects.push(oObject);
+          }
+
+          mResultElementCallback.call(self, lAllObjects);
+        });
+  },
+
+  getXYItems: function(sObjectStoreName, iX, iY, sIndexKey, iDirection,
+      mResultElementCallback) {
+    var self = this;
+
+    var lAllObjects = new Array();
+    this._oEngine.getXYItems(
+        sObjectStoreName, iX, iY, sIndexKey, iDirection,
+        function(oResult) {
+          if (!oResult) {
+            // If there was no result, send back null.
+            mResultElementCallback.call(self, lAllObjects);
+            return;
+          }
+          // else oResult is a list of Items.
+          for(var i = 0, oDBRecord; oDBRecord = oResult[i]; i++ ){
+            var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+            lAllObjects.push(oObject);
+          }
+
+          mResultElementCallback.call(self, lAllObjects);
+        });
+  },
+
+  find: function(sObjectStoreName, sIndexKey, oIndexValue,
+                  mResultElementCallback) {
+    var self = this;
+
+    this._oEngine.find(sObjectStoreName, sIndexKey, oIndexValue,
+      function(oDBRecord) {
+        if (!oDBRecord) {
+          // If there was no result, send back null.
+          mResultElementCallback.call(self, null);
+          return;
+        }
+
+        var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+        mResultElementCallback.call(self, oObject);
+    });
+  },
+
+  findGroup: function(sObjectStoreName, sIndexKey, lIndexValue,
+                        mResultElementCallback) {
+    var self = this;
+    var lAllObjects = new Array();
+
+    this._oEngine.findGroup(sObjectStoreName, sIndexKey, lIndexValue,
+      function(oResult) {
+        if (!oResult) {
+          // If there was no result, send back null.
+          mResultElementCallback.call(self, lAllObjects);
+          return;
+        }
+        // else oResult is a list of Items.
+        for(var i = 0, oDBRecord; oDBRecord = oResult[i]; i++ ){
+          var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+          lAllObjects.push(oObject);
+      }
+
+      mResultElementCallback.call(self, lAllObjects);
+    });
+  },
+
+  search: function(sObjectStoreName, sIndexKey, oIndexValue,
+                  mResultElementCallback) {
+    var self = this;
+    var lObjects = [];
+
+    this._oEngine.search(sObjectStoreName, sIndexKey, oIndexValue,
+      function(lResults) {
+        for (var i = 0, iLength = lResults.length; i < iLength; i++) {
+          var oDBRecord = lResults[i];
+          var oObject = self._dbRecordToObject(sObjectStoreName, oDBRecord);
+          lObjects.push(oObject);
+        }
+
+        mResultElementCallback.call(self, lObjects);
+    });
+  },
+
+  put: function(sObjectStoreName, oObject, mOnSaveCallback) {
+    var self = this;
+
+    var dDbRecord = self._objectToDBRecord(sObjectStoreName, oObject);
+    this._oEngine.put(sObjectStoreName, dDbRecord, function(iId) {
+
+      if (mOnSaveCallback) {
+        mOnSaveCallback.call(self, iId, oObject);
+      }
+    });
+  },
+
+  putList: function(sObjectStoreName, lObjects, mOnSaveCallback) {
+    var self = this;
+
+    var lAllItems = new Array();
+    for(var i = 0, oObject; oObject = lObjects[i]; i++ ){
+      var dDbRecord = self._objectToDBRecord(sObjectStoreName, oObject);
+      lAllItems.push(dDbRecord);
     }
-    var oTranslator = lTranslators[lTranslators.length - 1];
-    return oTranslator;
+
+    this._oEngine.AputList(sObjectStoreName, lAllItems, function(lAllId) {
+      if (mOnSaveCallback) {
+        mOnSaveCallback.call(self, lAllId);
+      }
+    });
+  },
+
+  putUniqueKeyword: function(sObjectStoreName, oObject, mOnSaveCallback) {
+    var self = this;
+
+    var dDbRecord = self._objectToDBRecord(sObjectStoreName, oObject);
+    this._oEngine.putUniqueKeyword(sObjectStoreName, dDbRecord, function(iId) {
+      if (mOnSaveCallback) {
+        mOnSaveCallback.call(self, iId);
+      }
+    });
+  },
+
+  putUniqueHistoryItem: function(sObjectStoreName, oObject, mOnSaveCallback) {
+    var self = this;
+
+    var dDbRecord = self._objectToDBRecord(sObjectStoreName, oObject);
+    this._oEngine.putUniqueHistoryItem(sObjectStoreName, dDbRecord, function(iId) {
+      if (mOnSaveCallback) {
+        mOnSaveCallback.call(self, iId);
+      }
+    });
+  },
+
+  putListUniqueHistoryItems: function(sObjectStoreName, lObjects, mOnSaveCallback) {
+    var self = this;
+
+    var lAllItems = new Array();
+    for(var i = 0, oObject; oObject = lObjects[i]; i++ ){
+      var dDbRecord = self._objectToDBRecord(sObjectStoreName, oObject);
+      lAllItems.push(dDbRecord);
+    }
+
+    this._oEngine.putListUniqueHistoryItems(sObjectStoreName, lAllItems, function(lAllId) {
+      if (mOnSaveCallback) {
+        mOnSaveCallback.call(self, lAllId);
+      }
+    });
+  },
+
+  update: function(sObjectStoreName, sId, oObject, mOnSaveCallback) {
+    var self = this;
+
+    var dDbRecord = oTranslator.objectToDbRecord(oObject);
+    this._oEngine.update(sObjectStoreName, sId, dDbRecord, function(iId) {
+      if (mOnSaveCallback) {
+        mOnSaveCallback.call(self, iId);
+      }
+    });
+  },
+
+  delete: function(sObjectStoreName, iId, mOnDeleteCallback) {
+    var self = this;
+
+    this._oEngine.delete(sObjectStoreName, iId, function() {
+      if (mOnDeleteCallback) {
+        mOnDeleteCallback.call(self);
+      }
+    });
+  },
+
+  purge: function(sObjectStoreName, mResultElementCallback) {
+    var self = this;
+
+    this._oEngine.purge(sObjectStoreName, function() {
+      mResultElementCallback.call(self);
+    });
   }
+
 });
