045d798c4976a3342418e5da7d2c472fffab7e11
Raphael Korach <raphael.korach@gmail.com>
2013-06-08 21:10:35 -0700

    UI: dark backround if no background screenshot

--- ui/world.js
+++ ui/world.js
@@ -52,25 +52,28 @@ Cotton.UI.World = Class.extend({
       'action': 'pass_background_screenshot'
     }, function(response) {
       //set background image and blur it
-      // Use a temporary div that will be filled with the bacground.
-      self._$temporary_background.css(
-        'background-image',
-        "url(" + response['src'] + ")"
-      );
-      $('body').blurjs({
-        'source': '#blur_target',
-        'radius': 15,
-        'overlay': 'rgba(0,0,0,0.2)'
-      });
-      setTimeout(function(){
-        self._$temporary_background.remove();
-      }, 1000);
+      // Use a temporary div that will be filled with the background.
+      if (response['src'] && response['src'] !== undefined){
+        self._$temporary_background.css(
+          'background-image',
+          "url(" + response['src'] + ")"
+        );
+        $('body').blurjs({
+          'source': '#blur_target',
+          'radius': 15,
+          'overlay': 'rgba(0,0,0,0.2)'
+        });
+        setTimeout(function(){
+          self._$temporary_background.remove();
+        }, 1000);
+        // progressive blur effect
+        setTimeout(function(){
+          self._$temporary_background.addClass('hidden_background');
+        }, 200);
+      } else {
+        $('body').addClass('plain');
+      }
     });
-
-    // progressive blur effect
-    setTimeout(function(){
-      self._$temporary_background.addClass('hidden_background');
-    }, 200);
   },
 
   $ : function () {
