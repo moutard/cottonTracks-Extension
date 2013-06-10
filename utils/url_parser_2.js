74a16822cd6c12d76ec41d9c1b47278de665f9ae
Raphael MOUTARD <rmoutard@ltutech.com>
2013-06-10 13:53:43 +0200

    Test: integration

---
+++ utils/url_parser_2.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * In charge of parsing urls to extract main informations.
+ */
+Cotton.Utils.UrlParser = Class.extend({
+
+
+  /**
+   * @param {String} url.
+   */
+  init: function(sUrl){
+
+  }
+
+});
