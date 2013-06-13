169f0c79f208031549fa22fb642642b65bfd3308
Raphael MOUTARD <rmoutard@ltutech.com>
2013-06-13 13:54:54 +0200

    Refactor: remove useless error.js

--- utils/error.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function CtError() {
-    Error.apply(this, arguments);
-}
-
-CtError.prototype = new Error();
-CtError.prototype.constructor = CtError;
-CtError.prototype.name = 'CtError';
