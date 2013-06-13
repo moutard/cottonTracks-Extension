16afe48c9e157d8e6933a695282fc10b8ade8298
Raphael MOUTARD <rmoutard@ltutech.com>
2013-06-13 15:25:27 +0200

    Refactor: remove function Cotton.Algo.roughlySeparateSession that is not used anymore because we use now Cotton.Algo.roughlySeparateSessionForVisitItems

--- algo/dbscan3/detect_sessions.js
+++ algo/dbscan3/detect_sessions.js
@@ -1,41 +1,13 @@
 'use strict';
 
 /**
- * separate roughly the historyItems when the time difference between two
+ * Separate roughly the visitItems when the time difference between two
  * consecutive items is more than 5 hours.
+ * And copy the corresponding historyItems so the algo can work.
  *
  * @param {Array}
  *          lHistoryItems
  */
-
-Cotton.Algo.roughlySeparateSession = function(lHistoryItems, mCallBack) {
-  // 5 hours
-  var threshold = 5 * 60 * 60 * 1000;
-
-  var lNewRoughSession = [];
-  var iPreviousTime;
-
-  for ( var i = 0, oCurrentHistoryItem; oCurrentHistoryItem = lHistoryItems[i]; i++) {
-    if (i === 0) {
-      iPreviousTime = oCurrentHistoryItem['iLastVisitTime'];
-    }
-    // var a = new Date(iPreviousTime).toLocaleString();
-    // var b = new Date(oCurrentHistoryItem['iLastVisitTime']).toLocaleString();
-
-    if (Math.abs(oCurrentHistoryItem['iLastVisitTime'] - iPreviousTime) <= threshold) {
-      lNewRoughSession.push(oCurrentHistoryItem);
-    } else {
-      mCallBack(lNewRoughSession);
-      lNewRoughSession = [];
-      lNewRoughSession.push(oCurrentHistoryItem);
-    }
-    iPreviousTime = oCurrentHistoryItem['iLastVisitTime'];
-
-  }
-
-  mCallBack(lNewRoughSession);
-};
-
 Cotton.Algo.roughlySeparateSessionForVisitItems = function(lHistoryItems, lChromeVisitItems,
     mCallBack) {
   // 5 hours
--- test/js/algo/dbscan3/dbscan3_tests.js
+++ test/js/algo/dbscan3/dbscan3_tests.js
@@ -50,66 +50,6 @@ Cotton.Algo.simpleCluster = function(lHistoryItems, iNbCluster){
   });
 
 };
-test('detect session for visititems.', function() {
-  var lSampleVisitItems = chrome_visit_source_green.slice();
-  lSampleVisitItems.sort(function(a,b){
-    return b['lastVisitTime'] - a['lastVisitTime'];
-  });
-  DEBUG && console.debug(lSampleVisitItems);
-  var iSessionId = 0;
-  Cotton.Algo.roughlySeparateSession(lSampleVisitItems,
-    function(lNewRoughSession){
-      if(lNewRoughSession.length < 10) {
-        DEBUG && console.debug("A session with length < 10:");
-        DEBUG && console.debug(lNewRoughSession);
-        iSessionId += 1;
-      }
-      ok((iSessionId < 40), iSessionId + ' ' + lNewRoughSession.length);
-  });
-});
-
-test("algo of dbscan3 worker.", function() {
-  var lSampleHistoryItems = cotton_history_source_hadrien;
-
-  // we need oHistoryItems objects to compute bagOfWords
-  var lTranslators = Cotton.Translators.HISTORY_ITEM_TRANSLATORS;
-  var oTranslator = lTranslators[lTranslators.length - 1];
-
-  var lHistoryItems = [];
-  for (var i = 0, dHistoryItem; dHistoryItem = lSampleHistoryItems[i]; i++){
-    lHistoryItems.push(oTranslator._mDbRecordToObjectConverter(dHistoryItem));
-  }
-  lHistoryItems = Cotton.Core.Populate.computeBagOfWordsForHistoryItemsList(lHistoryItems);
-  lHistoryItems = Cotton.Core.Populate.removeHistoryItemsWithoutBagOfWords(lHistoryItems);
-  // then back to dictionaries for the algo, just like in the worker
-  for (var i = 0, oHistoryItem; oHistoryItem = lHistoryItems[i]; i++){
-    lHistoryItems[i] = oTranslator._mObjectToDbRecordConverter(oHistoryItem);
-  }
-  // PARAMETERS
-  // Max Distance between neighborhood
-  var fEps = Cotton.Config.Parameters.dbscan3.fEps;
-  // Min Points in a cluster
-  var iMinPts = Cotton.Config.Parameters.dbscan3.iMinPts;
-
-  Cotton.Algo.roughlySeparateSession(lHistoryItems, function(lSession) {
-    // For each rough session, launch dbscan.
-    DEBUG && console.debug("New session : " + lSession.length);
-    DEBUG && console.debug(lSession);
-    // TODO(rmoutard) : Maybe create a worker, by session. or use a queue.
-    var iNbCluster = Cotton.Algo.DBSCAN(lSession, fEps, iMinPts,
-        Cotton.Algo.Score.DBRecord.HistoryItem);
-    /**
-     * This worker has no access to window or DOM. So update DOM should be done
-     * in the main thread.
-     */
-    var dData = {};
-    dData['iNbCluster'] = iNbCluster;
-    dData['lHistoryItems'] = lSession;
-
-    ok(true, "dbscan3 runs");
-  });
-
-});
 
 test("algo of dbscan3 worker for hadrien with very low fEps (0)", function() {
 
