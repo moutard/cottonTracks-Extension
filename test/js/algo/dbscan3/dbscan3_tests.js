a6b7a0c2231d3d4ec0b43f3e330c1dd8a00204fb
Raphael MOUTARD <rmoutard@ltutech.com>
2013-06-13 15:25:27 +0200

    Refactor: remove function Cotton.Algo.roughlySeparateSession that is not used anymore because we use now Cotton.Algo.roughlySeparateSessionForVisitItems

--- test/js/algo/dbscan3/dbscan3_tests.js
+++ test/js/algo/dbscan3/dbscan3_tests.js
@@ -50,64 +50,6 @@ Cotton.Algo.simpleCluster = function(lHistoryItems, iNbCluster){
   });
 
 };
-test('detect session for visititems.', function() {
-  var lSampleVisitItems = chrome_visit_source_green.slice();
-  lSampleVisitItems.sort(function(a,b){
-    return b['lastVisitTime'] - a['lastVisitTime'];
-  });
-  DEBUG && console.debug(lSampleVisitItems);
-  var iSessionId = 0;
-  Cotton.Algo.roughlySeparateSession(lSampleVisitItems,
-    function(lNewRoughSession){
-      if(lNewRoughSession.length < 10) {
-        DEBUG && console.debug("A session with length < 10:");
-        DEBUG && console.debug(lNewRoughSession);
-        iSessionId += 1;
-      }
-      ok((iSessionId < 40), iSessionId + ' ' + lNewRoughSession.length);
-  });
-});
-
-test("algo of dbscan3 worker.", function() {
-  var lSampleHistoryItems = cotton_history_source_hadrien;
-
-  // we need oHistoryItems objects to compute bagOfWords
-
-  var lHistoryItems = [];
-  for (var i = 0, dHistoryItem; dHistoryItem = lSampleHistoryItems[i]; i++){
-    lHistoryItems.push(new Cotton.Model.HistoryItem(dHistoryItem));
-  }
-  lHistoryItems = Cotton.DB.Populate.computeBagOfWordsForHistoryItemsList(lHistoryItems);
-  lHistoryItems = Cotton.DB.Populate.removeHistoryItemsWithoutBagOfWords(lHistoryItems);
-  // then back to dictionaries for the algo, just like in the worker
-  for (var i = 0, oHistoryItem; oHistoryItem = lHistoryItems[i]; i++){
-    lHistoryItems[i] = oHistoryItem.dbRecord();
-  }
-  // PARAMETERS
-  // Max Distance between neighborhood
-  var fEps = Cotton.Config.Parameters.dbscan3.fEps;
-  // Min Points in a cluster
-  var iMinPts = Cotton.Config.Parameters.dbscan3.iMinPts;
-
-  Cotton.Algo.roughlySeparateSession(lHistoryItems, function(lSession) {
-    // For each rough session, launch dbscan.
-    DEBUG && console.debug("New session : " + lSession.length);
-    DEBUG && console.debug(lSession);
-    // TODO(rmoutard) : Maybe create a worker, by session. or use a queue.
-    var iNbCluster = Cotton.Algo.DBSCAN(lSession, fEps, iMinPts,
-        Cotton.Algo.Score.DBRecord.HistoryItem);
-    /**
-     * This worker has no access to window or DOM. So update DOM should be done
-     * in the main thread.
-     */
-    var dData = {};
-    dData['iNbCluster'] = iNbCluster;
-    dData['lHistoryItems'] = lSession;
-
-    ok(true, "dbscan3 runs");
-  });
-
-});
 
 test("algo of dbscan3 worker for hadrien with very low fEps (0)", function() {
 
