28b50b56a246fedc6a3f51c8e4cdb7835a6fc072
Raphael MOUTARD <rmoutard@ltutech.com>
2013-06-13 11:45:09 +0200

    DB: remove useless methods in database_factory

--- db/database_factory.js
+++ db/database_factory.js
@@ -8,73 +8,6 @@ Cotton.DB.DatabaseFactory = Class.extend({
     this._lDatabaseTypesAvailable = ['indexeddb', 'localstorage'];
   },
 
-  get : function(sDatabaseType, sDatabaseName, dTranslators, mCallback) {
-    var self = this;
-    switch (sDatabaseType){
-
-      case 'indexeddb':
-        if(_.isEmpty(dTranslators)){
-          dTranslators = {
-            'stories' : Cotton.Translators.STORY_TRANSLATORS,
-            'historyItems' : Cotton.Translators.HISTORY_ITEM_TRANSLATORS,
-            'searchKeywords' : Cotton.Translators.SEARCH_KEYWORD_TRANSLATORS
-          };
-        }
-        //TODO(rmoutard) : put the engine creation here.
-        var oDatabase = new Cotton.DB.IndexedDB.Wrapper(sDatabaseName,
-          dTranslators,
-          function() {
-            mCallback();
-        });
-        // TODO(rmoutard) : extend the wrapper if needed.
-        // _.extend(oDatabase, indexeddb_mixin);
-        return oDatabase;
-        break;
-
-      case 'localstorage':
-        if(_.isEmpty(dTranslators)){
-          dTranslators = {
-            'historyItems' : Cotton.Translators.HISTORY_ITEM_TRANSLATORS,
-          };
-        }
-        //TODO(rmoutard) : put the engine creation here.
-        var oDatabase = new Cotton.DB.LocalStorage.Wrapper(sDatabaseName,
-            dTranslators,
-            function() {
-              mCallback();
-        });
-        // TODO(rmoutard) : extend the wrapper if needed.
-        //  _.extend(oDatabase, localstorage_mixin);
-        return oDatabase;
-
-        break;
-
-      default:
-        console.error("The database type" + sDatabaseType + "is not available."
-          + "Database types available are : ");
-        console.error(self._lDatabaseTypesAvailable);
-        break;
-    }
-  },
-
-  getCt : function(sDatabaseName, dTranslators, mOnReadyCallback) {
-    var dIndexesForObjectStoreNames = {};
-    _.each(dTranslators, function(lTranslators, sObjectStoreName) {
-      dIndexesForObjectStoreNames[sObjectStoreName] = self._lastTranslator(sObjectStoreName).indexDescriptions();
-    });
-
-    var oEngine = new Cotton.DB.IndexedDB.Engine(
-        sDatabaseName,
-        dIndexesForObjectStoreNames,
-        function() {
-          var oDatabase = new Cotton.DB.WrapperForEngine(sDatabaseName,
-            dTranslators, oEngine, mOnReadyCallback);
-          return oDatabase;
-    });
-
-
-  },
-
   getCache : function(sCacheName) {
     // is there is already a cache with this name, fill the cache with the old cache value.
     var lCacheStore = JSON.parse(localStorage.getItem('ct-cache-' + sCacheName + '-store'));
@@ -90,5 +23,7 @@ Cotton.DB.DatabaseFactory = Class.extend({
       // need a wrapper here.
       return oCache;
     }
-  },
+  }
+
+  // TODO(rmoutard): add a function to get the main database using models.
 });
