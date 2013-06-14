9d1de55602b6025c51ba941d683b716b67046478
Raphael MOUTARD <rmoutard@ltutech.com>
2013-06-14 11:59:42 +0200

    Test: use Date.UTC in test to avoid problem with localtime

--- test/js/ui/story/item/content/brick/date_tests.js
+++ test/js/ui/story/item/content/brick/date_tests.js
@@ -9,14 +9,27 @@ module("Cotton.UI.Story.Item.Content.Brick.Date",{
   }
 });
 
-test("init.", function(){
+test("init.", function() {
   var oTitle = new Cotton.UI.Story.Item.Content.Brick.Date(1363655245708);
   ok(oTitle);
 });
 
-test("init.", function(){
-  var oTitle = new Cotton.UI.Story.Item.Content.Brick.Date(1363655245708);
-  equal(oTitle._$date.text(), '18 March - 10:07 pm');
+test("preview of date is correct.", function() {
+  //Date is dependant of the local time config of the
+  //browser. for test we need to use UTC to avoid problems.
+  // Months and hours starts by 0 !
+  var a = Date.UTC(2012, 2, 18, 7, 6, 5);
+  var oTitle = new Cotton.UI.Story.Item.Content.Brick.Date(a);
+  equal(oTitle._$date.text(), '18 March - 8:06 am');
+});
+
+test("preview of date is correct.", function() {
+  //Date is dependant of the local time config of the
+  //browser. for test we need to use UTC to avoid problems.
+  // Months and hours starts by 0 !
+  var a = Date.UTC(2012, 2, 18, 9, 6, 5);
+  var oTitle = new Cotton.UI.Story.Item.Content.Brick.Date(a);
+  equal(oTitle._$date.text(), '18 March - 10:06 am');
 });
 
 
