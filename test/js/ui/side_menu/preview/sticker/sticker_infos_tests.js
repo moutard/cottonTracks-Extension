6f2b3320e221d5cf43491451a03c02e080b6e2f6
Raphael Korach <raphael.korach@gmail.com>
2013-07-14 13:42:21 -0400

    Test: fix stickers tests

--- test/js/ui/side_menu/preview/sticker/sticker_infos_tests.js
+++ test/js/ui/side_menu/preview/sticker/sticker_infos_tests.js
@@ -6,8 +6,11 @@ module('Cotton.UI.SideMenu.Preview.Sticker.Infos', {
 
 test('init.', function() {
   var sStoryTitle = "Alice in wonderland";
+  var iStoryId = 1;
   var oDispatcher = new MockDispatcher();
+  var sTypeOfSticker = "";
+  var iNumberOfItems = 15;
   var oStickerInfos = new Cotton.UI.SideMenu.Preview.Sticker.Infos(
-    sStoryTitle, oDispatcher);
+    sStoryTitle, iStoryId, oDispatcher, iNumberOfItems);
   ok(oStickerInfos);
 });
