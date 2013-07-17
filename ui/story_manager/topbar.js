99690f033438654c11e97fd2136d616a7463b01a
Raphael Korach <raphael.korach@gmail.com>
2013-07-17 01:23:17 -0400

    UI: no more 'go back to page'

--- ui/story_manager/topbar.js
+++ ui/story_manager/topbar.js
@@ -10,17 +10,12 @@ Cotton.UI.StoryManager.Topbar = Class.extend({
 
     this._$logo_manager = $('<img class="ct-logo_manager" src="media/images/manager/logo_manager.png">');
     this._oSearch = new Cotton.UI.StoryManager.Search(oDispatcher);
-    this._$quit = $('<div class="ct-quit"><img src="media/images/manager/close_manager.png"><p>Go back to the page</p></div>').click(function(){
-      Cotton.ANALYTICS.backToPage('topbar X');
-      window.history.back();
-    });
 
     this._$topbar.append(
       this._$logo_manager,
       this._$topbar_container.append(
         this._oSearch.$()
-      ),
-      this._$quit
+      )
     );
 
   },
