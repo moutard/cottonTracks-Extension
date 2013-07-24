278aa46b568f9624d0d87ac74d8593ea00f09ceb
Raphael MOUTARD <rmoutard@ltutech.com>
2013-07-24 17:37:29 +0200

    Compiler: solve problems of missing files

---
+++ test/integration/js/background/background.js
@@ -0,0 +1,26 @@
+'use strict';
+(fuction() {
+var IntegrationBackground = Class.extend({
+
+  init: function() {
+    var oDatabase = new Cotton.DB.IndexedDB.Wrapper('ctb', {
+        'searchKeywords': Cotton.Translators.SEARCH_KEYWORD_TRANSLATORS,
+        'historyItems': Cotton.Translators.HISTORY_ITEM_TRANSLATORS,
+        'stories': Cotton.Translators.STORY_TRANSLATORS
+      }, function() {
+        // Get the historyClient (depends on the browser)
+        var oClient = new MockHistoryClient();
+        Cotton.Core.Populate.visitItems(oDatabase,
+          function(lHistoryItems, lVisitItems) {
+            chrome.tabs.create({
+              "url": "integration_tests.html"
+            });
+        }, oClient);
+    });
+  },
+
+});
+oIntegrationBackground = new IntegrationBackground();
+})();
+
+
