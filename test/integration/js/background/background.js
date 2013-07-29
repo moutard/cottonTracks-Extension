63a73cf2e2f05ba89f9d8df7794963ca2b4466cc
Raphael Moutard <raphael.moutard@gmail.com>
2013-07-29 18:53:15 +0200

    Test: add qunit new console log

--- test/integration/js/background/background.js
+++ test/integration/js/background/background.js
@@ -1,15 +1,18 @@
 'use strict';
 var IntegrationBackground = Class.extend({
 
+  _oDatabase : undefined,
+
   init: function() {
-    var oDatabase = new Cotton.DB.IndexedDB.Wrapper('ctb', {
+    var self = this;
+    self._oDatabase = new Cotton.DB.IndexedDB.Wrapper('ctb', {
         'searchKeywords': Cotton.Translators.SEARCH_KEYWORD_TRANSLATORS,
         'historyItems': Cotton.Translators.HISTORY_ITEM_TRANSLATORS,
         'stories': Cotton.Translators.STORY_TRANSLATORS
       }, function() {
         // Get the historyClient (depends on the browser)
         var oClient = new MockHistoryClient();
-        Cotton.Core.Populate.visitItems(oDatabase,
+        Cotton.Core.Populate.visitItems(self._oDatabase,
           function(lHistoryItems, lVisitItems) {
             chrome.tabs.create({
               "url": "integration_tests.html"
