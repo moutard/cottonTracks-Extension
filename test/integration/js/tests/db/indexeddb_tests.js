305d1ea9108e9ea763bda7fbc464f4cfad1739ae
Raphael Moutard <raphael.moutard@gmail.com>
2013-07-29 18:53:15 +0200

    Test: add qunit new console log

---
+++ test/integration/js/tests/db/indexeddb_tests.js
@@ -0,0 +1,16 @@
+module("Cotton.Integration.DB",{
+  setup: function() {
+    // runs before each test
+  },
+  teardown: function() {
+    // runs after each test
+  }
+});
+
+test("putUnique.", function() {
+  equal(1,1);
+});
+
+asyncTest("putUnique.", function() {
+  equal(1,1);
+});
