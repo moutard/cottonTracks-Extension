58241b85b051ecb9db2cb938b0150ca893b874cc
Raphael Moutard <raphael.moutard@gmail.com>
2013-08-01 18:17:07 +0200

    Test: add tests for empty and merge

--- test/integration/js/tests/db/indexeddb_tests.js
+++ test/integration/js/tests/db/indexeddb_tests.js
@@ -38,7 +38,6 @@ asyncTest("putUnique do not duplicate the element.", function() {
       start();
     });
   });
-
 });
 
 /**
@@ -52,5 +51,45 @@ asyncTest("putList an empty list return callback.", function() {
     ok("the callback is called.");
     start();
   });
+});
 
+/**
+ * test that the empty method of the database work.
+ */
+asyncTest("empty works fine.", function() {
+  // Just to be sure that it's the last things display in the console,
+  // so it's more easy to debug.
+  var oDatabase = oIntegrationBackground.database();
+  oDatabase.empty('historyItems', function(id) {
+    equal(false, id);
+    oDatabase.empty('stories', function(id) {
+      equal(true, id);
+      start();
+    });
+  });
+});
+
+asyncTest("putUnique do not duplicate the element.", function() {
+  // Just to be sure that it's the last things display in the console,
+  // so it's more easy to debug.
+  var _id;
+  var oDatabase = oIntegrationBackground.database();
+  var oSearchKeyword1 = new Cotton.Model.SearchKeyword('alice');
+  oSearchKeyword1.addReferringHistoryItemId(1);
+  oSearchKeyword1.addReferringHistoryItemId(2);
+  oDatabase.putUnique('searchKeywords', oSearchKeyword1, function(id1) {
+    _id = id1;
+    var oSearchKeyword2 = new Cotton.Model.SearchKeyword('alice');
+    oSearchKeyword2.addReferringHistoryItemId(4);
+    oSearchKeyword2.addReferringHistoryItemId(5);
+    oSearchKeyword2.addReferringStoryId(9);
+    ok( true, "Passed and ready to resume!" );
+    oDatabase.putUnique('searchKeywords', oSearchKeyword2, function(id2) {
+      oDatabase.find('searchKeywords', 'id', id2, function(oSearchKeyword) {
+        deepEqual(oSearchKeyword.referringStoriesId(), [9]);
+        deepEqual(oSearchKeyword.referringHistoryItemsId(), [4,5,1,2]);
+        start();
+      });
+    });
+  });
 });
