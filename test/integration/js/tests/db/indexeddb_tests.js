6a4cfdcbb265113189b0ef5c794f986e13390621
Raphael Moutard <raphael.moutard@gmail.com>
2013-08-01 17:48:20 +0200

    Test: first async tests for indexeddb

--- test/integration/js/tests/db/indexeddb_tests.js
+++ test/integration/js/tests/db/indexeddb_tests.js
@@ -1,17 +1,17 @@
 module("Cotton.Integration.DB",{
-  setup,function() {
+  setup:function() {
     // runs before each test
   },
-  teardown,function() {
+teardown:function() {
     // runs after each test
   }
 });
 
-test("putUnique.", function() {
+test("fake succeed test.", function() {
   equal(1,1);
 });
 
-asyncTest("putUnique.", function() {
+asyncTest("fake succeed asynchronous test.", function() {
   // Just to be sure that it's the last things display in the console,
   // so it's more easy to debug.
   setTimeout(function() {
@@ -20,4 +20,37 @@ asyncTest("putUnique.", function() {
   }, 1000);
 });
 
-var l;
+/**
+ * check that the putunique do not duplicate the element.
+ */
+asyncTest("putUnique do not duplicate the element.", function() {
+  // Just to be sure that it's the last things display in the console,
+  // so it's more easy to debug.
+  var _id;
+  var oDatabase = oIntegrationBackground.database();
+  var oSearchKeyword = new Cotton.Model.SearchKeyword('avignon');
+  oDatabase.putUnique('searchKeywords', oSearchKeyword, function(id1) {
+    _id = id1;
+    ok( true, "Passed and ready to resume!" );
+    oDatabase.putUnique('searchKeywords', oSearchKeyword, function(id2) {
+      ok(id2);
+      equal( _id, id2);
+      start();
+    });
+  });
+
+});
+
+/**
+ * check that put an empty list return the callback function.
+ */
+asyncTest("putList an empty list return callback.", function() {
+  // Just to be sure that it's the last things display in the console,
+  // so it's more easy to debug.
+  var oDatabase = oIntegrationBackground.database();
+  oDatabase.putList('searchKeywords', [], function(id) {
+    ok("the callback is called.");
+    start();
+  });
+
+});
