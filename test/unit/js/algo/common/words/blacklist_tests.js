b562d7355ed31a95f1363f074bb4781767655fd5
Raphael Korach <raphael.korach@gmail.com>
2013-08-16 09:29:20 -0400

    Refactor: generateBlacklistExpression tests (now takes cotton historyitem)

--- test/unit/js/algo/common/words/blacklist_tests.js
+++ test/unit/js/algo/common/words/blacklist_tests.js
@@ -1,117 +1,97 @@
 module("Cotton.Algo.Common.GenerateBlacklistExpressions", {});
 
 test('Titles with "-".', function(){
-  var lHistoryItems = [
-    {
-      'title': "Test - Wikipedia, the free encyclopedia",
-      'url': "http://en.wikipedia.org/wiki/test",
-      'visitCount': 100
-    },
-    {
-      'title': "Test - Wikipédia, l'encyclopédie libre",
-      'url': "http://fr.wikipedia.org/wiki/test",
-      'visitCount': 100
-    },
-    {
-      'title': "- Hommage à Jacques Tati (Bataclan 2009)",
-      'url': "http://www.youtube.com/fr/watch?v=EzV8rd2",
-      'visitCount': 100
-    },
-    {
-      'title': "compare words in two strings - Recherche Google",
-      'url': 'https://www.google.com/search?q=compare+words+in+two+strings',
-      'visitCount': 100
-    },
-    {
-      'title': "Chrome Web Store - cottonTracks",
-      'url': 'https://chrome.google.com/webstore/detail/flmfagndkngjknjjcoejaihmibcfcjdh',
-      'visitCount': 100
-    },
-    {
-      'title': "The End - The Doors - YouTube",
-      'url': 'http://www.youtube.com/watch?v=ZUb4eh0AIhU&list=RD02OHQtbalbYMc',
-      'visitCount': 100
-    },
+  var oHistoryItem1 = new Cotton.Model.HistoryItem({
+      'sTitle': "Test - Wikipedia, the free encyclopedia",
+      'sUrl': "http://en.wikipedia.org/wiki/test",
+      'iVisitCount': 100
+    });
+  var oHistoryItem2 = new Cotton.Model.HistoryItem({
+      'sTitle': "Test - Wikipédia, l'encyclopédie libre",
+      'sUrl': "http://fr.wikipedia.org/wiki/test",
+      'iVisitCount': 100
+    });
+  var oHistoryItem3 = new Cotton.Model.HistoryItem({
+      'sTitle': "- Hommage à Jacques Tati (Bataclan 2009)",
+      'sUrl': "http://www.youtube.com/fr/watch?v=EzV8rd2",
+      'iVisitCount': 100
+    });
+  var oHistoryItem4 = new Cotton.Model.HistoryItem({
+      'sTitle': "compare words in two strings - Recherche Google",
+      'sUrl': 'https://www.google.com/search?q=compare+words+in+two+strings',
+      'iVisitCount': 100
+    });
+  var oHistoryItem5 = new Cotton.Model.HistoryItem({
+      'sTitle': "Chrome Web Store - cottonTracks",
+      'sUrl': 'https://chrome.google.com/webstore/detail/flmfagndkngjknjjcoejaihmibcfcjdh',
+      'iVisitCount': 100
+    });
+  var oHistoryItem6 = new Cotton.Model.HistoryItem({
+      'sTitle': "The End - The Doors - YouTube",
+      'sUrl': 'http://www.youtube.com/watch?v=ZUb4eh0AIhU&list=RD02OHQtbalbYMc',
+      'iVisitCount': 100
+    });
 
-  ];
+  var lHistoryItems = [oHistoryItem1 ,oHistoryItem2 ,oHistoryItem3 ,oHistoryItem4 ,oHistoryItem5 ,oHistoryItem6];
 
   var lBlacklist = Cotton.Algo.Common.Words.generateBlacklistExpressions(lHistoryItems);
   deepEqual(lBlacklist, [".jpg", ".jpeg", ".png", ".gif", ".pdf", "- Wikipedia, the free encyclopedia", "- Wikipédia, l'encyclopédie libre", "- Recherche Google", "Chrome Web Store -", "- YouTube"] );
 });
 
 test('Titles with "-".', function(){
-  var lBlacklist = Cotton.Algo.Common.Words.generateBlacklistExpressions(_history_items);
-  deepEqual(lBlacklist,
-  [".jpg",".jpeg",".png",".gif",".pdf","| ARTE"
-  ,"Shape the World -"
-  ,"- Aldebaran Robotics "
-  ," Aldebaran Robotics |"
-  ,"- Recherche Google"
-  ,"VOD ,Télécharger Sur un fil sur UniversCiné -"
-  ,"::,FILMS DISTRIBUTION ::,Peter Brook -"
-  ,"- ARTE Boutique"
-  ,"- WordReference Forums"
-  ,"- Vente"
-  ,"| LinkedIn"
-  ,"- WordReference.com"
-  ,"- Les Trois Coups"
-  ,"| Coup de théâtre"
-  ,"| Culturebox"
-  ,"- Shakespeare en devenir "
-  ,"- Université de Poitiers"
-  ," Shakespeare en devenir -"
-  ,"- Wikipedia, the free encyclopedia"
-  ,"| En streaming sur francetv pluzz"
-  ,"| Vidéo en streaming sur francetv pluzz"
-  ,"- Stack Overflow"
-  ,"- Dragon City Wiki"
-  ,"Cool Fire Dragon -"
-  ,"Lava Dragon -"
-  ,"- Culturebox "
-  ,"- France Télévisions"
-  ," Culturebox -"
-  ,"- GameSpot.com"
-  ,"- Dragon Island Wiki!"
-  ,"Dragon Island Forum • View topic -"
-  ,"- Mac OS X Hints"
-  ,"Electric Dragon -"
-  ,"- Wikipédia"
-  ,"- Haypo"
-  ,"Star Dragon -"
-  ,"Firebird Dragon -"
-  ,"Flaming Rock Dragon -"
-  ,"Terra Dragon -"
-  ,"Volcano Dragon -"
-  ,"- CASPEVI.com"
-  ,"| Qualys, Inc."
-  ,"Chameleon Dragon -"
-  ,"Armadillo Dragon -"
-  ,"Blizzard Dragon -"
-  ,"Storm Dragon -"
-  ,"Icecube Dragon -"
-  ,"Cloud Dragon -"
-  ,"Penguin Dragon -"
-  ,"Hot Metal Dragon -"
-  ,"Gummy Dragon -"
-  ,"Gold Dragon -"
-  ,"Pearl Dragon -"
-  ,"- Deauville avec Voyages"
-  ,"- Flyingblue"
-  ,"Mr Miles -"
-  ,"- CNET"
-  ,"ratp.fr -"
-  ,"- Google Maps"
-  ,"- videos.arte.tv"
-  ,"ARTE Journal -"
-  ,"| ARTE Journal "
-  ,"- ARTE"
-  ," ARTE Journal |"
-  ,"ARTE Journal |"
-  ,"Sculpteo |"
-  ,"- AlloCiné"
-  ,"TVsubtitles.net -"
-  ,"- Avignon Festival & Compagnies"
-  ,"- Site officiel du festival OFF d'Avignon"
-  ,"Avignon Festival & Compagnies -"
-  ,"- Super User"]);
+  // var lTranslators = Cotton.Translators.HISTORY_ITEM_TRANSLATORS;
+  // var oTranslator = lTranslators[lTranslators.length - 1];
+
+  var iLength = _history_items.length;
+  var lHistoryItems = [];
+
+  var oTranslator = Cotton.Translators.HISTORY_ITEM_TRANSLATORS[0];
+
+  for (var i = 0; i < iLength; i++){
+    var dHistoryItem = _history_items[i];
+    var oHistoryItem = oTranslator.chromeHistoryItemToObject(dHistoryItem);
+    lHistoryItems.push(oHistoryItem);
+  }
+  var lBlacklist = Cotton.Algo.Common.Words.generateBlacklistExpressions(lHistoryItems);
+  deepEqual(lBlacklist.sort(),
+  [
+    "- ARTE",
+    "- AlloCiné",
+    "- Avignon Festival & Compagnies",
+    "- Culturebox",
+    "- Dictionnaire Français-Anglais WordReference.com",
+    "- Dragon City Wiki",
+    "- English-French Dictionary WordReference.com",
+    "- Festival OFF 2013",
+    "- Flyingblue",
+    "- France Télévisions",
+    "- Google Drive",
+    "- Grooveshark",
+    "- Job | LinkedIn",
+    "- OVH",
+    "- Recherche Google",
+    "- Stack Overflow",
+    "- Wikipedia, the free encyclopedia",
+    "- Wikipédia",
+    "- cottonTracks Mail",
+    ".gif",
+    ".jpeg",
+    ".jpg",
+    ".pdf",
+    ".png",
+    "ARTE Journal |",
+    "Cover letter Google.docx -",
+    "Culturebox -",
+    "Festival OFF 2013 -",
+    "Grooveshark -",
+    "LTU Cloud |",
+    "Mr Miles -",
+    "Shape the World -",
+    "TVsubtitles.net -",
+    "| ARTE Journal",
+    "| Culturebox",
+    "| En streaming sur francetv pluzz",
+    "| LinkedIn",
+    "| fr - ARTE"
+  ].sort());
 });
