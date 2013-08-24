b285af4d4788eefa66296ee89ead4ebecb68b7c0
Raphael Korach <raphael.korach@gmail.com>
2013-08-24 14:03:39 -0400

    Core: Cotton.Core.Webstore for ui rate_us button
    Conflicts:
    
    	ui/settings/settings.js

---
+++ core/chrome/webstore.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Cotton.Core.Webstore = Class.extend({
+  init : function() {
+
+  },
+
+  getUrl : function() {
+    return "https://chrome.google.com/webstore/detail/cottontracks/flmfagndkngjknjjcoejaihmibcfcjdh/reviews";
+  }
+
+});
