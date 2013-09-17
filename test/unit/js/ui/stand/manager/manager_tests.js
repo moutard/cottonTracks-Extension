03fa1e12512cf46657816bf756d6fb8579f9a8f0
Raphael Moutard <raphael.moutard@gmail.com>
2013-09-17 23:12:07 +0200

    Test: createShelf simple test

--- test/unit/js/ui/stand/manager/manager_tests.js
+++ test/unit/js/ui/stand/manager/manager_tests.js
@@ -9,3 +9,25 @@ test('init.', function() {
   var oManager = new Cotton.UI.Stand.Manager.UIManager([], oDispatcher);
   ok(oManager);
 });
+
+test('createShelves.', function() {
+  var oDispatcher = new Cotton.Messaging.Dispatcher();
+  var oNow = new Date();
+  var oTomorrow = new Date(oNow.getFullYear(), oNow.getMonth(), oNow.getDate() + 1,
+        0, 0, 0, 0);
+  var fTomorrow = oTomorrow.getTime();
+
+  var ONE_DAY = 86400000; // Millisecond in one day.
+  var ONE_WEEK = 604800000; // Millisecond in one week.
+  var ONE_MONTH = oTomorrow.getTime() - new Date(oNow.getFullYear(),
+        oNow.getMonth() - 1, 1, 0, 0, 0, 0).getTime();
+
+  var lStories = [];
+  for (var i = 0; i < 10; i++) {
+    var oStory = new Cotton.Model.Story();
+    oStory.setLastVisitTime(oNow.getTime());
+    lStories.push(oStory);
+  }
+  var oManager = new Cotton.UI.Stand.Manager.UIManager(lStories, oDispatcher);
+  ok(oManager);
+});
