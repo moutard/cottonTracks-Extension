a62b8fc6a39f6a6515e04c1a36547c5e8b8e8fb9
Raphael Moutard <raphael.moutard@gmail.com>
2013-09-22 18:22:23 +0200

    UI: responsive design story
    Refactor: make sure that dashboard exist for pushBack bringFront when scrolling

--- ui/stand/story/ui_story.js
+++ ui/stand/story/ui_story.js
@@ -20,17 +20,45 @@ Cotton.UI.Stand.Story.UIStory = Class.extend({
    */
   _oDeck : null,
 
+  /**
+   * {DOM} story container
+   */
+  _$story : null,
+
+  /**
+   * {DOM} story container
+   */
+  _$story_container : null,
+
   init : function(oStory, oGlobalDispatcher) {
     var self = this;
 
-    // DOM object for the story container
-    this._$story = $('<div class="ct-main_container ct-story"></div>');
+    // DOM object for the story container.
+    this._$story = $('<div class="ct-story"></div>');
+
+    this._$story_container = $('<div class="ct-story_container"></div>');
 
     this._oDashboard = new Cotton.UI.Stand.Story.Dashboard(oStory, oGlobalDispatcher);
-    this._oDeck = new Cotton.UI.Stand.Story.Deck(oStory, oGlobalDispatcher);
-    this._$story.append(this._oDashboard.$(), this._oDeck.$());
+    this._oDeck = new Cotton.UI.Story.Deck(oStory, oGlobalDispatcher);
+    this._$story.append(
+      this._$story_container.append(
+        this._oDashboard.$(),
+        this._oDeck.$()
+      )
+    );
 
-    },
+    this._$story.bind('mousewheel',function(){
+      self._oDashboard.pushBack();
+      if (!self._bScrolling) {
+        self._bScrolling = true;
+        self._oScrollTimeout = setTimeout(function(){
+          self._bScrolling = false;
+          self._oDashboard.bringFront();
+        }, 600);
+      }
+    });
+
+  },
 
   $ : function() {
     return this._$story;
@@ -46,11 +74,16 @@ Cotton.UI.Stand.Story.UIStory = Class.extend({
   },
 
   purge : function() {
+    // Clear the timeout, otherwise we could try asyncronously to access the _oDashboard
+    // that has already been purged. ex: swipe right to go to previous page.
+    clearTimeout(this._oScrollTimeout);
+    this._oScrollTimeout = null;
+    this._bScrolling = null;
     this._oDeck.purge();
     this._oDeck = null;
     this._oDashboard.purge();
     this._oDashboard = null;
-    this._$story.remove();
+    this._$story.unbind('mousewheel').remove();
     this._$story = null;
   }
 
