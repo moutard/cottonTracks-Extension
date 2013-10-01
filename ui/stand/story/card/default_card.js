d68705983e2d30e97237c5d4b06dd37960e75fe0
Raphael Korach <raphael.korach@gmail.com>
2013-10-01 23:33:03 +0200

    UI: draw cards

---
+++ ui/stand/story/card/default_card.js
@@ -0,0 +1,8 @@
+"use strict";
+
+Cotton.UI.Stand.Story.Card.Default = Cotton.UI.Stand.Story.Card.Card.extend({
+
+  init : function(oHistoryItem, oGlobalDispatcher) {
+    this._super(oHistoryItem, oGlobalDispatcher);
+  }
+});
--- ui/stand/story/deck.js
+++ ui/stand/story/deck.js
@@ -7,24 +7,61 @@
  */
 Cotton.UI.Stand.Story.Deck = Class.extend({
   /**
-   * {DOM} current element.
+   * General Dispatcher that allows two diffent parts of the product to communicate
+   * together through the controller of the app.
    */
-  _$deck : null,
+  _oGlobalDispatcher : null,
+
+  /**
+   * {DOM} right column of the story, with all the cards
+   */
+  _$card_deck : null,
+
+  /**
+   * {array} array containing all the card objects
+   */
+  _lCards : null,
 
   init : function(oStory, oGlobalDispatcher) {
+    this._oGlobalDispatcher = oGlobalDispatcher;
 
     this._$card_deck = $('<div class="ct-story_card_deck"></div>');
+    this._lCards = [];
+
   },
 
   $ : function() {
     return this._$card_deck;
   },
 
-  drawCards : function() {
+  /**
+   * place all the cards in the deck
+   * @param {Cotton.Model.Story} oStory:
+   *    story to be drawn
+   **/
+  drawCards : function(oStory) {
+    var lHistoryItems = oStory.historyItems();
+    var iLength = lHistoryItems.length;
+    for (var i = 0; i < iLength; i++) {
+      var oHistoryItem = lHistoryItems[i];
+      var oCard = new Cotton.UI.Stand.Story.Card.Factory(oHistoryItem, this._oGlobalDispatcher);
+      this._lCards.push(oCard);
+      this._$card_deck.append(oCard.$());
+    }
+  },
 
+  purgeCards : function() {
+    var iLength = this._lCards.length;
+    for (var i = 0; i < iLength; i++) {
+      this._lCards[i].purge();
+      this._lCards[i] = null;
+    }
+    this._lCards = null;
   },
 
   purge : function() {
+    this._oGlobalDispatcher = null;
+    this.purgeCards();
     this._$card_deck.remove();
     this._$card_deck = null;
   }
