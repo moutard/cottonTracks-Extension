67ef2e725dde49bf187a8308efd4735ad515747a
Raphael Korach <raphael.korach@gmail.com>
2013-10-01 18:28:21 +0200

    UI: remove card

--- ui/stand/story/deck.js
+++ ui/stand/story/deck.js
@@ -28,6 +28,9 @@ Cotton.UI.Stand.Story.Deck = Class.extend({
     this._$card_deck = $('<div class="ct-story_card_deck"></div>');
     this._lCards = [];
 
+    this._oGlobalDispatcher.subscribe('remove_card', this, function(dArguments) {
+      this.removeCard(dArguments['history_item_id']);
+    });
   },
 
   $ : function() {
@@ -53,6 +56,33 @@ Cotton.UI.Stand.Story.Deck = Class.extend({
     }
   },
 
+  removeCard : function(iHistoryItemId) {
+    var self = this;
+    var iLength = this._lCards.length;
+    // Array of remaining cards
+    var lRemainingCards = [];
+    for (var i = 0; i < iLength; i++) {
+      if (this._lCards[i].id() === iHistoryItemId) {
+        // the cards to delete
+        var oCardToRemove = this._lCards[i];
+        oCardToRemove.removeCard();
+        setTimeout(function() {
+          oCardToRemove.purge();
+          oCardToRemove = null;
+          if (self._lCards.length === 0) {
+            self._oGlobalDispatcher.publish('home');
+          }
+        }, 600);
+        var bDeleted = true;
+      } else {
+        lRemainingCards.push(this._lCards[i]);
+        this._lCards[i] = null;
+      }
+    }
+    this._lCards = null;
+    this._lCards = lRemainingCards;
+  },
+
   purgeCards : function() {
     var iLength = this._lCards.length;
     for (var i = 0; i < iLength; i++) {
@@ -63,6 +93,7 @@ Cotton.UI.Stand.Story.Deck = Class.extend({
   },
 
   purge : function() {
+    this._oGlobalDispatcher.unsubscribe('remove_card', this);
     this._oGlobalDispatcher = null;
     this.purgeCards();
     this._$card_deck.remove();
