d3739252739bbe24f72591041fd0b8eb560a8e8b
Raphael Moutard <raphael.moutard@gmail.com>
2013-10-01 23:33:03 +0200

    UI: draw cards

---
+++ ui/stand/story/card/image_card.js
@@ -0,0 +1,8 @@
+"use strict";
+
+Cotton.UI.Stand.Story.Card.Image = Cotton.UI.Stand.Story.Card.Card.extend({
+
+  init : function(sImageUrl, oHistoryItem, oGlobalDispatcher) {
+    this._super(oHistoryItem, oGlobalDispatcher);
+  }
+});
---
+++ ui/stand/story/card/video_card.js
@@ -0,0 +1,8 @@
+"use strict";
+
+Cotton.UI.Stand.Story.Card.Video = Cotton.UI.Stand.Story.Card.Card.extend({
+
+  init : function(sEmbedCode, sVideoType, oHistoryItem, oGlobalDispatcher) {
+    this._super(oHistoryItem, oGlobalDispatcher);
+  }
+});
