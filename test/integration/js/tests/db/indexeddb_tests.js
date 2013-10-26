7011a124a1af7c3299094a6f49e03e6721020846
rmoutard <raphael.moutard@gmail.com>
2013-10-26 15:57:59 +0200

    Test: make integration tests for putUnique

--- test/integration/js/tests/db/indexeddb_tests.js
+++ test/integration/js/tests/db/indexeddb_tests.js
@@ -93,3 +93,226 @@ asyncTest("putUnique do not duplicate the element.", function() {
     });
   });
 });
+
+asyncTest("putUnique id not set.", function() {
+  // Just to be sure that it's the last things display in the console,
+  // so it's more easy to debug.
+  var _iOldId, _iNewId;
+  var sTestUrl = "http://ct-test.com/id_is_not_set";
+  var oDatabase = oIntegrationBackground.database();
+  var oHistoryItem1 = new Cotton.Model.HistoryItem({
+    'sUrl': sTestUrl,
+    'iVisitCount': 4,
+    'oExtractedDNA': {
+      'dBagOfWords': {
+        'first': 3,
+      }
+    }
+  });
+  oDatabase.putUnique('historyItems', oHistoryItem1, function(id1) {
+    _iOldId = id1;
+    var oHistoryItem2 = new Cotton.Model.HistoryItem({
+      'sUrl': sTestUrl,
+      'iVisitCount': 3,
+      'oExtractedDNA': {
+        'dBagOfWords': {
+          'second': 4
+        }
+      }
+    });
+
+    ok( true, "Passed and ready to resume!" );
+    oDatabase.putUnique('historyItems', oHistoryItem2, function(id2) {
+      _iNewId = id2;
+      oDatabase.find('historyItems', 'sUrl', sTestUrl, function(oHistoryItem) {
+        deepEqual(oHistoryItem.visitCount(), 4);
+        deepEqual(oHistoryItem.extractedDNA().bagOfWords(), {
+          'first': 3,
+          'second': 4
+        });
+        deepEqual(_iOldId, _iNewId);
+        start();
+      });
+    });
+  });
+});
+
+asyncTest("putUnique id(B) == id(A) and url(B) == url(A).", function() {
+  // if id(B) == id(A) and url(B) == url(A) > success and overwrite (we expect error)
+  // Just to be sure that it's the last things display in the console,
+  // so it's more easy to debug.
+  var _iOldId, _iNewId;
+  var sTestUrl = "http://ct-test.com/id_equal_and_url_equal";
+  var oDatabase = oIntegrationBackground.database();
+  var oHistoryItem1 = new Cotton.Model.HistoryItem({
+    'sUrl': sTestUrl,
+    'iVisitCount': 4,
+    'oExtractedDNA': {
+      'dBagOfWords': {
+        'first': 3,
+      }
+    }
+  });
+  oDatabase.putUnique('historyItems', oHistoryItem1, function(id1) {
+    _iOldId = id1;
+    var oHistoryItem2 = new Cotton.Model.HistoryItem({
+      'sId': id1,
+      'sUrl': sTestUrl,
+      'iVisitCount': 3,
+      'oExtractedDNA': {
+        'dBagOfWords': {
+          'second': 4
+        }
+      }
+    });
+
+    ok( true, "Passed and ready to resume!" );
+    oDatabase.putUnique('historyItems', oHistoryItem2, function(id2) {
+      _iNewId = id2;
+      oDatabase.find('historyItems', 'sUrl', sTestUrl, function(oHistoryItem) {
+        deepEqual(oHistoryItem.visitCount(), 4);
+        deepEqual(oHistoryItem.extractedDNA().bagOfWords(), {
+          'first': 3,
+          'second': 4
+        });
+        deepEqual(_iOldId, _iNewId);
+        start();
+      });
+    });
+  });
+});
+
+asyncTest("putUnique if id(B) != id(A) and url(B) == url(A).", function() {
+  // if id(B) == id(A) and url(B) == url(A) > success and overwrite (we expect error)
+  // Just to be sure that it's the last things display in the console,
+  // so it's more easy to debug.
+  var _iOldId, _iNewId;
+  var sTestUrl = "http://ct-test.com/id_not_equal_and_url_equal";
+  var oDatabase = oIntegrationBackground.database();
+  var oHistoryItem1 = new Cotton.Model.HistoryItem({
+    'sUrl': sTestUrl,
+    'iVisitCount': 4,
+    'oExtractedDNA': {
+      'dBagOfWords': {
+        'first': 3,
+      }
+    }
+  });
+  oDatabase.putUnique('historyItems', oHistoryItem1, function(id1) {
+    _iOldId = id1;
+    var oHistoryItem2 = new Cotton.Model.HistoryItem({
+      'sId': 74,
+      'sUrl': sTestUrl,
+      'iVisitCount': 3,
+      'oExtractedDNA': {
+        'dBagOfWords': {
+          'second': 4
+        }
+      }
+    });
+
+    ok( true, "Passed and ready to resume!" );
+    oDatabase.putUnique('historyItems', oHistoryItem2, function(id2) {
+      _iNewId = id2;
+      oDatabase.find('historyItems', 'sUrl', sTestUrl, function(oHistoryItem) {
+        deepEqual(oHistoryItem.visitCount(), 4, "it keeps right one");
+        deepEqual(oHistoryItem.extractedDNA().bagOfWords(), {
+          'first': 3,
+          'second': 4
+        }, "correctly merged");
+        deepEqual(_iOldId, _iNewId);
+        start();
+      });
+    });
+  });
+});
+
+asyncTest("putUnique if id(B) == id(A) and url(B) != url(A).", function() {
+  // if id(B) == id(A) and url(B) == url(A) > success and overwrite (we expect error)
+  // Just to be sure that it's the last things display in the console,
+  // so it's more easy to debug.
+  var _iOldId, _iNewId;
+  var sTestUrl = "http://ct-test.com/id_equal_and_url_not_equal";
+  var oDatabase = oIntegrationBackground.database();
+  var oHistoryItem1 = new Cotton.Model.HistoryItem({
+    'sUrl': sTestUrl,
+    'iVisitCount': 4,
+    'oExtractedDNA': {
+      'dBagOfWords': {
+        'first': 3,
+      }
+    }
+  });
+  oDatabase.putUnique('historyItems', oHistoryItem1, function(id1) {
+    _iOldId = id1;
+    var oHistoryItem2 = new Cotton.Model.HistoryItem({
+      'sId': id1,
+      'sUrl': "http://ct-test.com/what_the_fuck",
+      'iVisitCount': 3,
+      'oExtractedDNA': {
+        'dBagOfWords': {
+          'second': 4
+        }
+      }
+    });
+
+    ok( true, "Passed and ready to resume!" );
+    oDatabase.putUnique('historyItems', oHistoryItem2, function(id2) {
+      _iNewId = id2;
+      oDatabase.find('historyItems', 'sUrl', sTestUrl, function(oHistoryItem) {
+        deepEqual(oHistoryItem.visitCount(), 4, "it keeps right one");
+        deepEqual(oHistoryItem.extractedDNA().bagOfWords(), {
+          'first': 3,
+          'second': 4
+        }, "correctly merged");
+        deepEqual(_iOldId, _iNewId);
+        start();
+      });
+    });
+  });
+});
+
+asyncTest("putUnique if id(B) != id(A) and url(B) != url(A).", function() {
+  // if id(B) == id(A) and url(B) == url(A) > success and overwrite (we expect error)
+  // Just to be sure that it's the last things display in the console,
+  // so it's more easy to debug.
+  var _iOldId, _iNewId;
+  var sTestUrl = "http://ct-test.com/id_not_equal_and_url_not_equal";
+  var oDatabase = oIntegrationBackground.database();
+  var oHistoryItem1 = new Cotton.Model.HistoryItem({
+    'sUrl': sTestUrl,
+    'iVisitCount': 4,
+    'oExtractedDNA': {
+      'dBagOfWords': {
+        'first': 3,
+      }
+    }
+  });
+  oDatabase.putUnique('historyItems', oHistoryItem1, function(id1) {
+    _iOldId = id1;
+    var oHistoryItem2 = new Cotton.Model.HistoryItem({
+      'sId': 1,
+      'sUrl': "http://ct-test.com/what_the_fuck",
+      'iVisitCount': 3,
+      'oExtractedDNA': {
+        'dBagOfWords': {
+          'second': 4
+        }
+      }
+    });
+
+    ok( true, "Passed and ready to resume!" );
+    oDatabase.putUnique('historyItems', oHistoryItem2, function(id2) {
+      _iNewId = id2;
+      oDatabase.find('historyItems', 'sUrl', sTestUrl, function(oHistoryItem) {
+        deepEqual(oHistoryItem.visitCount(), 4, "it keeps right one");
+        deepEqual(oHistoryItem.extractedDNA().bagOfWords(), {
+          'first': 3,
+          'second': 4
+        }, "correctly merged");
+        deepEqual(_iOldId, _iNewId);
+        start();
+      });
+    });
+  });
+});
