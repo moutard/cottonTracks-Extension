64b0c59e8bba9320c66294f3f06ea1fd6d8acd73
pink <pink@cottontracks.com>
2013-11-15 17:04:26 -0800

    UI: add some RSS feeds

--- config/rss.js
+++ config/rss.js
@@ -34,5 +34,104 @@ Cotton.Config.RSS = [
   "http://www.wired.com/design/feed/",
   "http://www.wired.com/wiredenterprise/feed/",
   "http://www.wired.com/wiredscience/feed/",
-  "http://www.wired.com/wiredscience/category/science-blogs/feed/"
+  "http://www.wired.com/wiredscience/category/science-blogs/feed/",
+  // SPORT ILLUSTRATED
+  "http://rss.cnn.com/rss/si_topstories.rss",
+  "http://rss.cnn.com/rss/si_latest.rss",
+  // SMASH MAGAZINE
+  "http://rss1.smashingmagazine.com/feed/",
+  // ROLLING STONES
+  "http://www.rollingstone.com/siteServices/rss/allNews",
+  // VOGUE
+  "http://www.vogue.com/rss/just-in/",
+  // VARIETY
+  "http://variety.com/feed/",
+  // FUBIZ
+  "http://feeds.feedburner.com/fubiz",
+  // GRANTLAND
+  "http://www.grantland.com/feed",
+  // BBC SPORT
+  "http://feeds.bbci.co.uk/sport/0/rss.xml",
+  // FASTCOMPANY
+  "http://feeds.feedburner.com/fastcompany/headlines",
+  // BUSINESSWEEK
+  "http://www.businessweek.com/feeds/most-popular.rss",
+  "http://www.businessweek.com/feeds/homepage.rss",
+  // FINANCTIAL TIMES
+  "http://www.ft.com/rss/home/europe",
+  // BUSINESS INSIDER
+  "http://feeds2.feedburner.com/businessinsider",
+  // TED
+  "http://feeds.feedburner.com/tedtalks_video",
+  "http://feeds.feedburner.com/TedtalksHD",
+  "http://feeds.feedburner.com/tedtalks_audio",
+  "http://feeds.feedburner.com/tedblog",
+  // THE ATLANTIC
+  "http://feeds.feedburner.com/TheAtlantic",
+  "http://feeds.feedburner.com/Best-Of-The-Atlantic",
+  // WASHINGTON POST
+  "http://www.washingtonpost.com/rss/politics",
+  "http://feeds.washingtonpost.com/rss/rss_election-2012",
+  "http://feeds.washingtonpost.com/rss/opinions",
+  "http://feeds.washingtonpost.com/rss/opinions/toles",
+  "http://feeds.washingtonpost.com/rss/sports",
+  "http://feeds.washingtonpost.com/rss/national",
+  "http://feeds.washingtonpost.com/rss/national/energy-environment",
+  "http://feeds.washingtonpost.com/rss/national/health-science",
+  "http://feeds.washingtonpost.com/rss/national/higher-education",
+  "http://feeds.washingtonpost.com/rss/national/on-innovations",
+  "http://feeds.washingtonpost.com/rss/national/on-leadership",
+  "http://feeds.washingtonpost.com/rss/rss_innovations",
+  "http://feeds.washingtonpost.com/rss/rss_on-faith",
+  "http://feeds.washingtonpost.com/rss/rss_on-leadership",
+  "http://feeds.washingtonpost.com/rss/world",
+  "http://feeds.washingtonpost.com/rss/rss_blogpost",
+  "http://feeds.washingtonpost.com/rss/business",
+  "http://feeds.washingtonpost.com/rss/business/local-business",
+  "http://feeds.washingtonpost.com/rss/capital_business",
+  "http://feeds.washingtonpost.com/rss/business/on-it",
+  "http://feeds.washingtonpost.com/rss/business/blogs-columns",
+  "http://feeds.washingtonpost.com/rss/blogs/rss_the-switch",
+  "http://feeds.washingtonpost.com/rss/rss_ezra-klein",
+  "http://feeds.washingtonpost.com/rss/lifestyle",
+  "http://feeds.washingtonpost.com/rss/lifestyle/advice",
+  "http://feeds.washingtonpost.com/rss/lifestyle/food",
+  "http://feeds.washingtonpost.com/rss/lifestyle/home-garden",
+  "http://feeds.washingtonpost.com/rss/lifestyle/style",
+  "http://feeds.washingtonpost.com/rss/lifestyle/travel",
+  "http://feeds.washingtonpost.com/rss/lifestyle/weddings",
+  "http://feeds.washingtonpost.com/rss/lifestyle/wellness",
+  "http://feeds.washingtonpost.com/rss/lifestyle/magazine",
+  "http://feeds.washingtonpost.com/rss/lifestyle/kidspost",
+  "http://feeds.washingtonpost.com/rss/lifestyle/on-parenting",
+  "http://feeds.washingtonpost.com/rss/lifestyle/blogs-columns",
+  "http://feeds.washingtonpost.com/rss/rss_arts-post",
+  "http://feeds.washingtonpost.com/rss/rss_reliable-source",
+  "http://feeds.washingtonpost.com/rss/entertainment",
+  "http://feeds.washingtonpost.com/rss/entertainment/books",
+  "http://feeds.washingtonpost.com/rss/entertainment/celebrities",
+  "http://feeds.washingtonpost.com/rss/entertainment/comics",
+  "http://feeds.washingtonpost.com/rss/entertainment/museums",
+  "http://feeds.washingtonpost.com/rss/entertainment/music",
+  "http://feeds.washingtonpost.com/rss/entertainment/theater-dance",
+  "http://feeds.washingtonpost.com/rss/entertainment/tv",
+  "http://feeds.washingtonpost.com/rss/entertainment/blogs-columns",
+  "http://feeds.washingtonpost.com/rss/rss_classical-beat",
+  "http://feeds.washingtonpost.com/rss/rss_comic-riffs",
+  "http://feeds.washingtonpost.com/rss/jobs/jobs-articles",
+  "http://feeds.washingtonpost.com/rss/jobs/career-advice",
+  // THE GUARDIAN
+  "http://feeds.theguardian.com/theguardian/politics/rss",
+  // ARS TECHNICA
+  "http://feeds.arstechnica.com/arstechnica/index/",
+  // THE VERGE
+  "http://www.theverge.com/rss/index.xml",
+  // ENGADGET
+  "http://www.engadget.com/rss.xml",
+  // TECHCRUNCH
+  "http://feeds.feedburner.com/TechCrunch/",
+  // THENEXTWEB
+  "http://thenextweb.com/feed/",
+  // MASHABLE
+  "http://feeds.mashable.com/Mashable"
 ];
