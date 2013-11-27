571d05b9a9782088b62d8b8413d088600cebffef
Raphael Moutard <raphael.moutard@gmail.com>
2013-11-27 00:22:50 -0800

    UI: notification for edit title & share

--- core/chrome/notification.js
+++ core/chrome/notification.js
@@ -4,18 +4,9 @@ Cotton.Core.Notification = Class.extend({
   init : function(sPreviousVersion) {
     var self = this;
     this._sId = "1";
-    this._sTitle = "New release of cottonTracks";
-    this._sMessage = "There is a new release for cottonTracks, check it out and explore what is new!";
-
-    if (sPreviousVersion === "0.6.0") {
-      this._sTitle = "The new version of cottonTracks is out!";
-      this._sMessage = "We've released a new interface for you to make the best of your online discoveries. Click here to give it a try now!";
-    } else if (sPreviousVersion === "0.7.0" || sPreviousVersion === "0.7.1") {
-      this._sTitle = "\"Favorite stories\" now available";
-      this._sMessage = "We've added a new feature to cottonTracks, you can now favorite the stories you like the most to access them even quicker. Click here to give it a try now!";
-    } else {
-      return;
-    }
+    this._sTitle = "Wow, two new features in cottonTracks!";
+    this._sMessage = "- Edit manually the title of your stories by clicking on it.\n" +
+      "- Share easily any card with facebook, twitter or email.";
 
     chrome.notifications.create(self._sId, {
       "type":"basic",
