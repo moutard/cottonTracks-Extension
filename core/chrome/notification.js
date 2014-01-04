569b88c129a0afe15c81ae6341f178daed359af2
Raphael Moutard <raphael.moutard@gmail.com>
2014-01-04 14:46:10 +0100

    Notification: notify about the proto

--- core/chrome/notification.js
+++ core/chrome/notification.js
@@ -2,6 +2,23 @@
 Cotton.Core.Notification = Class.extend({
 
   init : function(sPreviousVersion) {
+    var self = this;
+    this._sId = "1";
+    this._sTitle = "Try out the future of cottonTracks!";
+    this._sMessage = "We are testing an advanced version for cottonTracks, with more control over your organisation\n"
+      + "Click here to check it out!";
 
+    chrome.notifications.create(self._sId, {
+      "type":"basic",
+      "iconUrl": "/media/images/browser_action/cbutton38.png",
+      "title": this._sTitle,
+      "message": this._sMessage
+    }, function(){});
+
+    chrome.notifications.onClicked.addListener(function(sNotificationId){
+      if (sNotificationId === self._sId){
+        chrome.browserAction.onClicked.dispatch();
+      }
+    });
   }
 });
