8c0bafd36f6cc7c1696e1a79885f2edd67b048b2
Raphael Moutard <raphael.moutard@gmail.com>
2014-01-10 21:15:53 +0100

    Hotfix: fix quick adder

--- core/chrome/notification.js
+++ core/chrome/notification.js
@@ -2,24 +2,26 @@
 Cotton.Core.Notification = Class.extend({
 
   init : function(sPreviousVersion) {
-    var self = this;
-    this._sId = "1";
-    this._sTitle = "Try out the future of cottonTracks!";
-    this._sMessage = "We are testing an advanced version for cottonTracks, with more control over your organisation\n"
-      + "Click here to check it out!";
+    if (sPreviousVersion < "0.8.0") {
+      var self = this;
+      this._sId = "1";
+      this._sTitle = "Try out the future of cottonTracks!";
+      this._sMessage = "We are testing an advanced version for cottonTracks, with more control over your organisation\n"
+        + "Click here to check it out!";
 
-    chrome.notifications.create(self._sId, {
-      "type":"image",
-      "iconUrl": "/media/images/browser_action/cbutton38.png",
-      "imageUrl": "/media/images/notifications/notif_screen.png",
-      "title": this._sTitle,
-      "message": this._sMessage
-    }, function(){});
+      chrome.notifications.create(self._sId, {
+        "type":"image",
+        "iconUrl": "/media/images/browser_action/cbutton38.png",
+        "imageUrl": "/media/images/notifications/notif_screen.png",
+        "title": this._sTitle,
+        "message": this._sMessage
+      }, function(){});
 
-    chrome.notifications.onClicked.addListener(function(sNotificationId){
-      if (sNotificationId === self._sId){
-        chrome.browserAction.onClicked.dispatch();
-      }
-    });
+      chrome.notifications.onClicked.addListener(function(sNotificationId){
+        if (sNotificationId === self._sId){
+          chrome.browserAction.onClicked.dispatch();
+        }
+      });
+    }
   }
 });
